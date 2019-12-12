ec5f90874895362a5bb6b715be8c803ab5c90711
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2019-12-12 20:25:09 +0300

    Add files via upload

---
+++ web/a_conect.php
@@ -0,0 +1,9 @@
+﻿<?php
+
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$host = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$dbname = substr($url["path"], 1);
+
+?>
