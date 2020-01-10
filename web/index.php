4bd67df6420e0d89f1dc766efaacdc4cdad4a2bb
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-10 13:39:11 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,13 +1,13 @@
 <?php 
 	include_once '../vendor/autoload.php';	
 	include_once 'a_conect.php';
-/*	
+	
 	include_once 'site/site_01_head.php';	
 	include_once 'site/site_03_url.html';
 	include_once 'site/site_05_body.php';
 	include_once 'site/site_09_footer.html';
 	
-	include_once 'NaHerokuBot.php';         */
+	include_once 'NaHerokuBot.php'; 
 	include_once 'TesterBotoffBot.php';
 ?>
 </body>	
