a9c2af62fc55e4036a15f4ece6d63763752d3d92
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-10 13:04:24 +0300

    Add files via upload

--- web/TesterBotoffBot.php
+++ web/TesterBotoffBot.php
@@ -4,13 +4,12 @@ include_once 'a_conect.php';
 
 //exit('ok');
 
-$tg = new \TelegramBot\Api\BotApi($tokenMARKET);
+$tg = new \TelegramBot\Api\BotApi($tokenGARANT);
 
-$id_bota = '983003157';
-$obsujdaem_bot_group = '-1001413152703';
+$id_bota = '1066944801';
 
 // Группа администрирования бота (Админка)
-$admin_group = '-1001311775764';
+$admin_group = '-1001306245472';
 // Группа для тестирования бота (Тестрование Ботов)
 $admin_group_test = '-362469306'; 
 // Мастер это Я
@@ -18,21 +17,8 @@ $master='351009636';
 $testerbotoff = '1038937592';
 $channel='-1001368618561';  // Канал ТестерБотофф
 
-if ($_GET['privet']) {
-	$tg->sendMessage($obsujdaem_bot_group, $_GET['privet']);
-	echo "Сообщение {$_GET['privet']} отправленно в группу!";
-}
-
 // ФЛАГ ДЛЯ ВКЛЮЧЕНИЯ РЕЖИМА ОТЛАДКИ БОТА
 $OtladkaBota = 'да';
-
-$credentials = new Aws\Credentials\Credentials($aws_key_id, $aws_secret_key);
-	
-$s3 = new Aws\S3\S3Client([
-	'credentials' => $credentials, 
-    'version'  => 'latest',
-    'region'   => $aws_region
-]);
 
 $mysqli = new mysqli($host, $username, $password, $dbname);
 
@@ -42,30 +28,23 @@ if (mysqli_connect_errno()) {
 	exit('ok');
 }else { 	
 	// ПОДКЛЮЧЕНИЕ ВСЕХ ОСНОВНЫХ ПЕРЕМЕННЫХ
-	include 'BiblioBota/bot_02_varia.php';
+	include 'GarantBibla/01_variables.php';
 	
-	$text = str_replace ("@NaHerokuBot", "", $text);
+	$text = str_replace ("@TesterBotoffBot", "", $text);
 
 	// ПОДКЛЮЧЕНИЕ ВСЕХ ОСНОВНЫХ ФУНКЦИЙ
-	include 'BiblioBota/bot_03_func.php';
-	
-	if ($_GET['kurs']=='pzm'||$_GET['kurs']=='PZM'||$_GET['kurs']=='Prizm'||$_GET['kurs']=='PRIZM'||$_GET['kurs']=='Pzm'||$_GET['kurs']=='пзм'||$_GET['kurs']=='Пзм'||$_GET['kurs']=='ПЗМ'||$_GET['kurs']=='призм'||$_GET['kurs']=='Призм'||$_GET['kurs']=='ПРИЗМ') $_GET['kurs']='prizm';
-	
-	if ($_GET['kurs']=='prizm') echo _kurs_PZM();
-	
+	include 'GarantBibla/02_functions.php';
 	
 	$this_admin = _this_admin();
 	
 	if ($text == "/start"||$text == "s"||$text == "S"||$text == "с"||$text == "С"||$text == "c"||$text == "C"||$text == "Старт"||$text == "старт") {
 		if ($chat_type=='private') {
-			//_start_PZMgarant_bota($this_admin);  //БЕЗОПАСНЫЕ СДЕЛКИ
-			
-			_start_PZMarket_bota($this_admin);  // PZMarket
+			_start_PZMgarant_bota();  //БЕЗОПАСНЫЕ СДЕЛКИ			
 		}	
 	}
 	
 	// ПОДКЛЮЧЕНИЕ ОСНОВНОГО МОДУЛЯ
-	include_once 'BiblioBota/bot_01_head.php';		
+	include_once 'GarantBibla/03_header_bot.php';		
 }
 
 // закрываем подключение 
--- web/a_conect.php
+++ web/a_conect.php
@@ -1,9 +1,16 @@
 ﻿<?php
 
+	$aws_key_id = getenv('AWS_ACCESS_KEY_ID');
+	$aws_secret_key = getenv('AWS_SECRET_ACCESS_KEY');
+	$aws_region = getenv('AWS_REGION');
+	$aws_bucket = getenv('AWS_BUCKET_NAME');
+
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 	$host = $url["host"];
 	$username = $url["user"];
 	$password = $url["pass"];
 	$dbname = substr($url["path"], 1);
 
+	$tokenMARKET = getenv('TOKEN_MARKET');
+	$tokenGARANT = getenv('TOKEN_GARANT');
 ?>
