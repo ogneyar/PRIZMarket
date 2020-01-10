cd2a2231c8cd4bd20c450acc06c069b399af0b86
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-10 12:51:56 +0300

    Add files via upload

---
+++ web/Proto/index.php
@@ -0,0 +1,53 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+	    <link rel="stylesheet" type="text/css" href="/site/style.css">  
+		<meta charset="utf-8">
+		<title>Форма заказа</title>		
+		
+	</head>
+	<body>
+	
+	<div class="fon-img">
+		<section class="block-img">
+			<table class="tab" align="center">
+			<tr>
+				<td align="center">
+					<form action="https://teleg.link/prizm_market" method="GET">
+						<button class="knopa"><img class="knopa_img" alt="Здесь красивая картинка" src="/site/photo/PRIZMarket.png"/></button>
+					</form>
+				</td>				
+			
+				<td align="center">
+					<form action="/site/zakaz_form.html" method="GET">
+						<button class="knopa"><img class="knopa_img" alt="Здесь красивая картинка" src="/site/photo/PRIZMarketZakaz.png"/></button>
+					</form>
+				</td>		
+
+				<td align="center">
+					<form action="/Proto/index.php" method="GET">
+						<button class="knopa"><img class="knopa_img" alt="«Здесь красивая картинка" src="/site/photo/PRIZMarketChat.png"/></button>
+					</form>
+				</td>				
+			</tr>
+			
+			
+			</table>	
+		
+		</section><br>
+	
+	
+		<center>
+		
+			<iframe width="500" height="550" src="/Proto/form.php"></iframe>&nbsp;&nbsp;			
+
+			<iframe width="350" height="550" src="/site/aside.html"></iframe>
+
+		</center><br>
+		
+		<center><iframe width="100%" height="100%" src="/site/footer.php"></iframe></center><br>	
+	</div>
+
+	</body>
+</html>
+	
