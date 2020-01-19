1944a79f153ba2817ae735c91ee1893a4104546b
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-19 17:16:02 +0300

    Add files via upload

--- web/botZakaz.php
+++ web/botZakaz.php
@@ -15,7 +15,7 @@ $id_bota = strstr($token, ':', true);
 $admin_group = $admin_group_Zakaz;
 
 // ФЛАГ ДЛЯ ВКЛЮЧЕНИЯ РЕЖИМА ОТЛАДКИ БОТА
-//$OtladkaBota = 'да';
+$OtladkaBota = 'да';
 
 // Подключение БД
 $mysqli = new mysqli($host, $username, $password, $dbname);
