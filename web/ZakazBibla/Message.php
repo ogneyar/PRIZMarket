dbe6dbda3869653d92887362c49050569b9a9b69
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-19 15:08:08 +0300

    Add files via upload

---
+++ web/ZakazBibla/Message.php
@@ -0,0 +1,29 @@
+﻿<?
+// Если пришла ссылка типа t.me//..?start=123456789
+if (strpos($text, "/start ")!==false) $text = str_replace ("/start ", "", $text);
+
+// проверяем если пришло сообщение
+if ($text) {
+
+	if ($reply_to_message) {	
+	
+			
+			
+	}else{
+		
+		
+		
+	}		
+       
+}elseif ($photo) {
+
+    
+}elseif ($video) {
+	
+	
+}
+
+
+
+
+?>
