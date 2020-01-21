4c6461d085284147f24fc1f705acd62bc13471c3
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-21 21:57:57 +0300

    Add files via upload

--- web/BiblaInfo/Commands.php
+++ web/BiblaInfo/Commands.php
@@ -1,4 +1,14 @@
 ﻿<?
+if (strpos($text, "удали:")!==false) {
+
+	$komanda = strstr($text, ':', true);	
+	
+	$id = substr(strrchr($text, ":"), 1);
+	
+	$text = $komanda;
+
+}
+
 
 if ($text == 'база') {
 
@@ -8,11 +18,49 @@ if ($text == 'база') {
 
 }elseif ($text == 'изи') {
 	
-	$query = "UPDATE `{$table_users}` SET `first_name` = 'Отрадъ', `last_name` = '', `user_name` = '@Otrad_ya' WHERE `id_client` =298466355";
-	if ($result = $mysqli->query($query)) {
-		$bot->sendMessage($master, "Всё отлично!");
-	}else throw new Exception("Не смог изменить таблицу {$table_message}");	
+	$query = "UPDATE `{$table_users}` SET `first_name` = 'Огънеяръ', `last_name` = '', `user_name` = '@Ogneyar_ya' WHERE `id_client` =351009636";
 	
+	if ($result = $mysqli->query($query)) {
+	
+		$bot->sendMessage($master, "Всё отлично!");
+		
+	}else throw new Exception("Не смог изменить таблицу {$table_users}");	
+	
+	
+	
+}elseif ($text == 'удали') {
+	
+	$query = "DELETE FROM ".$table_users." WHERE id_client=".$id;				
+	
+	if ($result = $mysqli->query($query)) {
+	
+		$bot->sendMessage($master, "Всё отлично!");
+		
+	}else throw new Exception("Не смог изменить таблицу {$table_users}");	
+	
+	
+}elseif (($text == "админ")&&($id)) {		
+		
+	$query = "UPDATE ".$table_users." SET status='admin' WHERE id=".$id;
+	
+	if ($result = $mysqli->query($query)) {
+	
+		$bot->sendMessage($master, "Всё отлично!");
+		
+	}else throw new Exception("Не смог изменить таблицу {$table_users}");	
+		
+		
+}elseif (($text == "-админ")&&($id)) {		
+		
+	$query = "UPDATE ".$table_users." SET status='client' WHERE id=".$id;
+	
+	if ($result = $mysqli->query($query)) {
+	
+		$bot->sendMessage($master, "Всё отлично!");
+		
+	}else throw new Exception("Не смог изменить таблицу {$table_users}");	
+		
+		
 }
 
 
