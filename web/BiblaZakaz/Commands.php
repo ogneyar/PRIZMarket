eb4db42361ccda37731a117ea9f800b35e90bef0
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-21 16:56:26 +0300

    Add files via upload

--- web/BiblaZakaz/Commands.php
+++ web/BiblaZakaz/Commands.php
@@ -12,7 +12,7 @@ if ($text == 'table message'||$text == 'таблица сообщений'||$tex
 
 }elseif ($text == 'изи') {
 	
-	$query = "ALTER TABLE `zakaz_message` CHANGE `date` `date` BIGINT NULL DEFAULT NULL";
+	$query = "UPDATE `zakaz_users` SET `first_name` = '{$from_first_name}', `last_name` = '', `user_name` = '@Ogneyar' WHERE `id_client` =351009636";
 	if (!$result = $mysqli->query($query)) throw new Exception("Не смог изменить таблицу {$table_message}");
 	
 	
