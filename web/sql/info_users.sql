ad663ada874f837a833929637eec834707179d59
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-21 11:01:09 +0300

    Add files via upload

---
+++ web/sql/info_users.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Янв 21 2020 г., 11:38
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `testerbotoff`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `info_users`
+--
+
+CREATE TABLE IF NOT EXISTS `info_users` ( 
+  `id_client` bigint(20) DEFAULT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `user_name` varchar(20) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `info_users`
+--
+
+INSERT INTO `info_users` (`id_client`, `first_name`, `last_name`, `user_name`, `status`) VALUES
+(351009636, 'Огънеяръ', '', 'Ogneyar', 'admin'),
+(298466355, 'Отрадъ', '', 'Otrad', 'admin');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
