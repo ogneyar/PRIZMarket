a978e163017b5c166d5191302a7d00bdfff1314f
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-21 21:41:06 +0300

    Add files via upload

--- web/BiblaInfo/Commands.php
+++ web/BiblaInfo/Commands.php
@@ -2,13 +2,13 @@
 
 if ($text == 'база') {
 
-	$bot->output_table($table_users, '240');
+	$bot->output_table($table_users, '260');
 	
 
 
 }elseif ($text == 'изи') {
 	
-	$query = "UPDATE `{$table_users}` SET `first_name` = 'Отрадъ' WHERE `id_client` =298466355";
+	$query = "UPDATE `{$table_users}` SET `first_name` = 'Отрадъ', `last_name` = '', `user_name` = '@Otrad_ya' WHERE `id_client` =298466355";
 	if ($result = $mysqli->query($query)) {
 		$bot->sendMessage($master, "Всё отлично!");
 	}else throw new Exception("Не смог изменить таблицу {$table_message}");	
