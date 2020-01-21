2ed4fe8f507a6150c0c0ce73b7c7fb7960a38684
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-21 22:30:47 +0300

    Add files via upload

--- web/BiblaZakaz/Commands.php
+++ web/BiblaZakaz/Commands.php
@@ -1,25 +1,82 @@
 ﻿<?
 
-if ($text == 'table message'||$text == 'таблица сообщений'||$text == 'соо') {
+if (strpos($text, ":")!==false) {
+
+	$komanda = strstr($text, ':', true);	
+	
+	$id = substr(strrchr($text, ":"), 1);
+	
+	$text = $komanda;
+
+}
+
+
+if ($text == 'соо') {
 	
 	$bot->output_table($table_message);	
 	
 		
 }elseif ($text == 'база') {
 
-	$bot->output_table($table_users);
+	if ($id) {
+	
+		$bot->output_table($table_users, $id);
+	
+	}else {
+		
+		$bot->output_table($table_users);
+		
+	}	
 
 
 }elseif ($text == 'изи') {
 	
-	$query = "UPDATE `zakaz_users` SET `first_name` = '{$from_first_name}', `last_name` = '', `user_name` = '@Ogneyar' WHERE `id_client` =351009636";
-	if (!$result = $mysqli->query($query)) throw new Exception("Не смог изменить таблицу {$table_message}");
+	$query = "ALTER TABLE `zakaz_users` CHANGE `first_name` `first_name` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
+CHANGE `last_name` `last_name` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
+CHANGE `user_name` `user_name` VARCHAR( 200 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL";
+	if ($result = $mysqli->query($query)) {
+		$bot->sendMessage($master, "Всё отлично!");
+	}else throw new Exception("Не смог изменить таблицу {$table_message}");
 	
 	
 }elseif ($text == 'дел') {
 	
 	_deleting_old_records($table_message);	
 	
+	
+}elseif ($text == 'удали') {
+	
+	$query = "DELETE FROM ".$table_users." WHERE id_client=".$id;				
+	
+	if ($result = $mysqli->query($query)) {
+	
+		$bot->sendMessage($master, "Всё отлично!");
+		
+	}else throw new Exception("Не смог изменить таблицу {$table_users}");	
+	
+	
+}elseif (($text == "админ")&&($id)) {		
+		
+	$query = "UPDATE ".$table_users." SET status='admin' WHERE id_client=".$id;
+	
+	if ($result = $mysqli->query($query)) {
+	
+		$bot->sendMessage($master, "Всё отлично!");
+		
+	}else throw new Exception("Не смог изменить таблицу {$table_users}");	
+		
+		
+}elseif (($text == "-админ")&&($id)) {		
+		
+	$query = "UPDATE ".$table_users." SET status='client' WHERE id_client=".$id;
+	
+	if ($result = $mysqli->query($query)) {
+	
+		$bot->sendMessage($master, "Всё отлично!");
+		
+	}else throw new Exception("Не смог изменить таблицу {$table_users}");	
+		
+		
 }
 
 
