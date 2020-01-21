3705dea52f7227356d816809f7ed8d2f1d29aaa2
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-21 12:40:20 +0300

    Add files via upload

---
+++ web/BiblaInfo/Commands.php
@@ -0,0 +1,18 @@
+﻿<?
+
+if ($text == 'база') {
+
+	$bot->output_table($table_users);
+
+
+}elseif ($text == 'изи') {
+	
+	$query = "ALTER TABLE `zakaz_message` CHANGE `date` `date` BIGINT NULL DEFAULT NULL";
+	if (!$result = $mysqli->query($query)) throw new Exception("Не смог изменить таблицу {$table_message}");
+	
+	
+}
+
+
+
+?>
