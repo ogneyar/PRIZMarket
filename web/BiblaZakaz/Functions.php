1cd0290b9c0b0bf7b1a92119ccabd4f7a971b3a5
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-21 12:18:20 +0300

    Add files via upload

--- web/BiblaZakaz/Functions.php
+++ web/BiblaZakaz/Functions.php
@@ -3,7 +3,9 @@
 // функция старта бота ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЯХ
 function _start_Zakaz_bota() {		
 
-	global $bot, $chat_id, $from_first_name, $HideKeyboard;
+	global $bot, $chat_id, $from_first_name, $HideKeyboard, $table_users;
+	
+	$bot->add_to_database($table_users);
 	
 	$bot->sendMessage($chat_id, "Добро пожаловать, *".$from_first_name."*!", markdown, $HideKeyboard);
 
