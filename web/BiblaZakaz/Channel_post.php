804ae1b1ebd760b4b3b6b4ae8a44851bd912cd2e
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-22 21:31:28 +0300

    Add files via upload

--- web/BiblaZakaz/Channel_post.php
+++ web/BiblaZakaz/Channel_post.php
@@ -1,5 +1,20 @@
 <?
 
+$number = stripos($text, 'Информация о пользователе:');
+if ($number!==false) {
+	
+	$string = substr(strrchr($text, "id: "), 4);
+	
+	$id = strstr($string, 10, true);	
+	
+	$text = substr(strrchr($string, 10), 1);
+	
+	$text.= "Информация о пользователе:\n".
+		"id: [".$forward_id."](tg://user?id=".$forward_id.")\n".$text;
+	
+	$bot->sendMessage($channel_info, $text);
+	
+}
 
 
 
