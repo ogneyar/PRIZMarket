efedf78c6f3a6603e273464d6da69a59537e8acb
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-23 21:55:39 +0300

    Add files via upload

---
+++ web/BiblaPublic/Channel_post.php
@@ -0,0 +1,42 @@
+<?
+
+foreach ($entities as $ent) {
+	
+	if ($ent['type'] == 'text_mention') $user = $ent['user'];
+	
+}
+
+if ($user) {
+	
+	$url_info = "https://t.me/Ne_wTest_Bot?start=".$user['id'];
+
+	$bot->sendMessage($chat_id, $url_info, null, null, null, true);
+
+}
+
+
+/*
+$number = stripos($text, 'Информация о пользователе:');
+
+if ($number!==false) {	
+	
+	//$string = strrchr($text, ":");
+	
+	$string = substr(strstr($text, "id: "), 4);
+	
+	$id = strstr($string, 10, true);	
+	
+	$text = substr(strrchr($string, 10), 1);
+	
+	$textA.= "Информация о пользователе:\n".
+		"id: [".$id."](tg://user?id=".$id.")\n".$text;
+	
+	$bot->sendMessage($channel_info, $textA);
+	
+}
+*/
+
+
+
+
+?>
---
+++ web/BiblaPublic/Message.php
@@ -0,0 +1,132 @@
+﻿<?
+
+if ($reply_to_message) {	
+
+	if ($reply_forward) {
+			
+		// $reply_forward_id - это айди клиента написавшего сообщение боту,
+		// из базы надо будет достать message_id клиента по reply_forward_id (id клиента)
+		// осуществив поиск reply_message_id,  которое в базе записано как
+		// message_id_out
+			
+		$query = "SELECT message_id_in FROM {$table_message} WHERE message_id_out={$reply_message_id} AND client_id={$reply_forward_id}";
+		if ($result = $mysqli->query($query)) {				
+			if($result->num_rows>0){
+				$arrayResult = $result->fetch_all(MYSQLI_ASSOC);				
+				$message_id_in = $arrayResult[0]['message_id_in'];
+			}				
+		}else throw new Exception("Не смог узнать message_id_in в таблице {$table_message}");
+			
+		if ($text) {
+			
+			$result = $bot->sendMessage($reply_forward_id, $text, null, null, $message_id_in);
+				
+		}elseif ($photo) {			
+			
+			$result = $bot->sendPhoto($reply_forward_id, $file_id, null, null, null, $message_id_in);
+			
+		}elseif ($video) {
+			
+			$result = $bot->sendVideo($reply_forward_id, $file_id, null, null, null, $message_id_in);
+			
+		}
+				
+			
+		if ($result) {
+			
+			// а после надо сохранить айди сообщения админа клиенту
+			// для возможности его редактирования
+			
+			$query = "INSERT INTO {$table_message} VALUES ('{$reply_forward_id}',
+				'{$message_id}', '{$result['message_id']}', '{$result['date']}')";
+			$mysql_result = $mysqli->query($query);
+					
+			if (!$mysql_result) throw new Exception("Не смог сделать записать в таблицу {$table_message}");
+		}
+			
+			
+	}elseif ($reply_sender_name) {
+			
+		$bot->sendMessage($chat_id, "Профиль скрыт.");
+			
+	}elseif ($chat_type == 'private') {			
+
+		$result = $bot->forwardMessage($admin_group, $chat_id, $message_id);
+			
+		if ($result) {			
+			
+			$query = "INSERT INTO {$table_message} VALUES ('{$chat_id}',
+				'{$message_id}', '{$result['message_id']}', '{$result['date']}')";
+			$mysql_result = $mysqli->query($query);
+					
+			if (!$mysql_result) throw new Exception("Не смог сделать записать в таблицу {$table_message}");
+				
+		}			
+	
+	}
+	
+}elseif ($text=='инфо') {
+		
+       _info_Zakaz_bota();
+		
+}elseif ($text=='пост'||$text == 'post') {
+		
+		if ($from_username == '') {
+		   
+			$bot->sendMessage($chat_id, "Так, чел сдрысни отседа!\n\nВозвращайся когда поставишь себе @username..");
+		   
+		}else {
+		   
+			$result = $bot->forwardMessage($channel_info, $chat_id, $message_id);
+		   
+			if ($result) {
+			   
+				$result = $bot->sendMessage($channel_info, "@".$from_username);
+			   
+				if ($result) {
+					
+					$result = $bot->sendMessage($channel_info, "&".$from_id);
+					
+				}
+			   
+			}
+		   
+		}
+		
+}else { // если просто текст, фото или видео, безовсяких ответов и пересылок
+		
+	if ($chat_type == 'private') {  // если в личку боту
+			
+		$bot->add_to_database($table_users);
+			
+		_deleting_old_records($table_message, $day);
+			
+		// клиент написал, надо в базе сохранить его id, message_id_in, date 
+		// (и id_message_out, которое будет найдено ниже)
+		// что бы потом с базы доставать message_id по date зная id клиента)			
+		$result = $bot->forwardMessage($admin_group, $chat_id, $message_id);
+					
+		if ($result) {
+				
+			// номер сообщения, которое бот отправил в админку
+			// по этому номеру будет находиться message_id клиента,
+			// когда админ ответит на сообщение (reply_to_message)
+				
+			$query = "INSERT INTO {$table_message} VALUES ('{$chat_id}',
+				'{$message_id}', '{$result['message_id']}', '{$result['date']}')";
+			$mysql_result = $mysqli->query($query);
+				
+			if (!$mysql_result) throw new Exception("Не смог сделать записать в таблицу {$table_message}");
+				
+		}
+		
+	}
+	
+}	
+  
+  
+
+
+
+
+?>
