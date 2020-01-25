77cc7ddc5230589c2c2191e43739cf4e70cce3bc
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-25 22:04:43 +0300

    Add files via upload

---
+++ web/sql/avtozakaz_pzmarket.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Янв 25 2020 г., 23:04
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `testerbotoff`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `avtozakaz_pzmarket`
+--
+
+CREATE TABLE IF NOT EXISTS `avtozakaz_pzmarket` (
+  `id_client` bigint(20) DEFAULT NULL,
+  `id_zakaz` int(20) DEFAULT NULL,
+  `kuplu_prodam` varchar(100) DEFAULT NULL,
+  `nazvanie` varchar(500) DEFAULT NULL,
+  `url_nazv` varchar(200) DEFAULT NULL,
+  `valuta` varchar(100) DEFAULT NULL,
+  `gorod` varchar(200) DEFAULT NULL,
+  `username` varchar(200) DEFAULT NULL,
+  `doverie` tinyint(1) DEFAULT NULL,
+  `otdel` varchar(100) DEFAULT NULL,
+  `format_file` varchar(20) DEFAULT NULL,
+  `file_id` varchar(200) DEFAULT NULL,
+  `url_podrobno` varchar(200) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `podrobno` blob,
+  `url_tgraph` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
