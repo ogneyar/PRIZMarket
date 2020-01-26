be78b78dedef9a2f94dbfa3255da3bfbc42344d7
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-26 20:54:20 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Callback_query.php
+++ web/BiblaAvtoZakaz/Callback_query.php
@@ -6,6 +6,17 @@ if ($callback_data=='создать'){
 
 	_insert_kuplu_prodam();
 
+}elseif ($callback_data=='продаю') {
+
+	$bot->answerCallbackQuery($callback_query_id, "Продано!");
+
+	$bot->forwardMessage(
+		$channel_info,
+		$chat_id,
+		$message_id
+	);
+
+
 }
 
 ?>
