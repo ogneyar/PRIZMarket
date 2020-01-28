3fdecd0cea6d609f0d2b7432ba6b08700a15a0ba
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-28 11:27:27 +0300

    Add files via upload

--- web/BiblaInfo/Commands.php
+++ web/BiblaInfo/Commands.php
@@ -25,7 +25,12 @@ if ($text == 'база') {
 
 }elseif ($text == 'изи') {
 	
-	$query = "ALTER TABLE `zakaz_message` ADD `flag` BOOLEAN NULL DEFAULT NULL";
+	$query = "CREATE TABLE IF NOT EXISTS `avtozakaz_ojidanie` (
+		  `id_client` bigint(20) DEFAULT NULL,
+		  `ojidanie` varchar(200) DEFAULT NULL,
+		  `last` varchar(200) DEFAULT NULL,
+		  `flag` tinyint(1) DEFAULT NULL
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8";
 	
 	if ($result = $mysqli->query($query)) {
 	
