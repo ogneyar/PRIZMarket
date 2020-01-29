de80f16c9bdf708c90faa9c80d44e2195e9c9245
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-29 12:40:51 +0300

    Add files via upload

---
+++ web/sql/avtozakaz_mediagroup.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Янв 29 2020 г., 13:40
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `testerbotoff`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `avtozakaz_mediagroup`
+--
+
+CREATE TABLE IF NOT EXISTS `avtozakaz_mediagroup` (
+  `id` int(10) DEFAULT NULL,
+  `id_client` bigint(20) DEFAULT NULL,
+  `media_group_id` bigint(20) DEFAULT NULL,
+  `format_file` varchar(20) DEFAULT NULL,
+  `file_id` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
