5853505791c4bcc25e992d532f22e58f6cd7a518
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-03 22:33:03 +0300

    Add files via upload

--- web/myBotApi/Variables.php
+++ web/myBotApi/Variables.php
@@ -421,5 +421,26 @@ $InputMediaPhoto = [
 
 
 
+$InlineQueryResultPhoto = [
+	[
+
+		'type' => 'photo',
+		'id' => $inline_query_id,
+		'photo_url' => 'https://i.ibb.co/SRCv6Z7/file-23.jpg',
+		'thumb_url' => 'https://i.ibb.co/SRCv6Z7/file-21.jpg',
+		'photo_width' => null,
+		'photo_height' => null,
+		'title' => null,
+		'description' => null,
+		'caption' => null,
+		'parse_mode' => null,
+		'reply_markup' => null,
+		'input_message_content' => null
+		
+	]	
+];
+
+
+
 
 ?>
