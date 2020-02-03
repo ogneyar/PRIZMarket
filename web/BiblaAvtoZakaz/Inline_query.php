f83a8f794a3d62610dcd83ec32cc7b81153ce868
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-03 23:17:04 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Inline_query.php
+++ web/BiblaAvtoZakaz/Inline_query.php
@@ -4,23 +4,6 @@ if ($inline_query) {
 
 	//$bot->sendMessage($master, "еее");
 	
-	$InlineQueryResult = [
-		[
-			'type' => 'photo',
-			'id' => $from_id,
-			'photo_url' => 'https://i.ibb.co/SRCv6Z7/file-23.jpg',
-			'thumb_url' => 'https://i.ibb.co/SRCv6Z7/file-21.jpg',
-			'photo_width' => null,
-			'photo_height' => null,
-			'title' => null,
-			'description' => null,
-			'caption' => null,
-			'parse_mode' => null,
-			'reply_markup' => null,
-			'input_message_content' => null		
-		]	
-	];
-	
 	$InputTextMessageContent = [
 		'message_text' => 'текст',
 		'parse_mode' => null,
@@ -36,7 +19,7 @@ if ($inline_query) {
 			'reply_markup' => null,
 			'url' => null,
 			'hide_url' => false,
-			'description' => null,
+			'description' => 'ляляля',
 			'thumb_url' => null,
 			'thumb_width' => null,
 			'thumb_height' => null
