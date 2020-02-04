9a46ab8e09776f09976215990354262bae3d80a2
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-04 20:44:45 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Inline_query.php
+++ web/BiblaAvtoZakaz/Inline_query.php
@@ -6,13 +6,26 @@ if ($inline_query) {
 		[
 			'type' => 'article',
 			'id' => $from_id,
+			'title' => 'Отличная кнопка',
+			'input_message_content' => [ 'message_text' => 'любой тут текст' ],
+			//'reply_markup' => null,
+			//'url' => null,
+			//'hide_url' => false,
+			'description' => 'нажми её',
+			//'thumb_url' => null,
+			//'thumb_width' => null,
+			//'thumb_height' => null
+		],
+		[
+			'type' => 'article',
+			'id' => $from_id,
 			'title' => 'титл',
 			'input_message_content' => [ 'message_text' => 'мессадж_текст' ],			
 			'description' => 'нажми на кнопку'		
 		]
 	];
 	
-	$bot->answerInlineQuery($inline_query_id, $InlineQueryResultArticle);
+	$bot->answerInlineQuery($inline_query_id, $InlineQueryResult);
 
 }
 
