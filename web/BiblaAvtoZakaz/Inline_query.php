3cca03a16e920926988f076c6b63f30c4670ddcb
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-04 20:05:55 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Inline_query.php
+++ web/BiblaAvtoZakaz/Inline_query.php
@@ -4,25 +4,19 @@ if ($inline_query) {
 
 	//$bot->sendMessage($master, "еее");
 	
-	$InputTextMessageContent = [
-		'message_text' => 'текст',
-		'parse_mode' => null,
-		'disable_web_page_preview' => false
-	];
-
-	$InlineQueryResultArticle = [
+	$InlineQueryResult = [
 		[
 			'type' => 'article',
 			'id' => $from_id,
-			'title' => 'тител',
-			'input_message_content' => $InputTextMessageContent,			
-			'description' => 'ляляля',			
+			'title' => 'title',
+			'input_message_content' => [ 'message_text' => 'message_text' ],			
+			'description' => 'description'		
 		]
 	];
 	
-	$р = $bot->answerInlineQuery($inline_query_id, $InlineQueryResultArticle);
+	$р = $bot->answerInlineQuery($inline_query_id, $InlineQueryResult);
 
-    $bot->sendMessage($from_id, $bot->PrintArray($p). "\n\nTrututu\n\n".$inline_query_id."\n\n".$from_id);
+    $bot->sendMessage($from_id, $bot->PrintArray($p));
 
 }
 
