4d6c07b42870b4316433dabfad32f739dfa3ff0b
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-04 09:54:41 +0300

    kurs

--- web/BiblaGarant/09_inline_query.php
+++ web/BiblaGarant/09_inline_query.php
@@ -31,7 +31,7 @@ if ($arr['inline_query']['query']=='курс'||$arr['inline_query']['query']=='�
 	$queryArticle = new \TelegramBot\Api\Types\Inline\QueryResult\Article($inline_query_from_id,
 		$title, $textClick, null,null,null, $inputText, $keyInLine10);		
 	$res=[$queryArticle];				
-	$tg->answerInlineQuery($inline_query_id, $res, null,null,null,'h','hek');			
+	$tg->answerInlineQuery($inline_query_id, $res, null,null,null,'курс','kurs');			
 	
 	exit('ok');
 }else {
