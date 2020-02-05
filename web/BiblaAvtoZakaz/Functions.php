1e6ccfc3db72b71a538abab81f5396e67650c79e
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-05 18:10:28 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Functions.php
+++ web/BiblaAvtoZakaz/Functions.php
@@ -47,6 +47,8 @@
 ** _вывод_лота_на_каналы
 ** _удалить_лот
 ** _отправка_сообщений_инфоботу
+** 
+** _возврат_лотов_для_инлайн
 **
 */
 
@@ -1532,6 +1534,128 @@ function _отправка_сообщений_инфоботу() {
 	$bot->sendMessage($channel_info, $from_id);	
 	
 }
+
+
+
+
+
+
+// возврат лотов из базы по номеру клиента для инлайн режима
+function _возврат_лотов_для_инлайн($id_client) {
+
+	global $table_market, $bot, $mysqli, $master;
+	
+	$response = false;
+	
+	$запрос = "SELECT * FROM {$table_market} WHERE id_client={$id_client} AND id_zakaz>0";
+		
+	$результат = $mysqli->query($запрос);
+	
+	if ($результат) {
+		
+		if ($результат->num_rows > 0) {
+		
+			$результМассив = $результат->fetch_all(MYSQLI_ASSOC);
+			
+			$i = 1;
+			
+			$InlineQueryResult = [];
+			
+			foreach ($результМассив as $строка) {
+			
+				$файлАйди = $строка['file_id'];		
+				$формат_файла = $строка['format_file'];				
+				$название = $строка['nazvanie'];
+				$ссыль_в_названии = $строка['url_nazv'];
+				$куплю_или_продам = $строка['kuplu_prodam'];								
+				$валюта = $строка['valuta'];				
+				$хештеги_города = $строка['gorod'];				
+				$юзера_имя = $строка['username'];				
+				$доверие = $строка['doverie'];
+				$категория = $строка['otdel'];				
+				//$подробности = $строка['podrobno'];	
+				$ссыль_на_подробности = $строка['url_podrobno'];
+				
+				$photo_url = $строка['url_tgraph'];	
+						
+				$inLine = [
+					'inline_keyboard' => [
+						[
+							[
+								'text' => 'Подробнее',
+								'url' => $ссыль_на_подробности
+							]
+						]
+					]
+				];				
+				
+				$хештеги = "{$куплю_или_продам}\n\n{$категория}\n▪️";				
+				$хештеги = str_replace('_', '\_', $хештеги);
+				
+				$текст_после_названия = "\n▪️{$валюта}\n▪️{$хештеги_города}\n▪️{$юзера_имя}";					
+				$текст_после_названия = str_replace('_', '\_', $текст_после_названия);
+					
+				if ($доверие) $текст_после_названия .= "\n\n✅ PRIZMarket доверяет❗️"; 
+					
+				$текст = "{$хештеги}[{$название}]({$ссыль_в_названии}){$текст_после_названия}";
+					
+				if ($формат_файла == 'фото') {
+				
+					$InlineQueryResult = array_merge($InlineQueryResult, [
+						[
+							'type' => 'photo',
+							'id' => $id_client."_".$i,
+							'photo_url' => $photo_url,
+							'thumb_url' => $photo_url,							
+							'title' => $куплю_или_продам,
+							'description' => $название,
+							'caption' => $текст,
+							'parse_mode' => 'markdown',
+							'reply_markup' => $inLine,							
+						],
+					]);			
+						
+				}elseif ($формат_файла == 'видео') {
+					
+					$Объект_файла = $bot->getFile($файлАйди);		
+		
+					$ссыль_на_файл = $bot->fileUrl . $bot->token;	
+					
+					$ссыль = $ссыль_на_файл . "/" . $Объект_файла['file_path'];
+					
+					$InlineQueryResult = array_merge($InlineQueryResult, [
+						[						
+							'type' => 'video',
+							'id' => $id_client."_".$i,
+							'video_url' => $ссыль,
+							'mime_type' => 'video/mp4', // или 'text/html'
+							'thumb_url' => $ссыль,
+							'title' => $куплю_или_продам,					
+							'caption' => $текст,
+							'description' => $название,
+							'parse_mode' => 'markdown',
+							'reply_markup' => $inLine,	
+							
+						],
+					]);				
+						
+				}
+				
+				$i++;
+				
+			}
+			
+			$response = $InlineQueryResult;
+		
+		}else throw new Exception("Или нет заказа или больше одного..");			
+	
+	}else throw new Exception("Нет такого заказа..");	
+	
+	return $response;
+	
+}
+
+
 
 
 
--- web/BiblaAvtoZakaz/Inline_query.php
+++ web/BiblaAvtoZakaz/Inline_query.php
@@ -2,61 +2,9 @@
 
 if ($inline_query) {
 
-	$InlineQueryResultVideo = [
-		[
-		// обязательные
-			'type' => 'video',
-			'id' => $from_id."xz",
-			'video_url' => 'https://prizm-market.s3.amazonaws.com/1006.mp4',
-			'mime_type' => 'video/mp4', // или 'text/html'
-			'thumb_url' => 'https://prizm-market.s3.amazonaws.com/1006.mp4',
-			'title' => 'нажми',
-		// необязательные
-			//'caption' => '',
-			//'parse_mode' => '',
-			//'video_width' => '',
-			//'video_height' => '',
-			//'video_duration' => '',
-			//'description' => '',
-			//'reply_markup' => '',
-			//'input_message_content' => ''
-		]
-	];
-
-	$InlineQueryResultPhoto = [
-		[
-			'type' => 'photo',
-			'id' => $from_id."z",
-			'photo_url' => 'https://i.ibb.co/SRCv6Z7/file-23.jpg',
-			'thumb_url' => 'https://i.ibb.co/SRCv6Z7/file-21.jpg',
-			//'photo_width' => null,
-			//'photo_height' => null,
-			'title' => 'Продам',
-			'description' => 'ассортимент',
-			'caption' => '#продам',
-			//'parse_mode' => null,
-			//'reply_markup' => null,
-			//'input_message_content' => null		
-		]	
-	];
+	$результат = _возврат_лотов_для_инлайн($from_id);
 	
-	$InlineQueryResult = [
-		[
-			'type' => 'article',
-			'id' => $from_id,
-			'title' => 'Отличная кнопка',
-			'input_message_content' => [ 'message_text' => 'любой тут текст' ],
-			//'reply_markup' => null,
-			//'url' => null,
-			//'hide_url' => false,
-			'description' => 'нажми её',
-			//'thumb_url' => null,
-			//'thumb_width' => null,
-			//'thumb_height' => null
-		]
-	];
-	
-	$bot->answerInlineQuery($inline_query_id, $InlineQueryResultVideo, null, false, null, "в бот", "s");
+	$bot->answerInlineQuery($inline_query_id, $результат, null, false, null, "в бот", "s");
 
 }
 
