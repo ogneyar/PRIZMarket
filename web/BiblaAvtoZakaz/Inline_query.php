6efc99c1922126798fc2d392d382583cbd832eb3
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-05 10:55:08 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Inline_query.php
+++ web/BiblaAvtoZakaz/Inline_query.php
@@ -2,26 +2,38 @@
 
 if ($inline_query) {
 
+
+$InlineQueryResultPhoto = [
+	[
+		'type' => 'photo',
+		'id' => $from_id,
+		'photo_url' => 'https://i.ibb.co/SRCv6Z7/file-23.jpg',
+		'thumb_url' => 'https://i.ibb.co/SRCv6Z7/file-21.jpg',
+		'photo_width' => null,
+		'photo_height' => null,
+		'title' => null,
+		'description' => null,
+		'caption' => null,
+		'parse_mode' => null,
+		'reply_markup' => null,
+		'input_message_content' => null		
+	]	
+];
+
+
 	$InlineQueryResult = [
 		[
 			'type' => 'article',
 			'id' => $from_id,
 			'title' => 'Отличная кнопка',
 			'input_message_content' => [ 'message_text' => 'любой тут текст' ],
-			//'reply_markup' => null,
+			'reply_markup' => null,
 			//'url' => null,
 			//'hide_url' => false,
 			'description' => 'нажми её',
 			//'thumb_url' => null,
 			//'thumb_width' => null,
 			//'thumb_height' => null
-		],
-		[
-			'type' => 'article',
-			'id' => $from_id."-2",
-			'title' => 'Превосходная кнопка',
-			'input_message_content' => [ 'message_text' => 'хоть какой текст' ],			
-			'description' => 'нажми лучше её'			
 		]
 	];
 	
