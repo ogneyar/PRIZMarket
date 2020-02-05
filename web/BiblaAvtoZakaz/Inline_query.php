42ee39076263716200c3e7d72cde8a4cb6ab782e
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-05 11:23:37 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Inline_query.php
+++ web/BiblaAvtoZakaz/Inline_query.php
@@ -2,25 +2,44 @@
 
 if ($inline_query) {
 
+	$InlineQueryResultVideo = [
+		[
+		// обязательные
+			'type' => 'video',
+			'id' => '',
+			'video_url' => '',
+			'mime_type' => 'video/mp4', // или 'text/html'
+			'thumb_url' => '',
+			'title' => '',
+		// необязательные
+			//'caption' => '',
+			//'parse_mode' => '',
+			//'video_width' => '',
+			//'video_height' => '',
+			//'video_duration' => '',
+			//'description' => '',
+			//'reply_markup' => '',
+			//'input_message_content' => ''
+		]
+	];
 
-$InlineQueryResultPhoto = [
-	[
-		'type' => 'photo',
-		'id' => $from_id."z",
-		'photo_url' => 'https://i.ibb.co/SRCv6Z7/file-23.jpg',
-		'thumb_url' => 'https://i.ibb.co/SRCv6Z7/file-21.jpg',
-		//'photo_width' => null,
-		//'photo_height' => null,
-		'title' => 'Продам молоток',
-		'description' => 'молотки в ассортименте',
-		'caption' => '#продам',
-		//'parse_mode' => null,
-		//'reply_markup' => null,
-		//'input_message_content' => null		
-	]	
-];
-
-
+	$InlineQueryResultPhoto = [
+		[
+			'type' => 'photo',
+			'id' => $from_id."z",
+			'photo_url' => 'https://i.ibb.co/SRCv6Z7/file-23.jpg',
+			//'thumb_url' => 'https://i.ibb.co/SRCv6Z7/file-21.jpg',
+			//'photo_width' => null,
+			//'photo_height' => null,
+			'title' => 'Продам молоток',
+			'description' => 'молотки в ассортименте',
+			'caption' => '#продам',
+			//'parse_mode' => null,
+			//'reply_markup' => null,
+			//'input_message_content' => null		
+		]	
+	];
+	
 	$InlineQueryResult = [
 		[
 			'type' => 'article',
