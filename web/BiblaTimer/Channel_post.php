1b564ffa4152a1290c36d895239f68955677ec71
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-09 15:34:17 +0300

    Add files via upload

--- web/BiblaTimer/Channel_post.php
+++ web/BiblaTimer/Channel_post.php
@@ -1,5 +1,36 @@
 <?
 
+if (strpos($text, ":")!==false) {
+
+	$komanda = strstr($text, ':', true);	
+	
+	$id = substr(strrchr($text, ":"), 1);
+	
+	$text = $komanda;
+
+}
+
+if ($id) {
+	
+	sleep($id);
+	
+	$bot->sendMessage($channel_info, "?Время");
+	
+}else {
+	
+	$дата = date("m.d.y");
+	
+	$время = date("H:i:s");
+	
+	$UNIXtime = time();
+	
+	$переведённая_дата = date("d.m.Y", $UNIXtime);
+	
+	$bot->sendMessage($channel_info, $дата);
+	
+}
+
+
 
 
 ?>
