d8dff0bf5b6796ab1092f32c497b50b44537b836
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-12 20:46:54 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Functions.php
+++ web/BiblaAvtoZakaz/Functions.php
@@ -1369,7 +1369,7 @@ function _на_публикацию() {
 		
 		_запись_в_таблицу_маркет($callback_from_id, 'date', time());
 		
-	}else $bot->answerCallbackQuery($callback_query_id, "Безоплатно можно публиковать только раз в сутки один лот !", true);
+	}else $bot->answerCallbackQuery($callback_query_id, "Безоплатно можно публиковать только раз в сутки один лот!");
 
 }
 
