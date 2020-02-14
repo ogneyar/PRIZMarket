0a54daf235db5b3a5bb0f0706587c5e366be09c9
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-14 10:44:15 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Commands.php
+++ web/BiblaAvtoZakaz/Commands.php
@@ -117,8 +117,8 @@ if ($text == 'база') {
 							if ($result->num_rows>0) {
 								$результат = $result->fetch_all(MYSQLI_ASSOC);
 								$айди_клиента = $результат[0]['id_client'];
-							}
-						}
+							}$bot->sendMessage($master, "Нет записей в таблице `zakaz_users`");
+						}else $bot->sendMessage($master, "Не смог .. `zakaz_users`");	
 						
 						if ($айди_клиента) {
 						
