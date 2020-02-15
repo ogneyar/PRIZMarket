c8973d9ac94c2770e863d4a24d63051f9e0a57c0
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-15 23:01:57 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Message.php
+++ web/BiblaAvtoZakaz/Message.php
@@ -322,6 +322,7 @@ if ($reply_to_message && $chat_id == $admin_group) {
 				
 				$text = str_replace("'", "\'", $text);
 				$text = str_replace('"', '\"', $text);
+				$text = str_replace('-', '\-', $text);
 				$text = str_replace(';', '', $text);
 				$text = str_replace('*', 'х', $text);
 				//$text = str_replace('%', '\%', $text);
