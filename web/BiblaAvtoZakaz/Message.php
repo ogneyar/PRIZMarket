603233dc044b7d0f88166e6b42e60be1f6038470
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-15 21:57:29 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Message.php
+++ web/BiblaAvtoZakaz/Message.php
@@ -322,18 +322,18 @@ if ($reply_to_message && $chat_id == $admin_group) {
 				
 				$text = str_replace("'", "\'", $text);
 				$text = str_replace('"', '\"', $text);
-				$text = str_replace(';', '\;', $text);
-				$text = str_replace('*', '\*', $text);
-				$text = str_replace('%', '\%', $text);
-				$text = str_replace('`', '\`', $text);
-				$text = str_replace('_', '\_', $text);
+				$text = str_replace(';', '', $text);
+				$text = str_replace('*', 'х', $text);
+				//$text = str_replace('%', '\%', $text);
+				$text = str_replace('`', "'", $text);
+				//$text = str_replace('_', '\_', $text);
 				$text = str_replace('|', '\|', $text);
 				//$text = str_replace('/', '\/', $text);
-				$text = str_replace('<', '\<', $text);
-				$text = str_replace('>', '\>', $text);
-				$text = str_replace('~', '\~', $text);
-				$text = str_replace('[', '\[', $text);
-				$text = str_replace(']', '\]', $text);
+				//$text = str_replace('<', '\<', $text);
+				//$text = str_replace('>', '\>', $text);
+				$text = str_replace('~', '', $text);
+				$text = str_replace('[', '(', $text);
+				$text = str_replace(']', ')', $text);
 				
 				
 				_запись_в_таблицу_маркет($from_id, 'podrobno', $text);				
