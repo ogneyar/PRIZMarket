918c2b5cfcaf825a5ff6fc0ceb8be013b71c9870
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-26 19:49:38 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Channel_post.php
+++ web/BiblaAvtoZakaz/Channel_post.php
@@ -1,49 +1,54 @@
 <?
+$number = stripos($text, '#');
 
-foreach ($entities as $ent) {
-	
-	if ($ent['type'] == 'text_mention') $user = $ent['user'];
-	
-}
+if ($number == false || $number != '0') {
 
-if ($user) {	
-		
-	$url_info = "https://t.me/check_user_infobot?start=".$user['id'];	
-	
-	_запись_в_таблицу_маркет($user['id'], 'url_info_bot', $url_info);
-
-	//$bot->sendMessage($admin_group, $url_info, null, null, null, true);		
-
-}elseif($urlCaption) {
-	
-}
-	
-$url_text = $reply_markup['inline_keyboard'][0][0]['text'];	
-	
-if ($url_text == 'Подробнее') {
-
-	$url_podrobnee = $reply_markup['inline_keyboard'][0][0]['url'];
-
-	$номер_заказа = substr(strrchr($url_podrobnee, '/'), 1);	
-/*	
-	$query ="UPDATE {$table_market} SET file_id='{$file_id}' WHERE id_zakaz={$номер_заказа}";
-		
-	$result = $mysqli->query($query);
+	if ($text != '?') {
 			
-	if ($result) {
-	
-		$bot->sendMessage($master, "обновил");
-		
-	}else $bot->sendMessage($master, "Не смог обновить запись в таблице {$table_market}");
-*/
-	
-	
-	
+		foreach ($entities as $ent) {
+			
+			if ($ent['type'] == 'text_mention') $user = $ent['user'];
+			
+		}
+
+		if ($user) {	
+				
+			$url_info = "https://t.me/check_user_infobot?start=".$user['id'];	
+			
+			_запись_в_таблицу_маркет($user['id'], 'url_info_bot', $url_info);
+
+			//$bot->sendMessage($admin_group, $url_info, null, null, null, true);		
+
+		}elseif($urlCaption) {
+			
+		}
+			
+		$url_text = $reply_markup['inline_keyboard'][0][0]['text'];	
+			
+		if ($url_text == 'Подробнее') {
+
+			$url_podrobnee = $reply_markup['inline_keyboard'][0][0]['url'];
+
+			$номер_заказа = substr(strrchr($url_podrobnee, '/'), 1);	
+		/*	
+			$query ="UPDATE {$table_market} SET file_id='{$file_id}' WHERE id_zakaz={$номер_заказа}";
+				
+			$result = $mysqli->query($query);
+					
+			if ($result) {
+			
+				$bot->sendMessage($master, "обновил");
+				
+			}else $bot->sendMessage($master, "Не смог обновить запись в таблице {$table_market}");
+		*/
+			
+			
+			
+		}
+
+	}
+
 }
-	
-
-
-
 
 
 ?>
