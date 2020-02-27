1f1d97782a3a326ce98bec914e875ee60aa01a98
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-27 23:27:29 +0300

    Add files via upload

--- web/BiblaTimer/Channel_post.php
+++ web/BiblaTimer/Channel_post.php
@@ -39,7 +39,7 @@ if ($запуск == 'старт') {
 		$UNIXtime = time();
 		$UNIXtime_Moscow = $UNIXtime + $три_часа;	
 		$время = date("H:i:s", $UNIXtime_Moscow);
-		sleep(2);
+		sleep(3);
 		$bot->sendMessage($channel_info, "#".$время);	
 	}
 	$bot->sendMessage($channel_info, "?");		
