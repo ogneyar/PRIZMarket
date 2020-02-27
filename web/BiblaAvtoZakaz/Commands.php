72abd40f2c323179bbf00cd8d579f31c0bc51162
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-27 21:04:29 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Commands.php
+++ web/BiblaAvtoZakaz/Commands.php
@@ -15,6 +15,12 @@ if ($text == 'база') {
 }elseif ($text == 'варя') {	
 	$bot->output_table('variables');		
 		
+}elseif ($text == 'удали очередь') {	
+	$query = "DELETE FROM 'variables' WHERE id_bota='1011417080'";		
+	if ($result = $mysqli->query($query)) {	
+		$bot->sendMessage($master, "Всё отлично!");		
+	}else throw new Exception("Не смог изменить таблицу 'variables'");		
+	
 }elseif ($text == 'время') {
 	$bot->sendMessage($chat_id, time());
 	
