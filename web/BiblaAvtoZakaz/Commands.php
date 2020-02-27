94044ca74328e632f2a45872aace70620714a13b
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-27 15:45:18 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Commands.php
+++ web/BiblaAvtoZakaz/Commands.php
@@ -1,29 +1,30 @@
 <?
-
 if (strpos($text, ":")!==false) {
-
-	$komanda = strstr($text, ':', true);	
-	
-	$id = substr(strrchr($text, ":"), 1);
-	
+	$komanda = strstr($text, ':', true);		
+	$id = substr(strrchr($text, ":"), 1);	
 	$text = $komanda;
-
 }
 
-
 if ($text == 'база') {
+	if ($id) {	
+		$bot->output_table($table_users, $id);	
+	}else {		
+		$bot->output_table($table_users);		
+	}	
 
+}elseif ($text == 'варя') {
+	
 	if ($id) {
 	
-		$bot->output_table($table_users, $id);
+		$bot->output_table('variables', $id);
 	
 	}else {
 		
-		$bot->output_table($table_users);
+		$bot->output_table('variables');
 		
-	}	
-
-
+	}		
+	
+	
 }elseif ($text == 'маркет') {
 	
 	if ($id) {
