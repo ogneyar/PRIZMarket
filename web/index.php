1a88c40ac81199596affebeaa3b91162ca36beb9
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-02 21:20:30 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,14 +6,14 @@
 	include_once 'site/site_03_url.html';
 	include_once 'site/site_05_body.php';
 	include_once 'site/site_09_footer.html';
-	
+/*	
 	include_once 'botMarket.php'; 
 	include_once 'botGarant.php';
 	include_once 'botZakaz.php';
 	include_once 'botInfo.php';
 	include_once 'botAvtoZakaz.php';
 	include_once 'botTimer.php';
-
+*/
 ?>
 </body>	
 </html>	
