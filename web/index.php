3efd015f65b3186e25b70fc16a33cf15f9628bf9
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-06 11:59:08 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -42,7 +42,7 @@ include_once 'site_pzm/pzmarket.php';
 			<span>PRIZM</span>arket
 		</a>
 		<span id="contact">
-			<a href="/reklama/index.php">Реклама</a>
+			<a href="/site_pzm/reklama/index.php">Реклама</a>
 			<a href="" onclick="alert('Эта функция ещё не подключена!')">Контакты</a>
 		</span>
 	</header>
