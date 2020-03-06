be7c688b979b06c4913e7cced600525c962436d0
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-06 14:05:28 +0300

    Add files via upload

--- web/site_pzm/o_prizmarket/index.php
+++ web/site_pzm/o_prizmarket/index.php
@@ -1,33 +1,33 @@
 <?php
 //include_once '../../../vendor/autoload.php';	
 include_once '../../a_conect.php';
-//include_once '../site_pzm/pzmarket.php';
+//include_once '../pzmarket.php';
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta charset="utf-8" />	
 	<title>О нас, о PRIZMarket!</title>
-	<?include_once 'site_pzm/site_files/head.html';?>
+	<?include_once '../site_files/head.html';?>
 </head>
 <body>
 	<header>
-		<?include_once 'site_pzm/site_files/header.html';?>
+		<?include_once '../site_files/header.html';?>
 	</header>
 	<nav>
-		<?include_once 'site_pzm/site_files/nav.html';?>
+		<?include_once '../site_files/nav.html';?>
 	</nav>
 	<div id="slideMenu">Моё детище, а не просто сайт!</div>
 	<div id="wrapper">
 		<div id="leftCol">		
-			<?include_once 'site_pzm/site_files/div-wrapper-leftCol-o_prizmarket.html';?>
+			<?include_once '../site_files/div-wrapper-leftCol-o_prizmarket.html';?>
 		</div>
 		<div id="rightCol">
-			<?include_once 'site_pzm/site_files/div-wrapper-rightCol.html';?>
+			<?include_once '../site_files/div-wrapper-rightCol.html';?>
 		</div>
 	</div>
 	<footer>
-		<?include_once 'site_pzm/site_files/footer.html';?>
+		<?include_once '../site_files/footer.html';?>
 	</footer>
 </body>
 </html>
