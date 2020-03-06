3a292e22ab73f4c8555997bd2f6f21ba61875fe1
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-06 12:45:17 +0300

    Update and rename header.php to header.html

--- web/site_pzm/site_files/header.php
+++
@@ -1,9 +0,0 @@
-﻿<?
-$header = "<a href='/' id='logo'>
-	<span>PRIZM</span>arket
-</a>
-<span id='contact'>
-	<a href='/site_pzm/reklama/index.php'>Реклама</a>
-	<a href=''>Контакты</a>
-</span>";
-?>
