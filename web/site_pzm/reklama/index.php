e2f6e880430fc4181e7689962a4232c2d634ad40
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-07 09:29:50 +0300

    Add files via upload

--- web/site_pzm/reklama/index.php
+++ web/site_pzm/reklama/index.php
@@ -1,34 +1,34 @@
 <?php
-//include_once '../vendor/autoload.php';	
-include_once 'a_conect.php';
-include_once 'site_pzm/pzmarket.php';
+//include_once '../../../vendor/autoload.php';	
+include_once '../../a_conect.php';
+//include_once '../pzmarket.php';
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta charset="utf-8" />	
 	<title>PRIZMarket!</title>
-	<?include_once 'site_pzm/site_files/head.html';?>
+	<?include_once '../site_files/head.html';?>
 	<style type="text/css"></style>
 </head>
 <body>
 	<header>
-		<?include_once 'site_pzm/site_files/header.html';?>
+		<?include_once '../site_files/header.html';?>
 	</header>
 	<nav>
-		<?include_once 'site_pzm/site_files/nav.html';?>
+		<?include_once '../site_files/nav.html';?>
 	</nav>
 	<div id="slideMenu">Моё детище, а не просто сайт!</div>
 	<div id="wrapper">
 		<div id="leftCol">		
-			<?include_once 'site_pzm/site_files/div-wrapper-leftCol-reklama.php';?>
+			<?include_once '../site_files/div-wrapper-leftCol-reklama.php';?>
 		</div>
 		<div id="rightCol">
-			<?include_once 'site_pzm/site_files/div-wrapper-rightCol.html';?>
+			<?include_once '../site_files/div-wrapper-rightCol.html';?>
 		</div>
 	</div>
 	<footer>
-		<?include_once 'site_pzm/site_files/footer.html';?>
+		<?include_once '../site_files/footer.html';?>
 	</footer>
 </body>
 </html>
