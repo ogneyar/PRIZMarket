6e10748d745996b7087452820bbd9ada252335b2
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-08 19:13:48 +0300

    Add files via upload

--- web/site_pzm/site_files/div-wrapper-TopCol.php
+++ web/site_pzm/site_files/div-wrapper-TopCol.php
@@ -1,6 +1,12 @@
-﻿<article>
+﻿<div id="brendPhoto">
+<article>
 	<a href='' title=''>
 		<img src="/site_pzm/img/art/PRIZMarket.jpg" />
-	</a>
+	</a>	
+</article>
+</div>
+<div id="brendName">
+<article>	
 	<h1><span>PRIZMarket</span> - Ваш рынок!</h1>
 </article>
+</div>
