4cfca490b501e15e47c49e6fdc424ea1e9ee9ffa
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-08 18:49:26 +0300

    Add files via upload

--- web/site_pzm/site_files/div-wrapper-TopCol.php
+++ web/site_pzm/site_files/div-wrapper-TopCol.php
@@ -2,5 +2,5 @@
 	<a href='' title=''>
 		<img src="/site_pzm/img/art/PRIZMarket.jpg" />
 	</a>
-	<h1><span>PRIZMarket - Ваш рынок!</span></h1>
+	<h1><span>PRIZMarket</span> - Ваш рынок!</h1>
 </article>
