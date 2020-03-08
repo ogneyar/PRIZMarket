d67357a58d56aae7547352d68bdcdc6de14963d3
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-08 20:40:39 +0300

    Add files via upload

--- web/site_pzm/site_files/div-wrapper-TopColPhoto.php
+++ web/site_pzm/site_files/div-wrapper-TopColPhoto.php
@@ -1,10 +1,5 @@
 ﻿<article>
-<div id="brendPhoto">
 	<a href='' title=''>
 		<img src="/site_pzm/img/art/PRIZMarket.jpg" />
-	</a>	
-</div>
-<div id="brendName">
-	<h1><span>PRIZMarket</span> - Ваш рынок!</h1>
-</div>
+	</a>
 </article>
--- web/site_pzm/site_files/div-wrapper-TopColText.php
+++ web/site_pzm/site_files/div-wrapper-TopColText.php
@@ -1,10 +1,3 @@
 ﻿<article>
-<div id="brendPhoto">
-	<a href='' title=''>
-		<img src="/site_pzm/img/art/PRIZMarket.jpg" />
-	</a>	
-</div>
-<div id="brendName">
 	<h1><span>PRIZMarket</span> - Ваш рынок!</h1>
-</div>
 </article>
