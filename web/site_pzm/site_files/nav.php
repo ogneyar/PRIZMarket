5703cf14dd59cba4d4b98109cb14ad24be23632e
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-10 15:42:35 +0300

    Add files via upload

--- web/site_pzm/site_files/nav.php
+++ web/site_pzm/site_files/nav.php
@@ -1,4 +1,4 @@
 ﻿<a href="/">Главная</a>
-<a href="/site_pzm/podrobnosti/index.php?podrobnosti=159634">Подробности</a> 
+<a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробности</a> 
 <a href="/site_pzm/support/index.php">Саппорт</a>
 <a href="/site_pzm/o_prizmarket/index.php">О нас</a>
