5345eb3c72d3f3e5ed58834d2495b5ca01958659
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-12 18:05:34 +0300

    Create index.php

---
+++ web/site_pzm/registraciya/index.php
@@ -0,0 +1,37 @@
+<?php
+//include_once '../../../vendor/autoload.php';	
+//include_once '../../a_conect.php';
+//include_once '../pzmarket.php';
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />	
+	<title>Регистрация на PRIZMarket!</title>
+	<?include_once '../site_files/head.php';?>
+	<style type="text/css"></style>
+</head>
+<body>
+	<header>
+		<?include_once '../site_files/header.php';?>
+	</header>
+	<nav>
+		<?include_once '../site_files/nav.php';?>
+	</nav>
+	<div id="slideMenu">Моё детище, а не просто сайт!</div>
+	<div id="wrapper">
+		<div id="TopCol">		
+			<?include_once '../site_files/wrapper-topCol.php';?>
+		</div>
+		<div id="leftCol">		
+			<?include_once '../site_files/wrapper-leftCol-registraciya.php';?>
+		</div>
+		<div id="rightCol">
+			<?include_once '../site_files/wrapper-rightCol.php';?>
+		</div>
+	</div>
+	<footer>
+		<?include_once '../site_files/footer.php';?>
+	</footer>
+</body>
+</html>
