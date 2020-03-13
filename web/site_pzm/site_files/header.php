5918957a3df6b1df1a592eae201c7f167bcfefc8
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-13 07:39:48 +0300

    Поменял местами

--- web/site_pzm/site_files/header.php
+++ web/site_pzm/site_files/header.php
@@ -2,7 +2,7 @@
 	<span>PRIZM</span>arket
 </a>
 <span id="contact">
+        <a href="/site_pzm/vhod/index.php">Вход</a>
 	<a href="/site_pzm/registraciya/index.php">Регистрация</a>
-	<a href="/site_pzm/vhod/index.php">Вход</a>
 	<!--  onclick="alert('Эта функция ещё не подключена!')" -->
 </span>
