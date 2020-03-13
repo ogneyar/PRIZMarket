890289796c94493a874a50f65064c76c3e97c815
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-13 07:41:25 +0300

    Убрал рекламу

--- web/site_pzm/site_files/nav.php
+++ web/site_pzm/site_files/nav.php
@@ -1,5 +1,5 @@
 ﻿<a href="/">Главная</a>
 <a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробности</a> 
 <a href="/site_pzm/support/index.php">ТехПоддержка</a>
-<a href="/site_pzm/reklama/index.php">Реклама</a>
+<!--<a href="/site_pzm/reklama/index.php">Реклама</a>-->
 <a href="/site_pzm/o_prizmarket/index.php">О нас</a>
