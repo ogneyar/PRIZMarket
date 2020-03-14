df57a94a79d2b3dcff3a145b39c9ca5664346c7c
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-14 21:34:35 +0300

    Add files via upload

--- web/site_pzm/site_files/nav.php
+++ web/site_pzm/site_files/nav.php
@@ -1,8 +1,12 @@
-﻿<a href="/">Главная</a>
+﻿<!--<a href="/">Главная</a>
 <a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробности</a>
 <a href="/site_pzm/support/index.php">ТехПоддержка</a>
-<a href="/site_pzm/o_prizmarket/index.php">О нас</a>
+<a href="/site_pzm/o_prizmarket/index.php">О нас</a>-->
 <ul class="topmenu">
+	<li><a href="/">Главная</a></li>
+	<li><a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробности</a></li>
+	<li><a href="/site_pzm/support/index.php">ТехПоддержка</a></li>
+	<li><a href="/site_pzm/o_prizmarket/index.php">О нас</a></li>
 	<li id="mini-menu"><a href="#">Меню</a>
 		<ul class="submenu">	
 			<li><a href="/">Главная</a></li>
