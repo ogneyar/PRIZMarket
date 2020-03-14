84f1f9ec66526297202773f14b255cd58ffea2d9
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-14 21:09:37 +0300

    Add files via upload

--- web/site_pzm/site_files/nav.php
+++ web/site_pzm/site_files/nav.php
@@ -1,27 +1,13 @@
 ﻿<a href="/">Главная</a>
-<a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробности</a> 
+<a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробности</a>
 <a href="/site_pzm/support/index.php">ТехПоддержка</a>
-<!--<a href="/site_pzm/reklama/index.php">Реклама</a>-->
 <a href="/site_pzm/o_prizmarket/index.php">О нас</a>
 <ul class="topmenu">
-	<li><a href="#">Тест</a>
+	<li id="mini-menu"><a href="#">Меню</a>
 		<ul class="submenu">	
-			<li><a href="">Менюх 1</a></li>
-			<li><a href="">Менюх 2</a></li>
+			<li><a href="/">Главная</a></li>
+			<li><a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробности</a></li>
+			<li><a href="/site_pzm/support/index.php">ТехПоддержка</a></li>
+			<li><a href="/site_pzm/o_prizmarket/index.php">О нас</a></li>
 		</ul>
 </ul>
-
-<!--
-<ul class="topmenu">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Work</a></li>
-    <li><a href="#">About<i class="fa fa-angle-down"></i></a>
-      <ul class="submenu">
-        <li><a href="">Item 1</a></li>
-        <li><a href="">Item 2</a></li>
-      </ul>
-    </li>
-    <li><a href="#">Blog</a></li>
-    <li><a href="#">Contact</a></li>
- </ul>
--->
