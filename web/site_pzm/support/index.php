62924f14fa7b39e9b60d86f3ffbd81286d8e9df7
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-14 16:58:02 +0300

    Add files via upload

--- web/site_pzm/support/index.php
+++ web/site_pzm/support/index.php
@@ -13,7 +13,7 @@ include_once '../../a_conect.php';
 	<style type="text/css">
                 @media (min-width: 700px) {
 		nav a:nth-child(3), nav#fixed a:nth-child(3) {
-			border-top: 5px solid #6accd7;
+			border-top: 5px solid rgba(255,235,59);
 		}} 
 	</style>
 	
