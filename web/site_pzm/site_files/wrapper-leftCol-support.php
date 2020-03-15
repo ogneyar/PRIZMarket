f1891b2d528315c74a5c7ff05e8e734e8a400d3c
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-15 08:34:01 +0300

    Add files via upload

--- web/site_pzm/site_files/wrapper-leftCol-support.php
+++ web/site_pzm/site_files/wrapper-leftCol-support.php
@@ -1,8 +1,8 @@
 ﻿<article id="support">
-<div>
+<p>
 	<h1><b>Команда PRIZMarket</b> трудится во благо Вам и для развития криптовалюты PRIZM.</h1><br />
 	<h3>Обращение в <b>ТехПоддержку</b> пока что возможно только через <a href="https://teleg.link/Prizm_market_supportbot" target="_blank">телеграм</a>.</h3><br />
 	<h3>Для этого перейдите по ссылке - <a href="https://teleg.link/Prizm_market_supportbot" target="_blank">Supportbot</a>.</h3><br />
 	<h3>Опишите свои трудности боту подробно, администраторы Вам обязательно помогут.</h3>
-</div>
+</p>
 </article>
