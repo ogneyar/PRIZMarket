fcb88f94b89dd13b57a255b0b97418dba704023b
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-15 20:58:23 +0300

    Add files via upload

--- web/site_pzm/site_files/head.php
+++ web/site_pzm/site_files/head.php
@@ -6,7 +6,9 @@
 	<!-- Adopt website to current screen -->
 	<meta name="viewport" content="user-scalable=yes, width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	
-	<link rel="stylesheet" href="/site_pzm/css/style.css">
+	<link rel="stylesheet" href="/site_pzm/css/style2.css">
+	
+	<link rel="stylesheet" href="/site_pzm/font-awesome/css/font-awesome.min.css">
 	
 	<!-- Here we add libs for jQuery, Ajax... -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
