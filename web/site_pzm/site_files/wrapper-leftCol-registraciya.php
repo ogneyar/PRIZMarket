c05e943bb509f4617778561d3c80b6ea6c89a21f
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-18 15:03:30 +0300

    Add files via upload

--- web/site_pzm/site_files/wrapper-leftCol-registraciya.php
+++ web/site_pzm/site_files/wrapper-leftCol-registraciya.php
@@ -1,3 +1,15 @@
 <article>
-	<h1><p>Здесь будет реализация возможности РЕГИСТРАЦИИ на сайте PRIZMarket!</p></h1>
+	<form action="save_user.php" method="post">    
+		<p>
+			<label>Ваш логин:<br></label>
+			<input name="login" type="text" size="15" maxlength="15">
+		</p>
+		<p>
+			<label>Ваш пароль:<br></label>
+			<input name="password" type="password" size="15" maxlength="15">
+		</p>
+		<p>
+			<input type="submit" name="submit" value="Зарегистрироваться">
+		</p>
+	</form>
 </article>		
