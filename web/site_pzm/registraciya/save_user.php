9da8d8112da7841a3d0ccc569ce21f6d982f2e16
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-18 15:50:36 +0300

    Add files via upload

--- web/site_pzm/registraciya/save_user.php
+++ web/site_pzm/registraciya/save_user.php
@@ -2,5 +2,57 @@
 echo $_POST['login'];
 echo '<br />';
 echo $_POST['password'];
+echo '<br />';
+echo $_POST['replay_pass'];
+echo '<br />';
+echo $_POST['e_mail'];
+?>
+
+<?php/*
+
+if (isset($_POST['login'])) { 
+	$login = $_POST['login']; 
+	if ($login == '') { 
+		unset($login);
+	} 
+} 
+//заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
+if (isset($_POST['password'])) { 
+	$password=$_POST['password']; 
+	if ($password =='') { 
+		unset($password);
+	} 
+}
+//заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем переменную
+if (empty($login) or empty($password)) {
+	//если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+    exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
+}
+//если логин и пароль введены, то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
+$login = stripslashes($login);
+$login = htmlspecialchars($login);
+$password = stripslashes($password);
+$password = htmlspecialchars($password);
+//удаляем лишние пробелы
+$login = trim($login);
+$password = trim($password);
+// подключаемся к базе
+include ("bd.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь 
+// проверка на существование пользователя с таким же логином
+$result = mysql_query("SELECT id FROM users WHERE login='$login'",$db);
+$myrow = mysql_fetch_array($result);
+if (!empty($myrow['id'])) {
+    exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
+}
+// если такого нет, то сохраняем данные
+$result2 = mysql_query ("INSERT INTO users (login,password) VALUES('$login','$password')");
+// Проверяем, есть ли ошибки
+if ($result2=='TRUE') {
+    echo "Вы успешно зарегистрированы! Теперь вы можете зайти на сайт. <a href='index.php'>Главная страница</a>";
+}else {
+    echo "Ошибка! Вы не зарегистрированы.";
+}
+	
+*/
 
 ?>
