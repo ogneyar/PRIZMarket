4db1fa1076b432f3a6cb555fa55b1cd040cb3322
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-18 21:45:48 +0300

    Add files via upload

--- web/site_pzm/site_files/header.php
+++ web/site_pzm/site_files/header.php
@@ -3,6 +3,6 @@
 </a>
 <span id="contact">
     <a href="/site_pzm/vhod/index.php">Вход</a>
-	<a href="/site_pzm/registraciya/index.php">Регистрация</a>
+	<a href="/site_pzm/registraciya/index.php?st=zero">Регистрация</a>
 	<!--  onclick="alert('Эта функция ещё не подключена!')" -->
 </span>
