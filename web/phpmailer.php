80a77a21b5d3a033efe65c9307b9fcf69b587439
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-18 22:30:06 +0300

    Add files via upload

--- web/phpmailer.php
+++ web/phpmailer.php
@@ -16,9 +16,9 @@ $mail->Username = $mail_smtp_login;   // SMTP username
 $mail->Password = $mail_smtp_pass;    // SMTP password
 $mail->SMTPSecure = 'tls';            // Enable encryption, only 'tls' is accepted
 
-$mail->From = 'support@pzmarket.ru';
+$mail->From = 'sup@pzmarket.ru';
 $mail->FromName = 'PRIZMarket';
-$mail->addAddress('ya13th@mail.ru');  // добавить получателя
+$mail->addAddress('g-u-4@mail.ru');  // добавить получателя
 
 $mail->WordWrap = 50;                 // автоматический перенос символов
 
