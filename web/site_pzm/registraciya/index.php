b009782e6c86bac3a8bd766397a7795a760503ce
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-18 22:55:52 +0300

    Add files via upload

--- web/site_pzm/registraciya/index.php
+++ web/site_pzm/registraciya/index.php
@@ -15,7 +15,17 @@ include '../../myBotApi/Variables.php';
 
 $admin_group = $admin_group_market;
 
-if ($_GET['st'] == 'zero') $bot->sendMessage($admin_group, "Кто-то желает зарегаться на сайте!");
+
+$client  = @$_SERVER['HTTP_CLIENT_IP'];
+$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+$remote  = @$_SERVER['REMOTE_ADDR'];
+ 
+if(filter_var($client, FILTER_VALIDATE_IP)) $ip = $client;
+elseif(filter_var($forward, FILTER_VALIDATE_IP)) $ip = $forward;
+else $ip = $remote;
+
+
+if ($_GET['st'] == 'zero') $bot->sendMessage($admin_group, "Кто-то желает зарегаться на сайте!\nего IP: {$ip}");
 
 ?>
 <!DOCTYPE html>
