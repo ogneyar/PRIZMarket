b58d2e4cc19c758bd1cddd6f5feb365775453929
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-18 16:07:35 +0300

    Add files via upload

--- web/site_pzm/site_files/wrapper-leftCol-vhod.php
+++ web/site_pzm/site_files/wrapper-leftCol-vhod.php
@@ -1,3 +1,31 @@
 <article>
-	<h1><p>Здесь будет реализация возможности ВХОДа зарегестрированных пользователей на сайт PRIZMarket!</p></h1>
-</article>		
+	<?php// session_start(); ?>
+	
+<form action="/site_pzm/vhod/testreg.php" method="post">
+<p>
+	<label>Ваш логин:<br></label>
+	<input name="login" type="text" size="15" maxlength="15">
+</p>
+<p>
+    <label>Ваш пароль:<br></label>
+    <input name="password" type="password" size="15" maxlength="15">
+</p>
+<p>
+    <input type="submit" name="submit" value="Войти">
+	<br>
+ 
+	<a href="/site_pzm/registraciya/index.php">Зарегистрироваться</a> 
+</p>
+</form>
+<br>
+
+<?php
+// Проверяем, пусты ли переменные логина и id пользователя
+if (empty($_SESSION['login']) or empty($_SESSION['id'])) {
+    // Если пусты, то мы не выводим ссылку
+    echo "Вы вошли на сайт, как гость<br><a href='#'>Эта ссылка  доступна только зарегистрированным пользователям</a>";
+}else {
+	// Если не пусты, то мы выводим ссылку
+	echo "Вы вошли на сайт, как ".$_SESSION['login']."<br><a  href='http://tvpavlovsk.sk6.ru/'>Эта ссылка доступна только  зарегистрированным пользователям</a>";
+}
+?>
