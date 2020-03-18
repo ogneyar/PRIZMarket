d1741b095f237548c426dab3eab98f986b61729a
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-18 22:12:22 +0300

    Add files via upload

--- web/pzm.php
+++ web/pzm.php
@@ -1,54 +1,36 @@
-﻿<?php	
-// Подключаем библиотеку с классом Bot
-include_once 'myBotApi/Bot.php';
-//exit('ok');
-$token = $tokenMARKET;
-// Создаем объект бота
-$bot = new Bot($token);
+﻿<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$id_bota = strstr($token, ':', true);	
-// ПОДКЛЮЧЕНИЕ ВСЕХ ОСНОВНЫХ ПЕРЕМЕННЫХ
-include 'myBotApi/Variables.php';
+include_once '../vendor/autoload.php';
+include_once 'a_conect.php';
 
-$admin_group = $admin_group_market;
 
-if ($_GET['st'] == 'zero') $bot->sendMessage($admin_group, "йуу");
+$mail = new PHPMailer;
 
-/*
-$mysqli = new mysqli($host, $username, $password, $dbname);
+$mail->isSMTP();                      // Set mailer to use SMTP
+$mail->Host = $mail_smtp_server;      // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;               // Enable SMTP authentication
+$mail->Username = $mail_smtp_login;   // SMTP username
+$mail->Password = $mail_smtp_pass;    // SMTP password
+$mail->SMTPSecure = 'tls';            // Enable encryption, only 'tls' is accepted
 
-// проверка подключения 
-if (mysqli_connect_errno()) {
-	throw new Exception('Чёт не выходит подключиться к MySQL');	
+$mail->From = 'support@pzmarket.ru';
+$mail->FromName = 'PRIZMarket';
+$mail->addAddress('ya13th@mail.ru');  // добавить получателя
+
+$mail->WordWrap = 50;                 // автоматический перенос символов
+
+$mail->Subject = 'Hello';
+$mail->Body    = 'Testing some Mailgun awesomness';
+
+if(!$mail->send()) {
+    echo 'Не смог отправить сообщение.';
+    echo 'Ошибка: ' . $mail->ErrorInfo;
+} else {
+    echo 'Сообщение отправлено!</br></br>';
 	exit('ok');
 }
 
-// Обработчик исключений
-set_exception_handler('exception_handler');
-
-$ссылка_на_амазон = "https://{$aws_bucket}.s3.{$aws_region}.amazonaws.com/";
-
-$показ_одного_лота = '';
-
-$запрос = "SELECT * FROM pzmarkt"; 
-$результат = $mysqli->query($запрос);
-if ($результат)	{
-	$i = $результат->num_rows;
-}else throw new Exception('Не смог проверить таблицу `pzm`.. (работа сайта)');	
-
-if($i>0) $arrS = $результат->fetch_all();		
-else throw new Exception('Таблица пуста.. (работа сайта)');
-		
-
-// закрываем подключение 
-$mysqli->close();
-
-// при возникновении исключения вызывается эта функция
-function exception_handler($exception) {
-	global $mysqli;
-	echo "Ошибка! ".$exception->getCode()." ".$exception->getMessage();	  
-	$mysqli->close();		
-	exit('ok');  	
-}
-*/
-?>		
+?>
