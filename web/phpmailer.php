fcfe225e044cd7357b48a213134afb79818941ef
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-19 23:15:02 +0300

    Add files via upload

---
+++ web/phpmailer.php
@@ -0,0 +1,37 @@
+﻿<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+include_once '../../vendor/autoload.php';
+include_once '../a_conect.php';
+
+$mail = new PHPMailer;
+
+$mail->isSMTP();                      // Set mailer to use SMTP
+$mail->Host = $mail_smtp_server;      // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;               // Enable SMTP authentication
+$mail->Username = $mail_smtp_login;   // SMTP username
+$mail->Password = $mail_smtp_pass;    // SMTP password
+$mail->SMTPSecure = 'tls';            // Enable encryption, only 'tls' is accepted
+
+$mail->From = 'sup@pzmarket.ru';
+$mail->FromName = 'PRIZMarket';
+$mail->addAddress('ya13th@mail.ru');  // добавить получателя
+
+$mail->WordWrap = 50;                 // автоматический перенос символов
+
+$mail->Subject = 'Hello';
+$mail->Body    = 'Testing some Mailgun awesomness';
+
+if(!$mail->send()) {
+	$bot->sendMessage($admin_group, "Не смог отправить сообщение.");
+    echo 'Не смог отправить сообщение.';
+    echo 'Ошибка: ' . $mail->ErrorInfo;
+	//exit('ok');
+} else {
+	$bot->sendMessage($admin_group, "Сообщение отправлено!");
+    echo 'Сообщение отправлено!';
+}
+
+?>
