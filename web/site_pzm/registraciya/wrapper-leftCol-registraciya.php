4f692bc71ad7dc4694f5af88cf36f5b190716908
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-19 19:38:06 +0300

    Add files via upload

--- web/site_pzm/registraciya/wrapper-leftCol-registraciya.php
+++ web/site_pzm/registraciya/wrapper-leftCol-registraciya.php
@@ -3,19 +3,19 @@
 		<br>
 		<p>
 			<label>Введите логин:<br></label>
-			<input type="text" name="login" id="login" size="15" maxlength="15">
+			<input type="text" placeholder="Ваш логин" name="login" id="login" size="15" maxlength="15">
 		</p>
 		<p>
 			<label>Введите пароль:<br></label>
-			<input type="password" name="password" id="password" size="15" maxlength="15">
+			<input type="password" placeholder="Ваш пароль" name="password" id="password" size="15" maxlength="15">
 		</p>
 		<p>
 			<label>Повторите пароль:<br></label>
-			<input type="password" name="password2" id="password2" size="15" maxlength="15">
+			<input type="password" placeholder="Повторите пароль" name="password2" id="password2" size="15" maxlength="15">
 		</p>
 		<p>
 			<label>Ваш e-mail:<br></label>
-			<input type="text" name="email" id="email" size="15" maxlength="30">
+			<input type="text" placeholder="Ваш e-mail" name="email" id="email" size="15" maxlength="30">
 		</p>
 		<p>
 			<input type="button" name="done" id="done" value="Зарегистрироваться">
