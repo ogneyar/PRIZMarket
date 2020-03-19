63ae0c5dca63b6c3d6dda2efde27fc50ec61d199
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-19 18:48:54 +0300

    Add files via upload

---
+++ web/site_pzm/registraciya/wrapper-leftCol-registraciya.php
@@ -0,0 +1,25 @@
+<article id="registr">
+	<form action="/site_pzm/registraciya/save_user.php" method="post">    
+		<br>
+		<p>
+			<label>Введите логин:<br></label>
+			<input name="login" type="text" size="15" maxlength="15">
+		</p>
+		<p>
+			<label>Введите пароль:<br></label>
+			<input name="password" type="password" size="15" maxlength="15">
+		</p>
+		<p>
+			<label>Повторите пароль:<br></label>
+			<input name="replay_pass" type="password" size="15" maxlength="15">
+		</p>
+		<p>
+			<label>Ваш e-mail:<br></label>
+			<input name="e_mail" type="text" size="15" maxlength="30">
+		</p>
+		<p>
+			<input type="submit" name="submit" value="Зарегистрироваться">
+		</p>
+		<br><br>
+	</form>
+</article>		
