eca2945d9ede4eadf23b565175631c6f4bfa2bdb
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-19 19:34:42 +0300

    Add files via upload

--- web/site_pzm/registraciya/wrapper-leftCol-registraciya.php
+++ web/site_pzm/registraciya/wrapper-leftCol-registraciya.php
@@ -1,25 +1,25 @@
 <article id="registr">
-	<form action="/site_pzm/registraciya/save_user.php" method="post">    
+<!--	<form action="/site_pzm/registraciya/save_user.php" method="post">    -->
 		<br>
 		<p>
 			<label>Введите логин:<br></label>
-			<input name="login" type="text" size="15" maxlength="15">
+			<input type="text" name="login" id="login" size="15" maxlength="15">
 		</p>
 		<p>
 			<label>Введите пароль:<br></label>
-			<input name="password" type="password" size="15" maxlength="15">
+			<input type="password" name="password" id="password" size="15" maxlength="15">
 		</p>
 		<p>
 			<label>Повторите пароль:<br></label>
-			<input name="replay_pass" type="password" size="15" maxlength="15">
+			<input type="password" name="password2" id="password2" size="15" maxlength="15">
 		</p>
 		<p>
 			<label>Ваш e-mail:<br></label>
-			<input name="e_mail" type="text" size="15" maxlength="30">
+			<input type="text" name="email" id="email" size="15" maxlength="30">
 		</p>
 		<p>
-			<input type="submit" name="submit" value="Зарегистрироваться">
+			<input type="button" name="done" id="done" value="Зарегистрироваться">
 		</p>
 		<br><br>
-	</form>
+<!--	</form>-->
 </article>		
