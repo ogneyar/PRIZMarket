f175aca5e83db76f79fb1e2c161ad33b4d29b1b1
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-19 18:54:48 +0300

    Delete wrapper-leftCol-support.php

--- web/site_pzm/site_files/wrapper-leftCol-support.php
+++
@@ -1,8 +0,0 @@
-﻿<article id="support">
-<div>
-	<h1><p><b>Команда PRIZMarket</b> трудится во благо Вам и для развития криптовалюты PRIZM.</p></h1><br />
-	<h3><p>Обращение в <b>ТехПоддержку</b> пока что возможно только через <a href="https://teleg.link/Prizm_market_supportbot" target="_blank">телеграм</a>.</p></h3><br />
-	<h3><p>Для этого перейдите по ссылке - <a href="https://teleg.link/Prizm_market_supportbot" target="_blank">Supportbot</a>.</p></h3><br />
-	<h3><p>Опишите свои трудности боту подробно, администраторы Вам обязательно помогут.</p></h3>
-</div>
-</article>
