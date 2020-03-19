3ef7ae5f39c0c1c5f23cc641098018ec9b8d0dde
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-19 21:53:46 +0300

    Add files via upload

--- web/site_pzm/registraciya/save_user.php
+++ web/site_pzm/registraciya/save_user.php
@@ -1,6 +1,17 @@
 <?
 echo $_POST['email'];
 
+include_once '../../a_conect.php';
+include_once '../../myBotApi/Bot.php';
+//exit('ok');
+$token = $tokenMARKET;
+$bot = new Bot($token);
+$id_bota = strstr($token, ':', true);	
+include '../../myBotApi/Variables.php';
+$admin_group = $admin_group_market;
+
+if ($_POST['email']) $bot->sendMessage($admin_group, $_POST['email']);
+
 $логин = htmlspecialchars($_POST['login']);
 $пароль = htmlspecialchars($_POST['password']);
 $емаил = htmlspecialchars($_POST['email']);
