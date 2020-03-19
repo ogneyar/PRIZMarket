f6db08512f64318ebcb14dd81fc46924c35628c3
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-19 22:32:02 +0300

    Add files via upload

--- web/site_pzm/registraciya/index.php
+++ web/site_pzm/registraciya/index.php
@@ -30,7 +30,7 @@ if ($_GET['st'] == 'zero') $bot->sendMessage($admin_group, "Кто-то жела
 	<script>
 		$(document).ready (function (){
 			$("#done").click (function (){
-				$('#warning').html ('' + "<br>");
+				$('#warning').html (' ' + "<br>");
 				$('#warning').show ();
 				var login = $("#login").val ();
 				var password = $("#password").val ();
