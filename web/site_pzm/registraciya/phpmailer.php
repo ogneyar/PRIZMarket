02db256b7ae5f3f7b69e915292c6659679b306d2
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-22 15:54:48 +0300

    Add files via upload

--- web/site_pzm/registraciya/phpmailer.php
+++ web/site_pzm/registraciya/phpmailer.php
@@ -10,7 +10,7 @@ $mail = new PHPMailer;
 
 $mail->isSMTP();                      // Set mailer to use SMTP
 $mail->Host = $smtp_server;      // Specify main and backup SMTP servers
-$mail->Port = $smtp_port;
+//$mail->Port = $smtp_port;
 $mail->SMTPAuth = true;               // Enable SMTP authentication
 $mail->Username = $smtp_login;   // SMTP username
 $mail->Password = $smtp_pass;    // SMTP password
@@ -19,7 +19,7 @@ $mail->SMTPSecure = 'tls';            // Enable encryption, only 'tls' is accept
 $mail->CharSet = "utf-8";
 $mail->IsHTML(true); 
 
-$mail->From = 'support@prizmarket.ru';
+$mail->From = 'prizmarket@rambler.ru';
 $mail->FromName = 'PRIZMarket';
 $mail->addAddress($емаил);  // добавить получателя
 
