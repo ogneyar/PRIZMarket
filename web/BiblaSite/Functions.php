7d4113260f2a3675b5c9c235e9f627491a5c01e1
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-23 15:13:35 +0300

    Add files via upload

---
+++ web/BiblaSite/Functions.php
@@ -0,0 +1,29 @@
+﻿<?
+/* Список всех функций:
+**
+** exception_handler	-	// функция отлова исключений
+**
+** ----------------------
+** _старт_СайтБота
+** ----------------------
+**
+*/
+
+// при возникновении исключения вызывается эта функция
+function exception_handler($exception) {
+	global $bot, $master;	
+	$bot->sendMessage($master, "Ошибка! ".$exception->getCode()." ".$exception->getMessage());	  
+	exit('ok');  	
+}
+
+// функция старта бота ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЯХ
+function _старт_СайтБота() {		
+	global $bot, $table_users, $chat_id, $callback_from_first_name, $from_first_name, $HideKeyboard;	
+	
+	$bot->sendMessage($chat_id, "Добро пожаловать, *".$callback_from_first_name."*!", markdown, $HideKeyboard);	
+	exit('ok');	
+}
+
+
+
+?>
