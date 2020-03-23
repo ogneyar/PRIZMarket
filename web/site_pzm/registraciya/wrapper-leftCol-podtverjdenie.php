53bdc3fbc06c8763c511a5bd3b67d820509801f8
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-23 21:28:05 +0300

    Add files via upload

--- web/site_pzm/registraciya/wrapper-leftCol-podtverjdenie.php
+++ web/site_pzm/registraciya/wrapper-leftCol-podtverjdenie.php
@@ -1,4 +1,9 @@
-<article id="podtverjdenie">
-	<br>
-	<p>Хорошечненько)</p>
+<article id="podtverjdenie">	
+	<form action="/site_pzm/vhod/index.php">
+		<br>
+		<p>
+			<label>Регистрация пройдена успешно!<br></label>			
+			<input type="button" value="Вход">
+		</p>
+	</form>
 </article>		
