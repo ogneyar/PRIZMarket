9e0e16a9104970282b41930a55cd55ad11a7f323
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-23 21:54:09 +0300

    Add files via upload

--- web/BiblaSite/Commands.php
+++ web/BiblaSite/Commands.php
@@ -13,10 +13,12 @@ if ($text == 'база') {
 	}	
 	
 }elseif ($text == 'удали') {
+	$bot->sendMessage($admin_group, "ttt");
 	$query = "DELETE FROM {$table_users} WHERE login='{$id}'";				
 	if ($result = $mysqli->query($query)) {					
 		$bot->sendMessage($admin_group, "Удаление из БД совершенно!");
-	}else $bot->sendMessage($admin_group, "Не получается удалить строку из БД");	
+	}else $bot->sendMessage($admin_group, "Не получается удалить строку из БД");
+	$bot->sendMessage($admin_group, "еее");
 	
 }
 
