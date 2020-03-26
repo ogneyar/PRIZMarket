1a57d3c4fe2900c0d8f1a5f1a3847427b2926b2c
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-26 21:26:40 +0300

    Add files via upload

---
+++ web/site_pzm/lk/lk.php
@@ -0,0 +1,13 @@
+﻿<?
+$вывод = "";
+if ($_COOKIE['login']) $вывод = $_COOKIE['login'];
+
+$login_json = json_encode($вывод);
+?>
+<script>
+	$(document).ready (function (){				
+		$('#client').html (<?echo $login_json;?>);
+		$('#client').show ();
+	});
+</script>
+<label id="client"></label>
