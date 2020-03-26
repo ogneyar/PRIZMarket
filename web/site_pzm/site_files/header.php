e8d8a9a7093b3fbb199681ff728e025ccb56878d
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-26 15:15:19 +0300

    Add files via upload

--- web/site_pzm/site_files/header.php
+++ web/site_pzm/site_files/header.php
@@ -2,9 +2,9 @@
 	<b><span>PRIZMarket</span></b>
 </a>
 <span id="contact">
+	<label id="login"></label>
     <a href="/site_pzm/vhod/index.php">Вход</a>
 	<a href="/site_pzm/registraciya/index.php?st=zero">Регистрация</a>
-	<!--  onclick="alert('Эта функция ещё не подключена!')" -->
-	<label id="login"></label>
+	<!--  onclick="alert('Эта функция ещё не подключена!')" -->	
 </span>
 
