8cbb993b4bc2bd5695527c7e350afa6638e4dfa1
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-29 20:48:52 +0300

    Add files via upload

--- web/site_pzm/vhod/vhod.php
+++ web/site_pzm/vhod/vhod.php
@@ -3,7 +3,7 @@ if ($_POST['login']) {
 	
 	setcookie("login", $_POST['login'], time()+60*60*24*365, '/');
 	
-	echo "Добро пожаловать ". $_POST['login'] ."!";	
+	echo "<p><label>Добро пожаловать ". $_POST['login'] ."!</label></p>";	
 	
 }else echo "Не получилось войти(";
 ?>
