96fbd6de8769d5b35b159e26c5c073b8da10d8ea
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-31 15:24:58 +0300

    Update head.php

--- web/site_pzm/site_files/head.php
+++ web/site_pzm/site_files/head.php
@@ -6,7 +6,7 @@
 	<!-- Adopt website to current screen -->
 	<meta name="viewport" content="user-scalable=yes, width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	
-	<link rel="stylesheet" href="/site_pzm/css/style.css">
+	<link rel="stylesheet" href="/site_pzm/css/style2.css">
 	
 	<link rel="stylesheet" href="/site_pzm/font-awesome/css/font-awesome.min.css">
 	
