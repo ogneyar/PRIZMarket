a2c78cbb2b3109e30ad5c898f93013ace8bcd1cd
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-03-31 16:43:08 +0300

    Add files via upload

--- web/site_pzm/site_files/header.php
+++ web/site_pzm/site_files/header.php
@@ -1,23 +1,3 @@
 <a href="/" id="logo">
 	<b><span>PRIZMarket</span></b>
 </a>
-<div id="lk_menu">
-		
-	<span id="contact">
-		<a href="/site_pzm/vhod/index.php">Вход</a>
-		<a href="/site_pzm/registraciya/index.php?st=zero">Регистрация</a>
-	</span>
-
-	<ul class="lk_topmenu">
-		<li id="lk_mini-menu">
-			<a href="#"><label id="client"></label></a>
-			
-			<ul class="lk_submenu">	
-				<li><a href="/site_pzm/lk/index.php">Личный Кабинет</a></li>
-				<li><a href="/site_pzm/lk/exit.php">Выход</a></li>			
-			</ul>
-			
-		</li>
-	</ul>
-
-</div>
