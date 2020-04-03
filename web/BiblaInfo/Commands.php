cca35234f7f1ecc9d1ffeb1e24d41fa603f64a2b
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-03 17:22:37 +0300

    Еее

--- web/BiblaInfo/Commands.php
+++ web/BiblaInfo/Commands.php
@@ -24,7 +24,7 @@ if ($text == 'база') {
 
 
 }elseif ($text == 'бан') {	
-	$query = "UPDATE ".$table_users." SET status='ban' WHERE user_name=".$id;
+	$query = "UPDATE ".$table_users." SET status='ban' WHERE user_name='{$id}';
 	if ($result = $mysqli->query($query)) {	
 		$bot->sendMessage($master, "Всё отлично!");		
 	}else throw new Exception("Не смог изменить таблицу {$table_users}");		
