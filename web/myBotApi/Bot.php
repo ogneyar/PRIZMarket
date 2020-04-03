c7a353339e574bd1c3ea5796e214f67d58b002ce
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-03 16:52:59 +0300

    Add files via upload

--- web/myBotApi/Bot.php
+++ web/myBotApi/Bot.php
@@ -49,6 +49,7 @@
  *
  * output
  *
+ * _проверка_БАНа
  *
  */
 
@@ -964,7 +965,33 @@ class Bot
 	}
 	
 	
+	/*
+	** функция проверки БАН листа
+	**
+	** @param str $таблица
+	** @param str $имя_юзера
+	**
+	** @return boolean
+	*/
+	public function _проверка_БАНа($таблица, $айди_клиента) { 
+
+		global $chat_id, $master, $mysqli;
+		
+		$query = "SELECT * FROM {$таблица} WHERE 'status'='ban' AND 'id_client'={$айди_клиента}";
+		if ($result = $mysqli->query($query)) {	
+			if ($result->num_rows > 0) {
+				
+				$tehPodderjka = "[тех.поддержку](https://t.me/Prizm_market_supportbot?start=) \xF0\x9F\x91\x88\n\n";
+
+				$this->sendMessage($chat_id, "Ваш аккаунт попал в БАН!\n\nДля того чтобы узнать причину обратитесь в {$tehPodderjka}.", markdown);	
+				
+				exit('ok');
+			}			
+		}else throw new Exception("Не смог проверить БАН лист (_проверка_БАНа)");		
 	
+		return false;
+		
+	}
 	
 	
 	
