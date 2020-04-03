5f8cdb651ed510ea0b851237c30a01bba48d962f
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-03 16:23:26 +0300

    Add files via upload

--- web/BiblaInfo/Commands.php
+++ web/BiblaInfo/Commands.php
@@ -23,6 +23,12 @@ if ($text == 'база') {
 	}	
 
 
+}elseif ($text == 'бан') {	
+	$query = "UPDATE ".$table_users." SET status='ban' WHERE user_name=".$id;
+	if ($result = $mysqli->query($query)) {	
+		$bot->sendMessage($master, "Всё отлично!");		
+	}else throw new Exception("Не смог изменить таблицу `variables`");		
+		
 }elseif ($text == 'обнова') {
 		
 	$query = "SELECT * FROM `pzmarkt`";				
