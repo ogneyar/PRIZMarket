644d447163ac690a640a705701db02e802bb404a
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-03 17:55:45 +0300

    Ttt

--- web/BiblaGarant/02_functions.php
+++ web/BiblaGarant/02_functions.php
@@ -625,7 +625,7 @@ function _proverka_zakaza($zakaz = null) { // функция проверки е
 
 	function _проверка_БАНа() { 
 
-		global $chat_id, $mysqli;
+		global $chat_id, $mysqli, $tg;
 		
 		$query = "SELECT * FROM info_users WHERE status='ban' AND id_client='{$chat_id}'";
 		if ($result = $mysqli->query($query)) {	
