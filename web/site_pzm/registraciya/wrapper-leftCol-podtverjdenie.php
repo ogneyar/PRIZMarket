b2fd6cc4edb712eb28bed9aaa2c27de7e61a4481
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-05 09:36:21 +0300

    Add files via upload

--- web/site_pzm/registraciya/wrapper-leftCol-podtverjdenie.php
+++ web/site_pzm/registraciya/wrapper-leftCol-podtverjdenie.php
@@ -7,11 +7,11 @@
 	<br>
 	<p>
 	<form action="/site_pzm/registraciya/input_svyazi.php" align="center">
-		<input type="submit" name="telegram" id="telegram"  value="Telegram">
+		<input type="submit" name="svyazi" id="telegram"  value="Telegram">
 		&nbsp;&nbsp;
-		<input type="submit" name="whatsup" id="whatsup"  value="Whatsup">
+		<input type="submit" name="svyazi" id="whatsup"  value="Whatsup">
 		&nbsp;&nbsp;
-		<input type="submit" name="wiber" id="wiber"  value="Wiber">
+		<input type="submit" name="svyazi" id="wiber"  value="Wiber">
 	</form>
 	</p>
 </article>		
