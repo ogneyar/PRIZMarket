43bdc7d8443809369411881f1a6fb532b72e9e94
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-11 16:51:15 +0300

    Add files via upload

--- web/site_pzm/lk/sozdanie.php
+++ web/site_pzm/lk/sozdanie.php
@@ -36,10 +36,10 @@ else $json_login = json_encode($_COOKIE['login']);
 				else if (currency.length < 4) fail = "Валюта не менее 4х символов";		
 				else if (hesh_city.length < 4) fail = "Хештеги не менее 4х символов";*/
 								
-				file = $("#file").val();				
+				file = $("#file").files[0];				
 				alert(`File name: ${file.name}`);
 				
-				if (${file.name} == 'undefined') {
+				if (file.name == 'undefined') {
 					fail = "Не выбран файл";
 					alert(`File name: ${file.name}`);					
 				}else if (opisanie.length < 4) fail = "Описание не менее 4х символов";
