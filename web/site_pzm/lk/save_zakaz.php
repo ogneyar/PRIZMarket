c6e6618609f16a9b74086ee2650c750183f09d3d
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-13 18:24:58 +0300

    Add files via upload

--- web/site_pzm/lk/save_zakaz.php
+++ web/site_pzm/lk/save_zakaz.php
@@ -1,7 +1,9 @@
 <?
 
 ?>
+<!--
 <form action="/site_pzm/lk/sozdanie.php" method="post" enctype="multipart/form-data">
+-->
 	<input type="hidden" name="photo" value="1">
 	<br><br>	
 	
@@ -16,13 +18,15 @@
 	<br><br><br>
 	
 	<input type="submit" class="button" name="done_files" id="done_files" value="Применить">
+<!--
 </form>
+-->
 
 	<script>
-	(function (){	
+	$(document).ready (function (){	
 		$("#done_files").click (function (){
 			$('#lk').html ("<br><h4>Идёт загрузка..</h4>");
 			$('#lk').show ();
 		});
-	}) (jQuery)
+	});
 	</script>
