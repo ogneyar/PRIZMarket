b85313a39dc475acdddc0e0c59ae9364c696c5e5
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-16 15:19:24 +0300

    Add files via upload

--- web/site_pzm/lk/wrapper-leftCol-sozdanie.php
+++ web/site_pzm/lk/wrapper-leftCol-sozdanie.php
@@ -10,27 +10,43 @@
 				<option value="#куплю" id="hesh_k">#куплю</option>
 			</select>
 		<br><br>		
-			<label>Введите название товара/услуги:<br></label>
+			<label>Введите название товара/услуги: *<br></label>
 			<input type="text" placeholder="Название" name="name" id="name" size="15" maxlength="100">
 		<br><br>		
-			<label>Ссылка вшитая в названии:<br></label>
+			<label>Ссылка вшитая в названии: (не обязательно)<br></label>
 			<input type="text" placeholder="Ссылка, если нужна." name="link_name" id="link_name" size="20" maxlength="200">
 		<br><br>		
 			<label>Категория товара/услуги:<br></label>
-			<input type="text" placeholder="Категория" name="hesh_kateg" id="hesh_kateg" size="15" maxlength="50">
+			<select name="hesh_kateg" id="hesh_kateg">			
+				<option value="#недвижимость">#недвижимость</option>
+				<option value="#работа">#работа</option>
+				<option value="#транспорт">#транспорт</option>
+				<option value="#услуги" selected>#услуги</option>
+				<option value="#личные_вещи">#личные_вещи</option>
+				<option value="#для_дома_и_дачи">#для_дома_и_дачи</option>
+				<option value="#бытовая_электроника">#бытовая_электроника</option>
+				<option value="#животные">#животные</option>
+				<option value="#хобби_и_отдых">#хобби_и_отдых</option>
+				<option value="#для_бизнеса">#для_бизнеса</option>
+				<option value="#продукты_питания">#продукты_питания</option>
+				<option value="#красота_и_здоровье">#красота_и_здоровье</option>
+			</select>
+			
+		<!--	<input type="text" placeholder="Категория" name="hesh_kateg" id="hesh_kateg" size="15" maxlength="50"> -->
+			
 		<br><br>
-			<label>Валюта, помимо PRIZM:<br></label>
+			<label>Валюта, помимо PRIZM: (не обязательно)<br></label>
 			<input type="text" placeholder="Валютебл" name="currency" id="currency" size="15" maxlength="100">
 		<br><br>
-			<label>Хештеги местонахождения:<br></label>
+			<label>Хештеги местонахождения: *<br></label>
 			<input type="text" placeholder="Хештеги города" name="hesh_city" id="hesh_city" size="15" maxlength="100">
 		<br><br>
 		
-			<label>Загрузите фото:<br></label>
+			<label>Загрузите фото: *<br></label>
 			<input type="file" name="file" id="file" accept=".jpg, .jpeg, .png">	
 		<br><br>
 		
-			<label>Введите описание:<br></label>
+			<label>Введите описание: *<br></label>
 			<textarea name="opisanie" id="opisanie" maxlength="500" rows="7" cols="25"></textarea>
 		<br><br>
 		
