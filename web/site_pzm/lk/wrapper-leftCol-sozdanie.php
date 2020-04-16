cfbd9e2f865097e4988f7fe7e08b005d501c89d6
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-16 15:40:55 +0300

    Add files via upload

--- web/site_pzm/lk/wrapper-leftCol-sozdanie.php
+++ web/site_pzm/lk/wrapper-leftCol-sozdanie.php
@@ -4,19 +4,19 @@
 		<br>		
 			<input type="hidden" name="login" id="login" value="<?=$_COOKIE['login'];?>">
 			
-			<label>Выберите действие:<br></label>			
+			<label>Выберите действие: <font color="red">*</font><br></label>			
 			<select name="hesh_pk" id="hesh_pk">			
 				<option value="#продаю" id="hesh_p" selected>#продаю</option>
 				<option value="#куплю" id="hesh_k">#куплю</option>
 			</select>
 		<br><br>		
-			<label>Введите название товара/услуги: <i color="red">*</i><br></label>
+			<label>Введите название товара/услуги: <font color="red">*</font><br></label>
 			<input type="text" placeholder="Название" name="name" id="name" size="15" maxlength="100">
 		<br><br>		
 			<label>Ссылка вшитая в названии:<br></label>
 			<input type="text" placeholder="Ссылка, если нужна." name="link_name" id="link_name" size="20" maxlength="200">
 		<br><br>		
-			<label>Категория товара/услуги: *<br></label>
+			<label>Категория товара/услуги: <font color="red">*</font><br></label>
 			<select name="hesh_kateg" id="hesh_kateg">			
 				<option value="#недвижимость">#недвижимость</option>
 				<option value="#работа">#работа</option>
@@ -38,15 +38,15 @@
 			<label>Валюта, помимо PRIZM:<br></label>
 			<input type="text" placeholder="Валютебл" name="currency" id="currency" size="15" maxlength="100">
 		<br><br>
-			<label>Хештеги местонахождения: *<br></label>
+			<label>Хештеги местонахождения: <font color="red">*</font><br></label>
 			<input type="text" placeholder="Хештеги города" name="hesh_city" id="hesh_city" size="15" maxlength="100">
 		<br><br>
 		
-			<label>Загрузите фото: *<br></label>
+			<label>Загрузите фото: <font color="red">*</font><br></label>
 			<input type="file" name="file" id="file" accept=".jpg, .jpeg, .png">	
 		<br><br>
 		
-			<label>Введите описание: *<br></label>
+			<label>Введите описание: <font color="red">*</font><br></label>
 			<textarea name="opisanie" id="opisanie" maxlength="500" rows="7" cols="25"></textarea>
 		<br><br>
 		
