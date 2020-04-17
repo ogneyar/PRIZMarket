e8e36dd7f30a0ed0118d6162313df7c8b0d62ea3
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-17 16:09:50 +0300

    Add files via upload

--- web/site_pzm/lk/sozdanie.php
+++ web/site_pzm/lk/sozdanie.php
@@ -1,12 +1,12 @@
 <?php
 include_once '../../a_conect.php';
-include_once '../../myBotApi/Bot.php';
+/*include_once '../../myBotApi/Bot.php';
 //exit('ok');
 $token = $tokenSite;
 $bot = new Bot($token);
 $id_bota = strstr($token, ':', true);	
 include '../../myBotApi/Variables.php';
-$admin_group = $admin_group_Site;
+$admin_group = $admin_group_Site;*/
 //$админка = $admin_group;
 //$мастер = $master;
 
