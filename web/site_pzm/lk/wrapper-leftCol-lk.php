349342037b0ea522b44c089b6c7ea510cb67d3bd
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-18 14:42:03 +0300

    Add files via upload

--- web/site_pzm/lk/wrapper-leftCol-lk.php
+++ web/site_pzm/lk/wrapper-leftCol-lk.php
@@ -9,7 +9,7 @@
 <br>
 <script async src="https://telegram.org/js/telegram-widget.js?8" data-telegram-share-url="https://prizmarket.herokuapp.com" data-size="large" data-text="notext"></script>
 <br>
-<script async src="https://telegram.org/js/telegram-widget.js?8" data-telegram-login="samplebot" data-size="medium" data-userpic="false" data-onauth="onTelegramAuth(user)"></script>
+<script async src="https://telegram.org/js/telegram-widget.js?8" data-telegram-login="TesterSiteBot" data-size="medium" data-userpic="false" data-onauth="onTelegramAuth(user)"></script>
 <script type="text/javascript">
   function onTelegramAuth(user) {
     alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
