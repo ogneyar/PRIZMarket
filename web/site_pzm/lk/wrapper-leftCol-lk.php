4b4f8a998caeac9a93772f8a916df8ca36459af7
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-18 14:30:32 +0300

    Add files via upload

--- web/site_pzm/lk/wrapper-leftCol-lk.php
+++ web/site_pzm/lk/wrapper-leftCol-lk.php
@@ -1,9 +1,9 @@
-﻿<script async src="https://telegram.org/js/telegram-widget.js?8" data-telegram-share-url="https://core.telegram.org/widgets/share" data-size="large" data-text="notext"></script>
-<article id="lk">
+﻿<article id="lk">
 	<h4>
 		<br>
 		<label>Здесь будет реализована возможность изменения никнейма клиента, его пароля, смена почтового ящика.<br></label>	
 		
 		<label>Можно добавить аватарку и ещё чего нибудь.<br></label>
 	</h4>
+	<script async src="https://telegram.org/js/telegram-widget.js?8" data-telegram-share-url="https://prizmarket.herokuapp.com" data-size="large" data-text="notext"></script>
 </article>
