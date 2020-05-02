d31ff21f2a527b7b81473d7f58e3f98fbd6407a6
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-02 18:05:51 +0300

    Add files via upload

--- web/BiblaTimer/Functions.php
+++ web/BiblaTimer/Functions.php
@@ -27,7 +27,7 @@ function _запуск_таймера($команда = null) {
 			}		
 		}else {			
 			if ($команда == 'старт') {				
-				$запрос = "INSERT INTO `variables` VALUES ('8', 'таймер', 'старт', '', '')";				
+				$запрос = "INSERT INTO `variables` VALUES ('8', 'таймер', 'старт', 'таймер запущен', '')";				
 				$результат = $mysqli->query($запрос);				
 				if ($результат) $ответ = true;				
 			}			
@@ -35,9 +35,37 @@ function _запуск_таймера($команда = null) {
 	}	
 	return $ответ;	
 }
-
-
-
+// запись в таблицу информации о работе таймера (об ожидании отклика бота)
+function _в_ожидании($команда = null) {	
+	global $mysqli;	
+	$ответ = false;
+	$запрос = "SELECT soderjimoe FROM `variables` WHERE id_bota='8' AND nazvanie='ожидание'";	
+	$результат = $mysqli->query($запрос);	
+	if ($результат) {		
+		$количество = $результат->num_rows;		
+		if ($количество > 0) {			
+			if ($команда == 'нет') {				
+				$запрос = "DELETE FROM `variables` WHERE id_bota='8' AND nazvanie='ожидание'";				
+				$результат = $mysqli->query($запрос);
+				if ($результат) $ответ = true;				
+			}elseif ($команда == 'есть') {				
+				$запрос = "UPDATE `variables` SET soderjimoe='есть' WHERE id_bota='8' AND nazvanie='ожидание'";				
+				$результат = $mysqli->query($запрос);				
+				if ($результат) $ответ = true;			
+			}else {			
+				$результМассив = $результат->fetch_all(MYSQLI_ASSOC);			
+				$ответ = $результМассив[0]['soderjimoe'];				
+			}		
+		}else {			
+			if ($команда == 'есть') {				
+				$запрос = "INSERT INTO `variables` VALUES ('8', 'ожидание', 'есть', 'ожидание отклика бота', '')";				
+				$результат = $mysqli->query($запрос);				
+				if ($результат) $ответ = true;				
+			}			
+		}		
+	}	
+	return $ответ;	
+}
 
 
 
