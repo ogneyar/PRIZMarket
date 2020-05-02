1b103087a50c2508d5abbdba408a4116c8594998
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-02 20:48:08 +0300

    Add files via upload

--- web/BiblaICQnew/Channel_post.php
+++ web/BiblaICQnew/Channel_post.php
@@ -1,5 +1,64 @@
 <?
+$eventId = _событие();
+if (!$eventId) {
+	$eventId = 0;
+	_событие($eventId);
+}
 
+//$bot_icq->sendText("@Ogneyar_", "Начало положено.");
 
+$события = $bot_icq->getEvents($eventId,20);
+
+if ($события != []) {
+	foreach($события as $event) {
+		$eventId = $event['eventId'];		
+		$payload = $event['payload'];
+			$chat = $payload['chat'];
+				$chatId = $chat['chatId'];
+				$chatType = $chat['type'];
+			$from = $payload['from'];
+				$firstName = $from['firstName'];
+				$nick = $from['nick'];
+				$userId = $from['userId'];
+			$msgId = $payload['msgId'];
+			$text = $payload['text'];
+			$timestamp = $payload['timestamp'];
+		$type = $event['type'];
+	}	
+		
+	if ($text=='ё') {
+		$bot_icq->sendText($chatId, "клмн");
+	}elseif ($text=='/start') {
+		$реплика = "Здравствуй ".$firstName."\n\nПопробуй команду /help";
+		$bot_icq->sendText($chatId, $реплика);
+	}elseif ($text=='/help') {
+		/*if ($nick == 'Ogneyar_') {
+			$реплика = "Список понимаемых мною команд:\n\nё\nПривет\nКак дела?\nеее\nуфь\n\n/exit";
+		}else */$реплика = "Список понимаемых мною команд:\n\nё\nПривет\nКак дела?\nеее\nуфь";
+			$bot_icq->sendText($chatId, $реплика);
+	}/*elseif ($text=='/exit') {
+		if ($nick == 'Ogneyar_') {
+			$события = $bot_icq->getEvents($eventId,0);
+			break;
+		}
+	}*/elseif ($text=='Привет') {
+		$реплика = "Сам ты привет. И брат твой привет. И сестра твоя привет.";
+		$bot_icq->sendText($chatId, $реплика);
+	}elseif ($text=='Как дела?') {
+		$реплика = "Дааа норм чо, а #сам_чо_как?";
+		$bot_icq->sendText($chatId, $реплика);
+	}elseif ($text=='еее') {
+		$реплика = "Так держать хозяин!";
+		$bot_icq->sendText($chatId, $реплика);
+	}elseif ($text=='уфь') {
+		$реплика = "Эт ты на каком языке? Уууфь, нет такой буквы в алфавите!";
+		$bot_icq->sendText($chatId, $реплика);
+	}elseif ($text) $bot_icq->sendText($chatId, "я не понимаю(");
+	
+	_событие($eventId);
+	
+}
+
+//$bot_icq->sendText("@Ogneyar_", "Цикл окончен.");
 
 ?>
--- web/BiblaICQnew/Functions.php
+++ web/BiblaICQnew/Functions.php
@@ -4,7 +4,33 @@ function _старт_АйСиКюБота() {
 	global $bot, $chat_id;	
 	$bot->sendMessage($chat_id, "Прива!");	
 }
-
+// запись в таблицу информации о последнем событии (Event)
+function _событие($событие = null) {	
+	global $mysqli;	
+	$ответ = false;
+	$запрос = "SELECT soderjimoe FROM `variables` WHERE id_bota='11' AND nazvanie='событие'";	
+	$результат = $mysqli->query($запрос);	
+	if ($результат) {		
+		$количество = $результат->num_rows;		
+		if ($количество > 0) {			
+			if ($событие) {				
+				$запрос = "UPDATE `variables` SET soderjimoe='{$событие}' WHERE id_bota='11' AND nazvanie='событие'";				
+				$результат = $mysqli->query($запрос);				
+				if ($результат) $ответ = true;			
+			}else {			
+				$результМассив = $результат->fetch_all(MYSQLI_ASSOC);			
+				$ответ = $результМассив[0]['soderjimoe'];				
+			}		
+		}else {			
+			if ($событие) {				
+				$запрос = "INSERT INTO `variables` VALUES ('11', 'событие', '{$событие}', 'номер последнего события', '')";				
+				$результат = $mysqli->query($запрос);				
+				if ($результат) $ответ = true;				
+			}			
+		}		
+	}	
+	return $ответ;	
+}
 
 
 
