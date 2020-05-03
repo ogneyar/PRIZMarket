c382ee294071787f4a3f60c9cb5f8393b7a33ac5
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-03 21:13:51 +0300

    Add files via upload

--- web/BiblaICQnew/Channel_post.php
+++ web/BiblaICQnew/Channel_post.php
@@ -51,18 +51,18 @@ if ($события != []) {
 		$реплика = "Здравствуй ".$firstName."\n\nПопробуй команду /help";
 		$bot_icq->sendText($chatId, $реплика);
 	}elseif ($text=='/help') {
-		$реплика = "Список понимаемых мною команд: \n\n/Privet \n/KakDela? \n/eee \n/uf`";
+		$реплика = "Список понимаемых мною команд: \n\n/Privet \n/KakDela \n/eee \n/uf";
 		$bot_icq->sendText($chatId, $реплика);	
 	}elseif ($text=='/Privet') {
 		$реплика = "Сам ты привет. И брат твой привет. И сестра твоя привет.";
 		$bot_icq->sendText($chatId, $реплика);
-	}elseif ($text=='/KakDela?') {
+	}elseif ($text=='/KakDela') {
 		$реплика = "Дааа норм чо, а #сам_чо_как?";
 		$bot_icq->sendText($chatId, $реплика);
 	}elseif ($text=='/eee') {
 		$реплика = "Так держать хозяин!";
 		$bot_icq->sendText($chatId, $реплика);
-	}elseif ($text=='/uf`') {
+	}elseif ($text=='/uf') {
 		$реплика = "Эт ты на каком языке? Уууфь, нет такой буквы в алфавите!";
 		$bot_icq->sendText($chatId, $реплика);
 	}elseif ($text) $bot_icq->sendText($chatId, "я не понимаю(");
