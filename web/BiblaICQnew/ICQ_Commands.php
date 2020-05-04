6f404038075281710923c3acda01dc7621f4dff3
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-04 15:13:40 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_Commands.php
+++ web/BiblaICQnew/ICQ_Commands.php
@@ -10,8 +10,9 @@ if ($text == 'сенд') {
 }elseif ($text=='дай айди') {		
 	$bot_icq->sendText($chatId, "Вот - {$chatId}\nтип чата - {$chatType}");
 	
-}elseif ($text=='сендФ') {		
-	$bot_icq->sendFile($chatId, "https://i.ibb.co/YZVdQrH/file-108.jpg");
+}elseif ($text=='сендФ') {
+	$файл = $file_get_contents("https://i.ibb.co/YZVdQrH/file-108.jpg");
+	$bot_icq->sendFile($chatId, $файл);
 	
 }
 ?>
