a04ee4476597adeb755901c9b7b14df8ebb4ccf9
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-04 15:09:06 +0300

    Add files via upload

--- web/myBotApi/ICQnew.php
+++ web/myBotApi/ICQnew.php
@@ -16,8 +16,11 @@
  * Список методов:
  * ---------------
  *
+ * getEvents
+ *
  * sendText
  *
+ * sendFile
  *
  */
 
@@ -70,10 +73,11 @@ class ICQnew
         $result = null;
         if (is_array($data)) {
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $this->apiUrl . $method);
-            curl_setopt($ch, CURLOPT_POST, count($data));
+            curl_setopt ($ch, CURLOPT_URL, $this->apiUrl . $method);
+            curl_setopt ($ch, CURLOPT_POST, count($data));
             curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+			curl_setopt ($ch, CURLOPT_SAFE_UPLOAD, false);
+            curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($data));
             $result = curl_exec($ch);
             curl_close($ch);
         }
@@ -179,8 +183,50 @@ class ICQnew
 	}
 	
 	
-	
-	
+	/*
+	**  функция отправки сообщения 
+	**
+	**  @param str $chatId
+	**  @param str $file
+ 	**  @param str $caption
+	**  @param array $inlineKeyboardMarkup
+	**  @param array $replyMsgId	
+	**  @param str $forwardChatId
+	**  @param array $forwardMsgId
+	**  
+	**  @return int (msgId)
+	*/
+    public function sendFile(
+		$chatId, 
+		$file,
+		$caption = null,
+		$inlineKeyboardMarkup = null,
+		$replyMsgId = null,
+		$forwardChatId = null,
+		$forwardMsgId = null
+	) {
+		
+		if ($inlineKeyboardMarkup) $inlineKeyboardMarkup = json_encode($inlineKeyboardMarkup);
+		
+		$response = $this->call("/messages/sendFile", [
+			'token' => $this->token,
+			'chatId' => $chatId,
+			'file' => "@".$file,
+			'text' => $text,
+			'replyMsgId' => $replyMsgId,			
+			'forwardChatId' => $forwardChatId,				
+			'forwardMsgId' => $forwardMsgId,			
+			'inlineKeyboardMarkup' => $inlineKeyboardMarkup
+		]);	
+				
+		$response = json_decode($response, true);
+		
+		if ($response['ok']) {
+			$response = $response['msgId'];
+		}else $response = false;
+		
+		return $response;
+	}
 	
 }
 
