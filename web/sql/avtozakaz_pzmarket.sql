32e44ad95828c554abd48a05c52fce55b65c84c6
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-04 10:28:34 +0300

    Add files via upload

--- web/sql/avtozakaz_pzmarket.sql
+++ web/sql/avtozakaz_pzmarket.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
---
--- Хост: 127.0.0.1
--- Время создания: Фев 08 2020 г., 22:32
--- Версия сервера: 5.5.25
--- Версия PHP: 5.3.13
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- База данных: `testerbotoff`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `avtozakaz_pzmarket`
---
 
 CREATE TABLE IF NOT EXISTS `avtozakaz_pzmarket` (
   `id_client` bigint(20) DEFAULT NULL,
@@ -47,7 +22,3 @@ CREATE TABLE IF NOT EXISTS `avtozakaz_pzmarket` (
   `url_info_bot` varchar(200) DEFAULT NULL,
   `date` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
