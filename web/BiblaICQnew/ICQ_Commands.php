4e1687338a6b105dfd55908ed241f4abf13d6ca2
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-05 12:39:32 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_Commands.php
+++ web/BiblaICQnew/ICQ_Commands.php
@@ -7,17 +7,18 @@ if (strpos($text, ":")!==false) {
 if ($text == 'сенд') {	
 	$bot_icq->sendText($ICQ_channel_market, "Здравствуйте дорогие товарищи!");
 	
-}elseif ($text=='дай айди') {		
+}elseif ($text == 'дай айди') {		
 	$bot_icq->sendText($chatId, "Вот - {$chatId}\nтип чата - {$chatType}");
 	
-}elseif ($text=='сендФ') {
+}elseif ($text == 'сендФ') {
 	/*$файл = file_get_contents("https://i.ibb.co/YZVdQrH/file-108.jpg");
 	$bot_icq->sendFile($chatId, $файл);*/
 	
 	/*$url = "https://i.ibb.co/YZVdQrH/file-108.jpg";
 	$файл = new CURLFile($url, 'image/jpeg', 'file-108.jpg');
 	$bot_icq->sendFile($chatId, $файл);*/
-	
-	include_once "../myBotApi/test.php";
+
+	include_once '../myBotApi/test.php';
+	$bot_icq->sendText($chatId, "Х.З.");
 }
 ?>
