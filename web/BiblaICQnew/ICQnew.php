a11d1f195f4cc5e9fb98cf9cecc8b2591023937a
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-05 12:02:56 +0300

    Add files via upload

--- web/BiblaICQnew/ICQnew.php
+++ web/BiblaICQnew/ICQnew.php
@@ -50,9 +50,9 @@ class ICQnew
         if (is_array($data)) {
             $ch = curl_init();
             curl_setopt ($ch, CURLOPT_URL, $this->apiUrl . $method);
-          	curl_setopt ($ch, CURLOPT_POST, count($data));
+          	curl_setopt ($ch, CURLOPT_POST, true); //count($data));
 			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);	           
-			curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+			curl_setopt ($ch, CURLOPT_POSTFIELDS, $data); //http_build_query($data));
             $result = curl_exec($ch);
             curl_close($ch);
         }
