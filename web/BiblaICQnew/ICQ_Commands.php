72833d5f0e67d2c8c5774620659b2df88d222195
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-07 21:36:15 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_Commands.php
+++ web/BiblaICQnew/ICQ_Commands.php
@@ -77,7 +77,7 @@ if ($text == 'сенд') {
 		'http' => [
 			'method' => 'POST',
 			'header' => 'Content-Type: multipart/form-data' . PHP_EOL,
-			'content' => ['file' => $fileContents]
+			'content' => http_build_query(['file' => $fileContents])
 		],
 	]);
  
