eee082bbe65ecd5e2aaf39f26a992168534c5823
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-09 16:05:19 +0300

    Add files via upload

--- web/site_pzm/lk/save_zakaz.php
+++ web/site_pzm/lk/save_zakaz.php
@@ -4,11 +4,11 @@ include_once '../../../vendor/autoload.php';
 include_once '../../a_conect.php';
 include_once '../../myBotApi/Bot.php';
 //exit('ok');
-$bot = new Bot($tokenSite);
-$id_bota = strstr($tokenSite, ':', true);	
+$bot = new Bot($tokenAvtoZakaz);
+$id_bota = strstr($tokenAvtoZakaz, ':', true);	
 include_once '../../myBotApi/Variables.php';
-include_once '../../BiblaSite/Functions.php';
-$admin_group = $admin_group_Site;
+include_once '../../BiblaAvtoZakaz/Functions_site.php';
+$admin_group = $admin_group_AvtoZakaz;
 $админка = $admin_group;
 $мастер = $master;
 $table_market = 'avtozakaz_pzmarket';
@@ -27,8 +27,8 @@ if (mysqli_connect_errno()) {
 	if ($_POST['hesh_city']) echo "Местонахождение: ".$_POST['hesh_city']."<br><br>";
 	if ($_POST['opisanie']) echo "Описание: ".$_POST['opisanie']."<br><br>";
 */
-
-	$путь_к_фото = $_FILES['file']['tmp_name'];
+	
+	$путь_к_фото = $_FILES['file']['tmp_name'];	
 
 	// Подключение к Амазон
 	$credentials = new Aws\Credentials\Credentials($aws_key_id, $aws_secret_key);
@@ -38,9 +38,41 @@ if (mysqli_connect_errno()) {
 		'version'  => 'latest',
 		'region'   => $aws_region
 	]);
-
-	$key = "TEMP-".$логин.".jpg";
+	
+	// поиск уникального номера файла (uniqid)
+	$запрос = "SELECT id_zakaz FROM {$table_market} WHERE id_client='7' AND username='{$логин}' AND status=''";		
 		
+	if ($результат = $mysqli->query($запрос)) {		
+		if ($результат->num_rows == 1) {		
+			$результМассив = $результат->fetch_all(MYSQLI_ASSOC);			
+			$uniqid = $результМассив[0]['id_zakaz'];
+			if ($uniqid) {
+				
+				$key = "temp{$uniqid}.jpg"; //{$логин}-
+				
+				$result = $s3->deleteObjects([
+					'Bucket' => $aws_bucket,			
+					'Delete' => [
+						'Objects' => [
+							[
+								'Key' => $key,						
+							],            
+						],				
+					],
+				]);
+				
+				if ($result['@metadata']['statusCode'] == '200') $bot->sendMessage($мастер, "Старый Файл {$key} удалён с Амазон");
+				else echo "Чего то не получается удалить лот {$key} из Amazon";	
+				
+			}
+		}		
+	}
+		
+ 	$uniqid = uniqid();	
+
+	//$key = "TEMP-{$логин}-{$uniqid}.jpg";
+	$key = "temp{$uniqid}.jpg";
+
 	$file = file_get_contents($путь_к_фото);
 	  
 	$upload = $s3->putObject([
@@ -52,13 +84,13 @@ if (mysqli_connect_errno()) {
 	// ---------------------------			
 
 	if(!$upload) {
-		echo "Не смог загрузить фото на Амазон";	
+		echo "Не смог загрузить файл на Амазон";	
 	}else {
-		//echo "Файл загружен на Амазон<br><br>";
+		//echo "Файл загружен на Амазон";
 		$ссылка_на_амазон = "https://{$aws_bucket}.s3.{$aws_region}.amazonaws.com/" . $key;
 		//echo "<br><br>" . $ссылка_на_амазон;	
-		$bot->sendMessage($мастер, "Файл {$key} загружен на Амазон");
-		//$bot->setMyCommands($BotCommand);		
+		$bot->sendMessage($мастер, "Файл загружен на Амазон");
+		//$bot->setMyCommands($BotCommand);
 		
 		
 		$query = "DELETE FROM {$table_market} WHERE id_client='7' AND username='{$логин}' AND status=''";		
@@ -68,18 +100,39 @@ if (mysqli_connect_errno()) {
 			  `gorod`, `username`, `doverie`, `otdel`, `format_file`, `file_id`, `url_podrobno`, 
 			  `status`, `podrobno`, `url_tgraph`, `foto_album`, `url_info_bot`, `date`
 			) VALUES (
-			  '7', '', '{$_POST['hesh_pk']}', '{$_POST['name']}', '{$_POST['link_name']}', '{$_POST['currency']} / PZM', '{$_POST['hesh_city']}', '{$логин}', '0', '{$_POST['hesh_kateg']}', '', '', '', '', '{$_POST['opisanie']}', '{$ссылка_на_амазон}', '', '', ''
+			  '7', '{$uniqid}', '{$_POST['hesh_pk']}', '{$_POST['name']}', '{$_POST['link_name']}', '{$_POST['currency']} / PZM', '{$_POST['hesh_city']}', '{$логин}', '0', '{$_POST['hesh_kateg']}', 'фото', '', '', '', '{$_POST['opisanie']}', '{$ссылка_на_амазон}', '', '', ''
 			)";							
 			$result = $mysqli->query($query);			
-			if (!$result) echo "Не смог сделать запись в таблицу {$table_market} (save_zakaz.php)";	
-			
-			//$bot->sendMessage($admin_group_AvtoZakaz, "Данные с сайта записаны в БД.");
-			
-			_отправка_лота_админам(); // BiblaSite/Functions.php
-			
-		}else echo "Не смог удалить запись в таблице {$table_market} (_запись_в_таблицу_маркет)";		
-		
+			if (!$result) {
+				echo "Не смог сделать запись в таблицу {$table_market} (save_zakaz.php)";	
+			}else {
+				//$bot->sendMessage($admin_group_AvtoZakaz, "Данные с сайта записаны в БД.");
+				
+				_отправка_лота_админам_с_сайта(); 
+			}
+		}else echo "Не смог удалить запись в таблице {$table_market} (_запись_в_таблицу_маркет)";	
+
+
+		$array = [
+			'login'   => $логин,
+			'file' => $ссылка_на_амазон
+		];	
+
 		echo "Заявка отправлена РАЙминистрации.<br><br>";
+		
+		if ($tester == 'да') $array = array_merge($array, [ 'tester' => $tester ]);
+		 
+		$ch = curl_init('http://f0430377.xsph.ru');
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $array); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		$html = curl_exec($ch);
+		curl_close($ch);	
+		 
+		echo $html;
+		
 		
 	}
 
