5ff86b49da70fb984756459f61b3d78871a8e8aa
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-10 09:37:07 +0300

    Add files via upload

--- web/site_pzm/site_files/head.php
+++ web/site_pzm/site_files/head.php
@@ -10,15 +10,15 @@
 	
 	<link rel="stylesheet" href="/site_pzm/font-awesome/css/font-awesome.min.css">
 	
-	<!--<link rel="shortcut icon" href="/site_pzm/img/favicon.png" type="image/png">-->
+	<link rel="shortcut icon" href="/site_pzm/img/favicon.png" type="image/png">
 	
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<!--<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 	<link rel="manifest" href="/site.webmanifest">
 	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
 	<meta name="msapplication-TileColor" content="#00aba9">
-	<meta name="theme-color" content="#ffffff">
+	<meta name="theme-color" content="#ffffff">-->
 	
 	<!-- Here we add libs for jQuery, Ajax... -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script> 
