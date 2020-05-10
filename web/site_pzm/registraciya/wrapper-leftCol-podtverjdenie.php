d96c18212bd9996688e675a80f9aada15d2529fc
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-10 16:15:55 +0300

    Add files via upload

--- web/site_pzm/registraciya/wrapper-leftCol-podtverjdenie.php
+++ web/site_pzm/registraciya/wrapper-leftCol-podtverjdenie.php
@@ -1,7 +1,7 @@
 <article id="podtverjdenie">		
 <h4>
 	<br>	
-	<label>&nbsp;&nbsp;<b><?=$логин;?></b>, для окончания регистрации выберите один из возможных вариантов, каким образом клиенты смогут с Вами в дальнейшем связязываться!</label>
+	<label>&nbsp;&nbsp;<b><?=$логин;?></b>, для окончания регистрации выберите один из вариантов, как клиенты смогут с Вами связаться!</label>
 	<br><br><br>
 	
 	<form action="/site_pzm/registraciya/input_svyazi.php" align="center">
@@ -11,8 +11,8 @@
 		<input type="submit" class="button" name="svyazi" id="telegram"  value="Telegram">
 		&nbsp;&nbsp;
 		<input type="submit" class="button" name="svyazi" id="whatsapp"  value="WhatsApp">
-		&nbsp;&nbsp;
-		<input type="submit" class="button" name="svyazi" id="wiber"  value="Wiber">
+		<!--&nbsp;&nbsp;
+		<input type="submit" class="button" name="svyazi" id="wiber"  value="Wiber">-->
 	</form>	
 </h4>
 </article>		
