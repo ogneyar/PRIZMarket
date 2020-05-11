b4fbbd01e46401cdbf66b50ed4c973978f2a1259
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-11 12:19:30 +0300

    Eee

--- web/BiblaAvtoZakaz/Commands.php
+++ web/BiblaAvtoZakaz/Commands.php
@@ -3,7 +3,7 @@ if (strpos($text, ":")!==false) {
 	$komanda = strstr($text, ':', true);		
 	$id = substr(strrchr($text, ":"), 1);	
 	$text = $komanda;
-}
+}else $id = '';
 
 if ($text == 'база') {
 	if ($id) {	
