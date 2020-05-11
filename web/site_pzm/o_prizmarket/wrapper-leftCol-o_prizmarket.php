088ab1071fcd6afaac36b9c0e346eb313c357515
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-11 17:35:36 +0300

    Add files via upload

--- web/site_pzm/o_prizmarket/wrapper-leftCol-o_prizmarket.php
+++ web/site_pzm/o_prizmarket/wrapper-leftCol-o_prizmarket.php
@@ -7,6 +7,7 @@
 	<h3><p><a href="https://teleg.link/Prizm_market_supportbot" target="_blank">Бот для ТехПоддержки!</a> - если возникнут трудности перейдите по этой ссылке в СаппортБота. Свяжитесь с нами и мы Вам обязательно поможем.</p></h3><br />
 	<h3><p><a href="https://teleg.link/prizm_market" target="_blank">Наш канал в телеграм!</a> - из этого канала на сайт PRIZMarket идёт вся публикация.</p></h3><br /><br />
 	<h3><p><a href="https://www.instagram.com/prizm_market_inst" target="_blank">Мы в инстаграм!</a> - публикации, в которых есть ссылки на Ваш инстаграм, дублируются на нашей странице.</p></h3><br /><br />
+	<h3><p><a href="https://icq.im/prizmarket" target="_blank">Наш канал в icq new!</a> - на этот канал идёт вся публикация с телеграм и сайта.</p></h3><br /><br />
 	<h1><p><b>Команда PRIZMarket</b> трудится во благо Вам и для развития криптовалюты PRIZM.</p></h1>
 </div>
 </article>
