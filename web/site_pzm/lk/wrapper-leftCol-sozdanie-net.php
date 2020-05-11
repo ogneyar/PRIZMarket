cb6714da0fea594f046e223486e06df1915d97f6
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-11 18:46:12 +0300

    Add files via upload

---
+++ web/site_pzm/lk/wrapper-leftCol-sozdanie-net.php
@@ -0,0 +1,8 @@
+﻿<article id="lk">
+	<h4>
+		<br>
+			<label><?=$_COOKIE['login'];?> после последней публикации прошло менее суток.<br></label>	
+			<label>При особом желании Вы можете публиковать чаще, для этого напишите нам в <a href="https://teleg.link/zakazLOTbot" target="_blank">телеграм</a>.<br></label>
+			<label>БезОплатно публиковать можно раз в сутки.<br></label>
+	</h4>
+</article>
