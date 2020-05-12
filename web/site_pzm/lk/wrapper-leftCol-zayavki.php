05af4db43210c175b6e36f63a1c6ab1b054b495a
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-12 17:02:54 +0300

    Add files via upload

--- web/site_pzm/lk/wrapper-leftCol-zayavki.php
+++ web/site_pzm/lk/wrapper-leftCol-zayavki.php
@@ -1,6 +1,15 @@
 ﻿<article id="lk">
+	<article>
+		<a href='' title=''>
+			<img src="/site_pzm/img/art/PRIZM.png" />
+		</a>
+		<h3><span>Здесь может быть Ваша реклама!</span></h3>
+	</article>
+</article>-->
+
+<!--<article id="lk">
 		<br>
 		<h4>
 			<label>Пусто тута)<br></label>		
 		</h4>
-</article>
+</article>-->
