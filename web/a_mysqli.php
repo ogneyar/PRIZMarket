7e20b5448fa002d8fd6709a9fd8d1602b0f8e1ea
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-13 23:48:54 +0300

    Eee

--- web/a_mysqli.php
+++ web/a_mysqli.php
@@ -4,8 +4,8 @@ include_once 'a_conect.php';
 $mysqli = new mysqli($host, $username, $password, $dbname);
 // проверка подключения 
 if (mysqli_connect_errno()) {	
-	echo "Ошибка! Нет подключения к БД.\n";	 
-	$mysqli->close();		
+	echo "Ошибка! Нет подключения к БД.<br>";	 
+	//$mysqli->close();		
 	exit('ok');  	
 }
 ?>
