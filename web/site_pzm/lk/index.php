cf27a5b24c58d52dd3f77921e27496e627bb4186
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-13 18:32:03 +0300

    Add files via upload

--- web/site_pzm/lk/index.php
+++ web/site_pzm/lk/index.php
@@ -17,7 +17,7 @@ if (!$_COOKIE['login']) header('Location: /site_pzm/vhod/index.php');
 		<?include_once '../site_files/header.php';?>
 	</header>
 	<div id="lk_menu">
-		<?include_once 'lk.php';?>
+		<?include_once 'index-lk_menu.php';?>
 	</div>
 	<nav>
 		<?include_once '../site_files/nav.php';?>
