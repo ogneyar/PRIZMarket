d2b02d917c8b1e051f56f8a4dc6626e90aa58ef2
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-13 18:17:54 +0300

    Add files via upload

--- web/site_pzm/lk/index.php
+++ web/site_pzm/lk/index.php
@@ -28,7 +28,7 @@ if (!$_COOKIE['login']) header('Location: /site_pzm/vhod/index.php');
 			<?include_once '../site_files/wrapper-topCol.php';?>
 		</div>
 		<div id="leftCol">
-			<?include_once 'wrapper-leftCol-lk.php';?>
+			<?include_once 'index-leftCol.php';?>
 		</div>
 		<div id="rightCol">
 			<?include_once '../site_files/wrapper-rightCol.php';?>
