71b25eb6d9d72e17c4e2264f1b942fe091e5f6a1
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-14 13:11:37 +0300

    Add files via upload

--- web/sql/avtozakaz_ojidanie.sql
+++ web/sql/avtozakaz_ojidanie.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
---
--- Хост: 127.0.0.1
--- Время создания: Янв 28 2020 г., 12:21
--- Версия сервера: 5.5.25
--- Версия PHP: 5.3.13
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- База данных: `testerbotoff`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `avtozakaz_ojidanie`
---
 
 CREATE TABLE IF NOT EXISTS `avtozakaz_ojidanie` (
   `id_client` bigint(20) DEFAULT NULL,
@@ -32,7 +7,3 @@ CREATE TABLE IF NOT EXISTS `avtozakaz_ojidanie` (
   `last` varchar(200) DEFAULT NULL,
   `flag` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
