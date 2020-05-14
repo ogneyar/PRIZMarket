7993bdfd7bf855322d4c06764934f4b3ba1a456f
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-14 09:41:52 +0300

    Add files via upload

--- web/site_pzm/lk/sozdanie.php
+++ web/site_pzm/lk/sozdanie.php
@@ -51,8 +51,8 @@ $mysqli->close();
 			if ($подтверждён) {
 				$давно = _последняя_публикация_на_сайте($_COOKIE['login']);	
 				if ($давно) { 
-                                         include_once 'sozdanie-leftCol.php';	
-				}else include_once 'sozdanie-leftCol.php';	
+					include_once 'sozdanie-leftCol.php';	
+				}else include_once 'sozdanie-net-leftCol.php';	
 			}else {
 				include_once 'sozdanie-nepodtv-leftCol.php';
 			}
