325ee0e3b11b72f2075d8d3af343b69ad75d6fe5
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-16 17:38:12 +0300

    Add files via upload

--- web/site_pzm/lk/zayavki-repeat_delete-leftCol.php
+++ web/site_pzm/lk/zayavki-repeat_delete-leftCol.php
@@ -5,7 +5,7 @@ $токен = $_POST['token'];
 $номер_лота = $_POST['id_lota'];
 $повтор_или_удаление = $_POST['repeat_delete'];
 if ($повтор_или_удаление == 'Повторить') {
-	echo "<h4>Пока не работает эта функция</h4>";
+	echo "<h4><br>Пока не работает эта функция</h4>";
 }elseif ($повтор_или_удаление == 'Удалить') {
 	//echo "<h4>Ещё не работает эта функция</h4>"; 
 	
@@ -14,7 +14,7 @@ if ($повтор_или_удаление == 'Повторить') {
 	<input type='hidden' name='token' id='token' value='<?=$токен;?>'>
 	<input type='hidden' name='id_lota' id='id_lota' value='<?=$номер_лота;?>'>
 
-	<h4>
+	<h4><br>
 	<label>Вы уверены что хотите удалить лот <?=$номер_лота;?>?</label><br><br>
 	<input type='button' class='button' name='done' id='done'  value='Да'>	
 	<input type='button' class='button' name='non' id='non'  value='Нет'>
