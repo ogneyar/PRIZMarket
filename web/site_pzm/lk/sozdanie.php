65c6d741d8178a69601a5a6a6a669e907ffbe56f
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-17 23:50:20 +0300

    Add files via upload

--- web/site_pzm/lk/sozdanie.php
+++ web/site_pzm/lk/sozdanie.php
@@ -31,19 +31,14 @@ if (!isset($_POST['save_photo'])) {
 
 <script>
 
-$(document).ready (function (){						
-	var file;		
-	$("#file").on('change', function(){
-		file = this.files[0];			
-	});					
+$(document).ready (function (){
 	$("#done").click (function ( event ){
 		event.stopPropagation(); // остановка всех текущих JS событий
 		event.preventDefault();  // остановка дефолтного события
 				
 		$('#warning').html (' ' + "<br>");
 		$('#warning').show ();
-				
-		//var login = <?=$json; ?>;
+						
 		var login = $("#login").val ();
 		var hesh_pk = $("#hesh_pk").val ();
 		var name = $("#name").val ();
@@ -54,8 +49,6 @@ $(document).ready (function (){
 		var opisanie = $("#opisanie").val ();
 		var fail = "";		
 		
-		//if (typeof file == 'undefined') fail = "Не выбран файл";
-		//else 
 		if (opisanie.length < 100) fail = "Описание не менее 100 символов";
 		
 		if (currency.length < 0) fail = "Валюта не менее 4х символов";		
@@ -66,9 +59,7 @@ $(document).ready (function (){
 		
 		if (name.length < 3) fail = "Название не менее 3х символов";
 		else if (link_name.length > 0) {
-			if(link_name.indexOf('.')+1 < 1) fail = "В ссылке не указан домен '.ru' или '.com'";
-			//else if(link_name.indexOf(':')+1 < 1) fail = "В ссылке отсутствует 'https://'";
-			//else if(link_name.indexOf('/')+1 < 1) fail = "Не корректная ссылка";
+			if(link_name.indexOf('.')+1 < 1) fail = "В ссылке не указан домен '.ru' или '.com'";			
 		}
 		
 		if (login == 'Огнеяр') fail = "";
