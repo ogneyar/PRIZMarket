aeb2e42614524834ee4a0518dcba8d059e2d2c1c
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-17 16:53:06 +0300

    Add files via upload

--- web/BiblaAvtoZakaz/Channel_post.php
+++ web/BiblaAvtoZakaz/Channel_post.php
@@ -8,9 +8,9 @@ if ($number !== false && $number == '0') {
 }elseif (isset($photo)) {
 
 	if (stripos($caption, ':') !== false) {
-		if (strstr($caption, ':', true) == "Логин") {
-			$логин = substr(strrchr($caption, ":"), 1);	
-			$query ="UPDATE {$table_market} SET file_id='{$file_id}' WHERE id_client='7' AND username='{$логин}' AND status=''";
+		if (strstr($caption, ':', true) == "Лот") {
+			$лот = substr(strrchr($caption, ":"), 1);	
+			$query ="UPDATE {$table_market} SET file_id='{$file_id}' WHERE id_zakaz='{$лот}'";
 			$result = $mysqli->query($query);		
 			if ($result) {
 				$bot->sendMessage($channel_info, "Записал в базу file_id этого фото.");
