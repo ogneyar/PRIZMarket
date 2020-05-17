ee7e9ac0323018f7e8df57f4d909c654975d1a04
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-17 16:00:10 +0300

    Add files via upload

--- web/site_pzm/lk/sozdanie-save_zakaz.php
+++ web/site_pzm/lk/sozdanie-save_zakaz.php
@@ -58,16 +58,14 @@ if (mysqli_connect_errno()) {
 		// инфа о том с какого сайта (тестового или оригинала) идёт посылка
 		if ($tester == 'да') $array = array_merge($array, [ 'tester' => $tester ]);		
 		// отправка madeLine фото для публикации её в телеге
-		$ch = curl_init('http://f0430377.xsph.ru');
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($array)); 
+		$ch = curl_init("http://f0430377.xsph.ru"."?".http_build_query($array));
+		//curl_setopt($ch, CURLOPT_POST, 1);
+		//curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($array)); 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_HEADER, false);
 		$html = curl_exec($ch);
 		curl_close($ch);	
-		
-		echo "<br>ЧЕГО ТУТ - ".$html." - А?<br>";
 		
 		
 		$связь = _дай_связь($логин);
