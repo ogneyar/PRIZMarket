0307ea36f64d8c2bb009e5c1db5ea1dd2149b37c
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-18 10:03:58 +0300

    Курс PRIZM

--- web/BiblaICQnew/ICQ_Message.php
+++ web/BiblaICQnew/ICQ_Message.php
@@ -1,4 +1,7 @@
 ﻿<?
+include_once "../BiblaMarket/bot_03_func.php";
+
+
 if ($chatType == 'private') {
 //------------------------------------------
 
@@ -6,16 +9,19 @@ if ($text=='/start') {
 	_старт();
 }elseif ($text=='/help') {
 		
-}elseif ($text=='/Privet') {
+}elseif ($text=='/kurs' || $text=='курс' || $text=='Курс') {
+        $курс = _kurs_PZM();
+	$bot_icq->sendText($chatId, $курс);
+}elseif ($text=='/privet' || $text=='привет' || $text=='Привет') {
 	$реплика = "Сам ты привет. И брат твой привет. И сестра твоя привет.";
 	$bot_icq->sendText($chatId, $реплика);
-}elseif ($text=='/KakDela') {
-	$реплика = "Дааа норм чо, а #сам_чо_как?";
+}elseif ($text=='/KakDela' || $text=='Как дела?' || $text=='как дела?' || $text=='Как дела' || $text=='как дела') {
+	$реплика = "Дааа норм чо, #сам_чо_как?";
 	$bot_icq->sendText($chatId, $реплика);
-}elseif ($text=='/eee') {
+}elseif ($text=='/eee' || $text=='Еее' || $text=='еее' || $text=='Eee' || $text=='eee') {
 	$реплика = "Так держать хозяин!";
 	$bot_icq->sendText($chatId, $реплика);
-}elseif ($text=='/uf') {
+}elseif ($text=='/uf' || $text=='Уфь' || $text=='уфь') {
 	$реплика = "Эт ты на каком языке? Уууфь, нет такой буквы в алфавите!";
 	$bot_icq->sendText($chatId, $реплика);
 }elseif ($text) $bot_icq->sendText($chatId, "я не понимаю(");
