98074731f19e209e49dd543232eb853b7cd3ce13
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-18 09:23:28 +0300

    TESTMarket

--- web/site_pzm/site_files/header.php
+++ web/site_pzm/site_files/header.php
@@ -1,3 +1,15 @@
+<? 
+if ($tester == 'да') {
+?>
+<a href="/" id="logo">
+	<b><span>TESTMarket</span></b>
+</a>
+<?
+}else {
+?>
 <a href="/" id="logo">
 	<b><span>PRIZMarket</span></b>
 </a>
+<?
+} 
+?>
