f11bed295c4120880fbb22245fd96667ff1a2c89
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-18 10:59:16 +0300

    Update ICQ_Functions.php

--- web/BiblaICQnew/ICQ_Functions.php
+++ web/BiblaICQnew/ICQ_Functions.php
@@ -2,7 +2,7 @@
 // реакция бота на команду старт
 function _старт() {	
 	global $bot_icq, $chatId, $firstName;
-	$реплика = "Здравствуй ".$firstName."\n\nЯ помогу тебе узнать сколько стоит PRIZM на CoinMarketCap.com, для этого нажми на команду /kurs или же пришли мне слово *Курс*. \n\nСписок понимаемых мною доп. команд: \n\n/privet \n\n/KakDela \n\n/eee \n\n/uf";
+	$реплика = "Здравствуй ".$firstName."\n\nЯ помогу тебе узнать сколько стоит PRIZM на \nCoinMarketCap.com, \nдля этого нажми на команду \n/kurs \nили же пришли мне слово *Курс*. \n\nСписок понимаемых мною доп. команд: \n\n/privet \n\n/KakDela \n\n/eee \n\n/uf";
 	$bot_icq->sendText($chatId, $реплика);
 }
 
