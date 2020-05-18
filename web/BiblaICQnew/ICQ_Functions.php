bf6331ce41310b001ed0791739716fbf3ab35deb
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-18 09:44:02 +0300

    Update ICQ_Functions.php

--- web/BiblaICQnew/ICQ_Functions.php
+++ web/BiblaICQnew/ICQ_Functions.php
@@ -2,7 +2,7 @@
 // реакция бота на команду старт
 function _старт() {	
 	global $bot_icq, $chatId, $firstName;
-	$реплика = "Здравствуй ".$firstName."\n\nПопробуй команду /help";
+	$реплика = "Здравствуй ".$firstName."\n\nСписок понимаемых мною команд: \n\n/Privet \n/KakDela \n/eee \n/uf";
 	$bot_icq->sendText($chatId, $реплика);
 }
 
