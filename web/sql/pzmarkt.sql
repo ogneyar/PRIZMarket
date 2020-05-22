af100d69de467d21234082d86208a1adad16bc00
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-22 15:36:13 +0300

    Add files via upload

--- web/sql/pzmarkt.sql
+++ web/sql/pzmarkt.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
---
--- Хост: 127.0.0.1
--- Время создания: Фев 14 2020 г., 10:04
--- Версия сервера: 5.5.25
--- Версия PHP: 5.3.13
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- База данных: `testerbotoff`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `pzmarkt`
---
 
 CREATE TABLE IF NOT EXISTS `pzmarkt` (
   `id` int(20) NOT NULL,
@@ -41,7 +16,3 @@ CREATE TABLE IF NOT EXISTS `pzmarkt` (
   `podrobno` varchar(100) DEFAULT NULL,
   `time` int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
