5eb419cdcee516e08b1f54fb3b2fdbd61804b6be
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-22 11:06:07 +0300

    Add files via upload

--- web/site_pzm/site_files/head.php
+++ web/site_pzm/site_files/head.php
@@ -1,14 +1,17 @@
-﻿	<meta name="description" content="" />
-	<meta name="author" content="" />
+﻿	
+	<meta name="author" content="Огънеяръ" />
 	<meta name="robots" content="all,index,follow" />
 	<meta name="distribution" content="global" />
 	
 	<!-- Adopt website to current screen -->
 	<meta name="viewport" content="user-scalable=yes, width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	
+	<!-- редирект -->
+	<!-- <meta http-equiv="refresh" content="5; URL=https://www.PRIZMarket.ru"> -->
+	
 	<meta http-equiv="Cache-Control" content="no-cache">
 	
-	<link rel="stylesheet" href="/site_pzm/css/style7.css">
+	<link rel="stylesheet" href="/site_pzm/css/style.css">
 	
 	<link rel="stylesheet" href="/site_pzm/font-awesome/css/font-awesome.min.css">
 	
