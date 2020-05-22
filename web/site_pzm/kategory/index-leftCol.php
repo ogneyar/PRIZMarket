6b6ff60bf42d05e662f501ec1d804b71c48d05f5
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-22 14:19:38 +0300

    Add files via upload

--- web/site_pzm/kategory/index-leftCol.php
+++ web/site_pzm/kategory/index-leftCol.php
@@ -1,3 +1,36 @@
-﻿<?
-
-?>	
+﻿<article id="kategory">
+<h4><br>
+	<form action="index.php" method="post" id="nedvij"></form>
+	<form action="index.php" method="post" id="rabota"></form>
+	<form action="index.php" method="post" id="transport"></form>
+	<form action="index.php" method="post" id="uslugi"></form>
+	<form action="index.php" method="post" id="lich_veschi"></form>
+	<form action="index.php" method="post" id="dlya_doma"></form>
+	<form action="index.php" method="post" id="electro"></form>
+	<form action="index.php" method="post" id="jivotnie"></form>
+	<form action="index.php" method="post" id="hobbi"></form>
+	<form action="index.php" method="post" id="biznes"></form>
+	<form action="index.php" method="post" id="producti"></form>
+	<form action="index.php" method="post" id="krasota"></form>
+	
+	<label>Выберите категорию товара/услуги:<br><br></label>
+		
+	<input type="submit" class="button" name="kategory" form="nedvij" value="#недвижимость">
+	<input type="submit" class="button" name="kategory" form="rabota" value="#работа">
+	<br><br>
+	<input type="submit" class="button" name="kategory" form="transport" value="#транспорт">
+	<input type="submit" class="button" name="kategory" form="uslugi" value="#услуги">
+	<br><br>
+	<input type="submit" class="button" name="kategory" form="lich_veschi" value="#личные_вещи">
+	<input type="submit" class="button" name="kategory" form="dlya_doma" value="#для_дома_и_дачи">
+	<br><br>
+	<input type="submit" class="button" name="kategory" form="electro" value="#бытовая_электроника">
+	<input type="submit" class="button" name="kategory" form="jivotnie" value="#животные">
+	<br><br>
+	<input type="submit" class="button" name="kategory" form="hobbi" value="#хобби_и_отдых">
+	<input type="submit" class="button" name="kategory" form="biznes" value="#для_бизнеса">
+	<br><br>
+	<input type="submit" class="button" name="kategory" form="producti" value="#продукты_питания">
+	<input type="submit" class="button" name="kategory" form="krasota" value="#красота_и_здоровье">
+</h4>
+</article>
