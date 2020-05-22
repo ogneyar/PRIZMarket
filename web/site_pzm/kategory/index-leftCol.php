924c5b874b5b50b7fc5039357d159e1eb803dfd4
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-22 21:02:58 +0300

    Update index-leftCol.php

--- web/site_pzm/kategory/index-leftCol.php
+++ web/site_pzm/kategory/index-leftCol.php
@@ -16,21 +16,27 @@
 	<label>Выберите категорию товара/услуги:<br><br></label>
 		
 	<input type="submit" class="button" name="kategory" form="nedvij" value="#недвижимость">
+        <br><br>
 	<input type="submit" class="button" name="kategory" form="rabota" value="#работа">
 	<br><br>
 	<input type="submit" class="button" name="kategory" form="transport" value="#транспорт">
+        <br><br>
 	<input type="submit" class="button" name="kategory" form="uslugi" value="#услуги">
 	<br><br>
 	<input type="submit" class="button" name="kategory" form="lich_veschi" value="#личные_вещи">
+        <br><br>
 	<input type="submit" class="button" name="kategory" form="dlya_doma" value="#для_дома_и_дачи">
 	<br><br>
 	<input type="submit" class="button" name="kategory" form="electro" value="#бытовая_электроника">
+        <br><br>
 	<input type="submit" class="button" name="kategory" form="jivotnie" value="#животные">
 	<br><br>
 	<input type="submit" class="button" name="kategory" form="hobbi" value="#хобби_и_отдых">
+        <br><br>
 	<input type="submit" class="button" name="kategory" form="biznes" value="#для_бизнеса">
 	<br><br>
 	<input type="submit" class="button" name="kategory" form="producti" value="#продукты_питания">
+        <br><br>
 	<input type="submit" class="button" name="kategory" form="krasota" value="#красота_и_здоровье">
 </h4>
 </article>
