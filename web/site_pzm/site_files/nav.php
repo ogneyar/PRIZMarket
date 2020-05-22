845ef26ea2650d23f113958d8391da5c5cd3facd
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-22 10:36:08 +0300

    Add files via upload

--- web/site_pzm/site_files/nav.php
+++ web/site_pzm/site_files/nav.php
@@ -1,6 +1,7 @@
 ﻿<div class="line-menu">
 <a href="/">Главная</a>
-<a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробности</a>
+<a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробно</a>
+<a href="/site_pzm/kategory/index.php">Категории</a>
 <a href="/site_pzm/support/index.php">ТехПоддержка</a>
 <a href="/site_pzm/o_prizmarket/index.php">О нас</a>
 </div>
@@ -8,7 +9,8 @@
 	<li id="mini-menu"><a href="#"><i class="fa fa-bars fa-lg" aria-hidden="true"></i><!-- Меню--></a>
 		<ul class="submenu">	
 			<li><a href="/">Главная</a></li>
-			<li><a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробности</a></li>
+			<li><a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробно</a></li>
+			<li><a href="/site_pzm/kategory/index.php">Категории</a></li>
 			<li><a href="/site_pzm/support/index.php">ТехПоддержка</a></li>
 			<li><a href="/site_pzm/o_prizmarket/index.php">О_нас</a></li>
 		</ul>
