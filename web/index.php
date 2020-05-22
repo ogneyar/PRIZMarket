3495fcdcf5527a642bdb3994c19743aa76185a43
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-22 11:07:27 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -6,44 +6,17 @@ include_once 'site_pzm/pzmarket.php';
 <!DOCTYPE html>
 <html xmlns="https://www.w3.org/1999/xhtml">
 <head>
-	<meta charset="utf-8" />	
+	<meta charset="utf-8" />
 	<title>PRIZMarket!</title>
+	<meta name="description" content="Сайт для рекламы товаров/услуг за криптовалюту PRIZM" />
+	<meta name="keywords" content="PRIZM, криптовалюта, товары, услуги, куплю, продам, безоплатно, бесплатно" />
 	<?include_once 'site_pzm/site_files/head.php';?>
-	
 	<style type="text/css">
 		@media (min-width: 700px) {
 		nav a:first-child, nav#fixed a:first-child {
 			border-top: 5px solid rgba(255,235,59);
 		}} 
-	</style>	
-	
-	<script>
-	/*$(document).ready (function (){
-		$(<?=$json_button_id;?>).click (function (){				
-			$(<?=$json_article_id;?>).html (' ' + "<br>");
-			$(<?=$json_article_id;?>).show ();
-				
-			//var last_lot = $("#last_lot").val ();
-				
-			var last_lot = <?=$json_last_lot; ?>;
-				
-			$.ajax ({
-				url: '/site_pzm/pzmarket.php',
-				type: 'POST',
-				cache: false,
-				data: {'last_lot': last_lot},
-				dataType: 'html',
-				success: function (data) {
-					$(<?=$json_article_id;?>).html ( data );
-					$(<?=$json_article_id;?>).show ();						
-				}
-			});
-				
-		});			
-			
-	});		*/
-	</script>
-		
+	</style>
 </head>
 <body>
 	<header>
