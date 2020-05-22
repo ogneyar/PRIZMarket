6b1705920927ba73a698f01658a3765aa8c1e5cd
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-22 14:45:00 +0300

    Add files via upload

--- web/a_mysqli.php
+++ web/a_mysqli.php
@@ -1,11 +1,10 @@
 <?php
-include_once 'a_conect.php';
+include_once '/a_conect.php';
 
 $mysqli = new mysqli($host, $username, $password, $dbname);
-// проверка подключения 
+
 if (mysqli_connect_errno()) {	
-	echo "Ошибка! Нет подключения к БД.<br>";	 
-	//$mysqli->close();		
-	exit('ok');  	
+	echo "Ошибка! Нет подключения к БД.<br>";	 		
+	exit;  	
 }
 ?>
