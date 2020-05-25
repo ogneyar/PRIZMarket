bf20ab2a4fc99ba7b2f22a87690fbf24c11b5962
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-25 11:11:57 +0300

    Add files via upload

--- web/site_pzm/lk/sozdanie-save_photo.php
+++ web/site_pzm/lk/sozdanie-save_photo.php
@@ -1,6 +1,7 @@
 <article id="lk">
 <h4><br>
 <?
+include_once '../../a_mysqli.php';
 include_once '../../myBotApi/Bot.php';
 include_once '../../BiblaAvtoZakaz/Functions_site.php';
 //exit('ok');
@@ -15,7 +16,7 @@ if (empty($_GET['url'])) {
 	echo "<br><h4>Ошибка! Не выбран файл.</h4>";		
 	exit;
 }else {
-/*	
+	
 	$есть = _есть_ли_запись($логин, 'url_tgraph');
 	
 	if ($есть) {
@@ -31,12 +32,15 @@ if (empty($_GET['url'])) {
 		_отправка_лота_админам_с_сайта(); 
 		
 	}
-*/	
+	
 	echo "Заявка отправлена РАЙминистрации.<br><br>";
 	echo "Сообщение о решении будет отправленно Вам на email.<br><br>";
 	echo "Все вопросы в <a href='https://teleg.link/Prizm_market_supportbot'>тех.поддержку.</a>";
 	
 }
+
+// закрываем подключение 
+$mysqli->close();
 
 ?>
 </h4>
