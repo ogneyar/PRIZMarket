799b3f7362967e4575fd62724c7822895b5c9e4b
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-26 19:06:24 +0300

    Add files via upload

--- web/botVK_PZM.php
+++ web/botVK_PZM.php
@@ -27,7 +27,7 @@ elseif ($data_vk->object->body == "Прива") {
 
    //mesSend();
 
-  file_get_contents("https://api.vk.com/method/". "messages.send". "?access_token=". $vk_token. "&peer_id=". $data_vk->object->user_id. "&message=zdravstvuj&v=5.68");
+  file_get_contents("https://api.vk.com/method/". "messages.send". "?access_token=". $vk_token. "&peer_id=". $data_vk->object->user_id. "&message=Ну да, здравствуй)&v=5.68");
 
 
 } 
