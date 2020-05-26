579d8d5ea235c1e24d7ea4bef07d72dddac683a5
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-26 21:45:13 +0300

    Add files via upload

--- web/myBotApi/VK.php
+++ web/myBotApi/VK.php
@@ -75,8 +75,8 @@ class VK
 		  //curl_setopt ($ch, CURLOPT_POST, 1);
 		  //curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 		  curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt($ch, CURLOPT_HEADER, false);
+        //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			//curl_setopt($ch, CURLOPT_HEADER, false);
         $result = curl_exec($ch);
         curl_close($ch);
 		
