586d1546e68f0aea7d4310c3c2b794a7a4fdf32c
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-27 06:16:43 +0300

    Add files via upload

---
+++ web/myBotApi/VKvariables.php
@@ -0,0 +1,15 @@
+﻿<?
+// Обрабатываем пришедшие данные
+$data_vk = $bot_vk->init('php://input');
+
+$тип_данных_вк = $data_vk['type'];
+
+$секрет =  $data_vk['secret'];
+
+$объект = $data_vk['object'];
+
+$user_id_vk = $объект['user_id'];
+
+$тело = $объект['body'];
+
+?>
