830559b5ab927e87acfc55ba7555b83e3485d0c0
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-27 06:29:21 +0300

    Add files via upload

--- web/botVK_PZM.php
+++ web/botVK_PZM.php
@@ -4,39 +4,24 @@ include_once 'a_conect.php';
 
 // Создаем объект VK бота
 $bot_vk = new VK($vk_token);
-$data_vk = $bot_vk->init('php://input');
+//$data_vk = $bot_vk->init('php://input');
+include_once 'myBotApi/VKvariables.php';
 
 $айди_вк_группы = "190150616";
 
-//$data_vk = json_decode(file_get_contents('php://input'));
-
-if ($data_vk['secret'] !== $vk_secret_key) {
-	echo "Ошибка!";
-	exit;
+if ($секрет !== $vk_secret_key) {
+	 exit("Ошибка!");
 }
 
-if ($data_vk['type'] == "confirmation") {
-	echo $vk_api_response;
-	exit;
+if ($тип_данных_вк == "confirmation") {
+	exit($vk_api_response);
 }
 
-if ($data_vk['object']['body'] == "Прива") {
-
-   $массив = [
-      "access_token" => $vk_token, 
-      "peer_id" => $data_vk['object']['user_id'] , 
-      "message" => "Ну да, здравствуй)", 
-      "v" => $vk_api_version
-   ];
-
-  file_get_contents("https://api.vk.com/method/". "messages.send?". http_build_query($массив));
-
-
-}elseif ($data_vk['type'] == "message_new") {
+if ($тип_данных_вк == "message_new") {
 
    include_once "BiblaVK_PZM/Message.php"; 
 
 } 
 
-echo "ok";
+exit("ok");
 ?>
