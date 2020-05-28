37fd0908d698a169a43fe1077374d5a6527aad13
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-28 19:42:22 +0300

    Add files via upload

--- web/BiblaVK_PZM/Message.php
+++ web/BiblaVK_PZM/Message.php
@@ -7,7 +7,7 @@ if ($тело == "Прива") {
 	$массив = [
 		"access_token" => $vk_token, 
 		"random_id" => $random_id, 
-		"peer_id" => $user_id_vk, 
+		"peer_id" => $user_id, 
 		"message" => "Ну да, здравствуй)", 
 		"v" => $vk_api_version
 	];
@@ -17,29 +17,34 @@ if ($тело == "Прива") {
 
 }elseif ($тело == "загрузи") {	
 	
-	$результат = $bot_vk->photosGetUploadServer($vk_album_id, $vk_group_id);
+	$результат = $vk->photosGetUploadServer($vk_album_id, $vk_group_id);
 	
-	$bot_vk->messagesSend($user_id_vk, "результат: ".print_r($результат));
+	if ($результат['error_msg']) {		
+		$vk->messagesSend($user_id, "Ошибка: ".$результат['error_msg']);
+		exit;		
+	}
 	
-	$bot_vk->messagesSend($user_id_vk, "upload_url: ".$результат['upload_url']);
+	//$vk->messagesSend($user_id, "результат: ".print_r($результат));
+	
+	$vk->messagesSend($user_id, "upload_url: ".$результат['upload_url']);
 		
-	$результат = $bot_vk->upload($результат['upload_url'], "http://f0430377.xsph.ru/image/test5eccceaecbdc4.jpg");
+	$результат = $vk->upload($результат['upload_url'], "http://f0430377.xsph.ru/image/test5eccceaecbdc4.jpg");
 		
 	$server = $результат['server'];
 	$photos_list = $результат['photos_list'];
 	$hash = $результат['hash'];
 		
-	$bot_vk->messagesSend($user_id_vk, "server: {$server}, photos_list: {$photos_list}, hash: {$hash}");
+	$vk->messagesSend($user_id, "server: {$server}, photos_list: {$photos_list}, hash: {$hash}");
 		
-	$результат = $bot_vk->photosSave($vk_album_id, $vk_group_id, $server, $photos_list, $hash);
+	$результат = $vk->photosSave($vk_album_id, $vk_group_id, $server, $photos_list, $hash);
 		
 	//echo "id фото: ".$результат['id'];
 		
-	$bot_vk->messagesSend($user_id_vk, "id фото: ".$результат['id']);
+	$vk->messagesSend($user_id, "id фото: ".$результат['id']);
 
 }else {
 
-    $bot_vk->messagesSend($user_id_vk, "не пойму(");
+    $vk->messagesSend($user_id, "не пойму(");
 
 } 
 
