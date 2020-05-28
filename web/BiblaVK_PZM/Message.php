6aea2596a2cf975006c3c95d7da9d58a72df619c
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-28 13:48:21 +0300

    Add files via upload

--- web/BiblaVK_PZM/Message.php
+++ web/BiblaVK_PZM/Message.php
@@ -2,39 +2,50 @@
 
 if ($тело == "Прива") {
 
-   $массив = [
-      "access_token" => $vk_token, 
-      "peer_id" => $user_id_vk, 
-      "message" => "Ну да, здравствуй)", 
-      "v" => $vk_api_version
-   ];
+	$массив = [
+		"access_token" => $vk_token, 
+		"peer_id" => $user_id_vk, 
+		"message" => "Ну да, здравствуй)", 
+		"v" => $vk_api_version
+	];
 
-  file_get_contents("https://api.vk.com/method/". "messages.send?". http_build_query($массив));
+	file_get_contents("https://api.vk.com/method/". "messages.send?". http_build_query($массив));
 
 
 }elseif ($тело == "загрузи") {
 
-        $результат = $bot_vk->photosGetUploadServer($vk_album_id, $vk_group_id);
+    //$результат = $bot_vk->photosGetUploadServer($vk_album_id, $vk_group_id);
+	$массив = [
+		"access_token" => $vk_token, 
+		"album_id" => $vk_album_id, 
+		"group_id" => $vk_group_id, 
+		"v" => '5.107'
+	];
+	$результат = file_get_contents("https://api.vk.com/method/". "photos.getUploadServer?". http_build_query($массив));
+	
+	$результат = json_decode($результат, true);
+	
+	$результат = $результат['response'];
 		
-		$bot_vk->messagesSend($user_id_vk, "upload_url: ".$результат['upload_url']);
+	$bot_vk->messagesSend($user_id_vk, "upload_url: ".$результат['upload_url']);
 		
-		$результат = $bot_vk->upload($результат['upload_url'], "http://f0430377.xsph.ru/image/test5eccceaecbdc4.jpg");
+	$результат = $bot_vk->upload($результат['upload_url'], "http://f0430377.xsph.ru/image/test5eccceaecbdc4.jpg");
 		
-		$server = $результат['server'];
-		$photos_list = $результат['photos_list'];
-		$hash = $результат['hash'];
+	$server = $результат['server'];
+	$photos_list = $результат['photos_list'];
+	$hash = $результат['hash'];
 		
-		$bot_vk->messagesSend($user_id_vk, "server: {$server}, photos_list: {$photos_list}, hash: {$hash}");
+	$bot_vk->messagesSend($user_id_vk, "server: {$server}, photos_list: {$photos_list}, hash: {$hash}");
 		
-		$результат = $bot_vk->photosSave($vk_album_id, $vk_group_id, $server, $photos_list, $hash);
+	$результат = $bot_vk->photosSave($vk_album_id, $vk_group_id, $server, $photos_list, $hash);
 		
-		//echo "id фото: ".$результат['id'];
+	//echo "id фото: ".$результат['id'];
 		
-		$bot_vk->messagesSend($user_id_vk, "id фото: ".$результат['id']);
+	$bot_vk->messagesSend($user_id_vk, "id фото: ".$результат['id']);
 
 }else {
 
-        $bot_vk->messagesSend($user_id_vk, "не пойму(");
+    $bot_vk->messagesSend($user_id_vk, "не пойму(");
 
 } 
 
