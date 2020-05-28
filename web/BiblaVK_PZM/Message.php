a5f1252b8ae203dc1db5dad8db8cb1069a1868db
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-28 13:33:30 +0300

    Add files via upload

--- web/BiblaVK_PZM/Message.php
+++ web/BiblaVK_PZM/Message.php
@@ -16,11 +16,15 @@ if ($тело == "Прива") {
 
         $результат = $bot_vk->photosGetUploadServer($vk_album_id, $vk_group_id);
 		
+		$bot_vk->messagesSend($user_id_vk, "upload_url: ".$результат['upload_url']);
+		
 		$результат = $bot_vk->upload($результат['upload_url'], "http://f0430377.xsph.ru/image/test5eccceaecbdc4.jpg");
 		
 		$server = $результат['server'];
 		$photos_list = $результат['photos_list'];
 		$hash = $результат['hash'];
+		
+		$bot_vk->messagesSend($user_id_vk, "server: {$server}, photos_list: {$photos_list}, hash: {$hash}");
 		
 		$результат = $bot_vk->photosSave($vk_album_id, $vk_group_id, $server, $photos_list, $hash);
 		
