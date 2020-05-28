4cdc1cb4250812924e6830fe8acb6ec4c9bf6e0e
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-28 19:57:59 +0300

    Add files via upload

--- web/BiblaVK_PZM/Message.php
+++ web/BiblaVK_PZM/Message.php
@@ -7,7 +7,7 @@ if ($text == "Прива") {
 	$массив = [
 		"access_token" => $vk_token, 
 		"random_id" => $random_id, 
-		"peer_id" => $from_id, 
+		"peer_id" => $peer_id, 
 		"message" => "Ну да, здравствуй)", 
 		"v" => $vk_api_version
 	];
@@ -20,13 +20,13 @@ if ($text == "Прива") {
 	$результат = $vk->photosGetUploadServer($vk_album_id, $vk_group_id);
 	
 	if ($результат['error_msg']) {		
-		$vk->messagesSend($from_id, "Ошибка: ".$результат['error_msg']);
+		$vk->messagesSend($peer_id, "Ошибка: ".$результат['error_msg']);
 		exit;		
 	}
 	
-	//$vk->messagesSend($from_id, "результат: ".print_r($результат));
+	//$vk->messagesSend($peer_id, "результат: ".print_r($результат));
 	
-	$vk->messagesSend($from_id, "upload_url: ".$результат['upload_url']);
+	$vk->messagesSend($peer_id, "upload_url: ".$результат['upload_url']);
 		
 	$результат = $vk->upload($результат['upload_url'], "http://f0430377.xsph.ru/image/test5eccceaecbdc4.jpg");
 		
@@ -34,17 +34,17 @@ if ($text == "Прива") {
 	$photos_list = $результат['photos_list'];
 	$hash = $результат['hash'];
 		
-	$vk->messagesSend($from_id, "server: {$server}, photos_list: {$photos_list}, hash: {$hash}");
+	$vk->messagesSend($peer_id, "server: {$server}, photos_list: {$photos_list}, hash: {$hash}");
 		
 	$результат = $vk->photosSave($vk_album_id, $vk_group_id, $server, $photos_list, $hash);
 		
 	//echo "id фото: ".$результат['id'];
 		
-	$vk->messagesSend($from_id, "id фото: ".$результат['id']);
+	$vk->messagesSend($peer_id, "id фото: ".$результат['id']);
 
 }else {
 
-    $vk->messagesSend($from_id, "не пойму(");
+    $vk->messagesSend($peer_id, "не пойму(");
 
 } 
 
