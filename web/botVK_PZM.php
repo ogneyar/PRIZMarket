874728d2c80c7b9c404af417feb5bc267fa32287
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-28 19:48:20 +0300

    Add files via upload

--- web/botVK_PZM.php
+++ web/botVK_PZM.php
@@ -3,26 +3,28 @@ include_once 'myBotApi/VK.php';
 include_once 'a_conect.php';
 
 // Создаем объект VK бота
-$bot_vk = new VK($vk_token);
-//$data_vk = $bot_vk->init('php://input');
+$vk = new VK($vk_token);
 include_once 'myBotApi/VKvariables.php';
 
 $vk_group_id = "190150616";
 $vk_album_id = "270858085";
 
-if ($секрет !== $vk_secret_key) {
-	 exit("Ошибка!");
+if ($secret !== $vk_secret_key) {
+	echo "Ошибка!";
+	exit;
 }
 
-if ($тип_данных_вк == "confirmation") {
-	exit($vk_api_response);
+if ($type == "confirmation") {
+	echo $vk_api_response;
+	exit;
 }
 
-if ($тип_данных_вк == "message_new") {
+if ($type == "message_new") {
 
    include_once "BiblaVK_PZM/Message.php"; 
 
 } 
 
-exit("ok");
+echo "ok";
+exit;
 ?>
