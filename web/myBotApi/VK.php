6a7bfa07d3b13038b9966040377ceeb7bc6b166c
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-28 13:20:56 +0300

    Add files via upload

--- web/myBotApi/VK.php
+++ web/myBotApi/VK.php
@@ -8,14 +8,17 @@
  *
  * call
  *
+ * upload
+ *
  * ---------------
  * Список методов:
  * ---------------
  *
- *  messagesSend
+ * messagesSend
  *
- * 
+ * photosGetUploadServer
  *
+ * photosSave
  * 
  *
  */
@@ -25,9 +28,6 @@ class VK
     // $token - токен бота 
     public $token = null;
 
-	// $version - версия vk api
-    public $version = null;
-
     // адрес для запросов к API
     public $apiUrl = "https://api.vk.com/method/";
 	
@@ -36,10 +36,9 @@ class VK
 	** @param str $token
 	*/
 
-    public function __construct($token, $version = '5.68')
+    public function __construct($token)
     {
 		$this->token = $token;
-		$this->version = $version;
     }    
     
     
@@ -70,26 +69,55 @@ class VK
         $response = null;		
 		
 	   	$data['access_token'] = $this->token;
-	   	$data['v'] = $this->version;		
 		
         $ch = curl_init();
-        //curl_setopt ($ch, CURLOPT_URL, $this->apiUrl . $method. "?". http_build_query($data));
         curl_setopt ($ch, CURLOPT_URL, $this->apiUrl . $method);
-		  curl_setopt ($ch, CURLOPT_POST, 1);
-		  curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-		  curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-        //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			//curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt ($ch, CURLOPT_POST, 1);
+		curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+		//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		//curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		//curl_setopt($ch, CURLOPT_HEADER, false);
         $result = curl_exec($ch);
         curl_close($ch);
 		
-       //file_get_contents($this->apiUrl . $method . "?". http_build_query($data));
+		//file_get_contents($this->apiUrl . $method . "?". http_build_query($data));
 
         $response = json_decode($result, true);
 		
         return $response['response'];
     }
+	
+	
+	/* 
+	** Загрузка файла на сервер ВК
+	**
+    ** @param str $url
+    ** @param file $file    
+	**
+    ** @return mixed
+    */
 
+    public function upload($url, $file)
+    {
+        $response = null;		
+		
+		$data = ['file1' => "@".$file];
+		
+        $ch = curl_init();
+        curl_setopt ($ch, CURLOPT_URL, $url);
+		curl_setopt ($ch, CURLOPT_POST, 1);
+		curl_setopt ($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        $result = curl_exec($ch);
+        curl_close($ch);
+		
+        $response = json_decode($result, true);
+		
+        return $response['response'];
+    }
 
 	
 //--------------------------------------
@@ -102,29 +130,106 @@ class VK
 	**
 	**  @param str $peer_id
  	**  @param str $message
+	**  @param str $version
 
 пока нет
 	**  @param array $attachment
+	
 	**  
 	**  @return array 
 	*/
 
     public function messagesSend(
 		$peer_id, 
-		$message
+		$message,
+		$version = '5.68'
 	) {				
 	
 		$response = $this->call("messages.send", [
-        //	'access_token' => $this->token, 
 			'peer_id' => $peer_id,
 			'message' => $message, 
-         //'v' => $this->version
+			'v' => $version
+		]);	
+	
+		return $response;
+	}
+	
+	
+	
+	/*
+	**  функция получения URL сервера для загрузки фото
+	**
+	**  @param int $album_id
+ 	**  @param int $group_id
+	**  @param str $version
+	**  
+	**  
+	**  @return array 
+	*/
+
+    public function photosGetUploadServer(
+		$album_id, 
+		$group_id,
+		$version = '5.107'
+	) {				
+	
+		$response = $this->call("photos.getUploadServer", [
+			'album_id' => $album_id,
+			'group_id' => $group_id, 
+			'v' => $version
 		]);	
 	
 		return $response;
 	}
 
+	
+	
+	
+	/*
+	**  функция сохранения фото
+	**
+	**  @param int $album_id
+ 	**  @param int $group_id
+	**  @param int $server
+	**  @param JSON $photos_list
+	**  @param str $hash
+	**  @param int $latitude
+	**  @param int $longitude
+	**  @param str $caption
+	**  @param str $version
+	**  
+	**  
+	**  @return array 
+	*/
 
+    public function photosSave(
+		$album_id, 
+		$group_id,		
+		$server,
+		$photos_list,
+		$hash,
+		$caption = null,
+		$latitude = 0,
+		$longitude = 0,		
+		$version = '5.107'
+	) {				
+	
+		$response = $this->call("photos.save", [
+			'album_id' => $album_id,
+			'group_id' => $group_id, 
+			'server' => $server,
+			'photos_list' => $photos_list,
+			'hash' => $hash,
+			'latitude' => $latitude,
+			'longitude' => $longitude,
+			'caption' => $caption,
+			'v' => $version
+		]);	
+	
+		return $response;
+	}
+	
+	
 	
 } 
 
