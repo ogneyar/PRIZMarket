0d9089941547e7d241d46435496d3064e5a8604b
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-28 19:48:36 +0300

    Add files via upload

--- web/BiblaVK_PZM/Message.php
+++ web/BiblaVK_PZM/Message.php
@@ -1,13 +1,13 @@
 ﻿<?
 
-if ($тело == "Прива") {
+if ($text == "Прива") {
 	
 	$random_id = time();
 	
 	$массив = [
 		"access_token" => $vk_token, 
 		"random_id" => $random_id, 
-		"peer_id" => $user_id, 
+		"peer_id" => $from_id, 
 		"message" => "Ну да, здравствуй)", 
 		"v" => $vk_api_version
 	];
@@ -15,18 +15,18 @@ if ($тело == "Прива") {
 	file_get_contents("https://api.vk.com/method/". "messages.send?". http_build_query($массив));
 
 
-}elseif ($тело == "загрузи") {	
+}elseif ($text == "загрузи") {	
 	
 	$результат = $vk->photosGetUploadServer($vk_album_id, $vk_group_id);
 	
 	if ($результат['error_msg']) {		
-		$vk->messagesSend($user_id, "Ошибка: ".$результат['error_msg']);
+		$vk->messagesSend($from_id, "Ошибка: ".$результат['error_msg']);
 		exit;		
 	}
 	
-	//$vk->messagesSend($user_id, "результат: ".print_r($результат));
+	//$vk->messagesSend($from_id, "результат: ".print_r($результат));
 	
-	$vk->messagesSend($user_id, "upload_url: ".$результат['upload_url']);
+	$vk->messagesSend($from_id, "upload_url: ".$результат['upload_url']);
 		
 	$результат = $vk->upload($результат['upload_url'], "http://f0430377.xsph.ru/image/test5eccceaecbdc4.jpg");
 		
@@ -34,17 +34,17 @@ if ($тело == "Прива") {
 	$photos_list = $результат['photos_list'];
 	$hash = $результат['hash'];
 		
-	$vk->messagesSend($user_id, "server: {$server}, photos_list: {$photos_list}, hash: {$hash}");
+	$vk->messagesSend($from_id, "server: {$server}, photos_list: {$photos_list}, hash: {$hash}");
 		
 	$результат = $vk->photosSave($vk_album_id, $vk_group_id, $server, $photos_list, $hash);
 		
 	//echo "id фото: ".$результат['id'];
 		
-	$vk->messagesSend($user_id, "id фото: ".$результат['id']);
+	$vk->messagesSend($from_id, "id фото: ".$результат['id']);
 
 }else {
 
-    $vk->messagesSend($user_id, "не пойму(");
+    $vk->messagesSend($from_id, "не пойму(");
 
 } 
 
