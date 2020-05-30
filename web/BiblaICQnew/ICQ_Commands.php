864cfb795ad4ce0d9c2f8045020faa8608bc250d
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-30 11:25:24 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_Commands.php
+++ web/BiblaICQnew/ICQ_Commands.php
@@ -11,16 +11,11 @@ if ($text == 'сенд') {
 	$bot_icq->sendText($chatId, "Вот - {$chatId}\nтип чата - {$chatType}");
 	
 }elseif ($text == 'сендФ') {
-	//$файл = file_get_contents("https://i.ibb.co/YZVdQrH/file-108.jpg");
+
 	$файл = "https://i.ibb.co/YZVdQrH/file-108.jpg";
 	$bot_icq->sendFile($chatId, $файл);
-	
-	/*$url = "https://i.ibb.co/YZVdQrH/file-108.jpg";
-	$файл = new CURLFile($url, 'image/jpeg', 'file-108.jpg');
-	$bot_icq->sendFile($chatId, $файл);*/
 
-	//include_once 'test.php';
-	$bot_icq->sendText($chatId, "Х.З.");
+	//$bot_icq->sendText($chatId, "Х.З.");
 	
 }elseif ($text == 'кноп') {		
 
@@ -65,28 +60,7 @@ if ($text == 'сенд') {
  
 	file_get_contents('https://api.icq.net/bot/v1/messages/sendText', false, $headers);
 	
-}elseif ($text == 'file_get_contents_file') {		
-	
-	$token = "001.2839288818.3919878723:752122979";
-	$chatId = "752067062";
-	$файл = "https://i.ibb.co/YZVdQrH/file-108.jpg";
-	$file = file_get_contents($файл);
-	$fileContents = stream_get_contents($file);
-	
-	$headers = stream_context_create([
-		'http' => [
-			'method' => 'POST',
-			'header' => 'Content-Type: multipart/form-data' . PHP_EOL,
-			'content' => http_build_query(['file' => $fileContents])
-		],
-	]);
- 
-	file_get_contents("https://api.icq.net/bot/v1/messages/sendFile?token={$token}&chatId={$chatId}", false, $headers);
-	
-	$bot_icq->sendText($chatId, "Х.З.");
-	
 }
-
 
 
 
