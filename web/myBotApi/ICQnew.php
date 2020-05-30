72d0e46cb02c5c4254405dfe4343542f8e7cfc6b
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-05-30 11:26:27 +0300

    Add files via upload

--- web/myBotApi/ICQnew.php
+++ web/myBotApi/ICQnew.php
@@ -1,12 +1,12 @@
 <?php
 
-/**-------------+
+/*------------------------+
  * Class ICQnew |
- * -------------+
+ * ------------------------+
  *
  * call
  *
- * PrintArray
+ * 
  *
  * ---------------
  * Список методов:
@@ -44,54 +44,50 @@ class ICQnew
 	**
     ** @return mixed
     */
-    public function call($method, $data, $photo=false)
+    public function call($method, $data, $file = null)
     {
         $result = null;
-        if (is_array($data)) {
+
+        $data['token'] = $this->token;
+
+        if ($file == null) {
+
             $ch = curl_init();
             curl_setopt ($ch, CURLOPT_URL, $this->apiUrl . $method);
-            if ($photo) {
-				//curl_setopt ($ch, CURLOPT_HEADER, false);
-				curl_setopt ($ch, CURLOPT_POST, 1);
-				curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-				//curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, false);	
-				//curl_setopt ( $ch, CURLOPT_SAFE_UPLOAD, true );
-				curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-			
-			}else {
 				curl_setopt ($ch, CURLOPT_POST, count($data));
 				curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);	           
 				curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-			}
             $result = curl_exec($ch);
             curl_close($ch);
-        }
-        return $result;
+
+        }else {
+
+           $url = $this->apiUrl . $method . "?" . http_build_query($data);
+		
+		$mimetype = mime_content_type($file);
+		$file_name = basename($file);
+		$curl_file = new CURLFile($file, $mimetype, $file_name);
+		
+		$dataFile = ['file1' => $curl_file];
+		
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $dataFile);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        } 
+
+        $response = json_decode($result, true);
+
+        return $response;
     }
     
-	
-	/*
-	**  функция вывода на печать массива
-	**
-	**  @param array $mass
-	**  @param int $i
-	**  @param str $flag
-	**
-	**  @return string
-	*/
-	public function PrintArray($mass, $i = 0) {		
-		global $flag;			
-		$flag .= "\t\t\t\t";						
-		foreach($mass as $key[$i] => $value[$i]) {				
-			if (is_array($value[$i])) {			
-					$response .= $flag . $key[$i] . " : \n";					
-					$response .= $this->PrintArray($value[$i], ++$i);					
-			}else $response .= $flag . $key[$i] . " : " . $value[$i] . "\n";			
-		}		
-		$str = $flag;		
-		$flag = substr($str, 0, -4);		
-		return $response;		
-	}
 	
 	
 	
@@ -109,12 +105,9 @@ class ICQnew
 	) {
 				
 		$response = $this->call("/events/get", [
-			'token' => $this->token,
 			'lastEventId' => $lastEventId,
 			'pollTime' => $pollTime
 		]);	
-				
-		$response = json_decode($response, true);
 		
 		if ($response['ok']) {
 			$response = $response['events'];
@@ -149,7 +142,6 @@ class ICQnew
 		if ($inlineKeyboardMarkup) $inlineKeyboardMarkup = json_encode($inlineKeyboardMarkup);
 		
 		$response = $this->call("/messages/sendText", [
-			'token' => $this->token,
 			'chatId' => $chatId,
 			'text' => $text,
 			'replyMsgId' => $replyMsgId,			
@@ -157,8 +149,6 @@ class ICQnew
 			'forwardMsgId' => $forwardMsgId,			
 			'inlineKeyboardMarkup' => $inlineKeyboardMarkup
 		]);	
-				
-		$response = json_decode($response, true);
 		
 		if ($response['ok']) {
 			$response = $response['msgId'];
@@ -169,10 +159,10 @@ class ICQnew
 	
 	
 	/*
-	**  функция отправки сообщения 
+	**  функция отправки файла
 	**
 	**  @param str $chatId
-	**  @param str $file
+	**  @param str $file or $file_id
  	**  @param str $caption
 	**  @param array $inlineKeyboardMarkup
 	**  @param array $replyMsgId	
@@ -190,30 +180,45 @@ class ICQnew
 		$forwardChatId = null,
 		$forwardMsgId = null
 	) {
-		//$image_mime = image_type_to_mime_type(exif_imagetype($file));
 		
-		if ($inlineKeyboardMarkup) $inlineKeyboardMarkup = json_encode($inlineKeyboardMarkup);
-		
-		$curl_file = curl_file_create($file, mime_content_type($file), "file-108.jpg");
-		
-		$response = $this->call("/messages/sendFile", [
-			'token' => $this->token,
-			'chatId' => $chatId,			
+        if ($inlineKeyboardMarkup) $inlineKeyboardMarkup = json_encode($inlineKeyboardMarkup);
+
+   $pos = strpos($file, "://");
+   if ($pos !== false) {
+
+      $response = $this->call("/messages/sendFile", [
+			'chatId' => $chatId,
 			'caption' => $caption,
 			'replyMsgId' => $replyMsgId,			
 			'forwardChatId' => $forwardChatId,				
 			'forwardMsgId' => $forwardMsgId,			
-			'inlineKeyboardMarkup' => $inlineKeyboardMarkup,
-			'file' =>  $curl_file
-		], true);	
+			'inlineKeyboardMarkup' => $inlineKeyboardMarkup
+		], 
+     $file);	
 		
-		$response = json_decode($response, true);
+			return $response;
+        
+   }else {
+		
+		$response = $this->call("/messages/sendFile", [
+			'chatId' => $chatId,			
+        	'file' =>  $file, 
+			'caption' => $caption,
+			'replyMsgId' => $replyMsgId,			
+			'forwardChatId' => $forwardChatId,				
+			'forwardMsgId' => $forwardMsgId,			
+			'inlineKeyboardMarkup' => $inlineKeyboardMarkup
+		]);	
 		
 		if ($response['ok']) {
 			$response = $response['msgId'];
 		}else $response = false;
 		
 		return $response;
+   } 
+
+
+
 	}
 	
 }
