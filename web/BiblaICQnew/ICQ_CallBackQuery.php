4cff000042c4581788d2b15dba7187721d1ba00a
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-03 21:45:30 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_CallBackQuery.php
+++ web/BiblaICQnew/ICQ_CallBackQuery.php
@@ -2,7 +2,9 @@
 
 if ($callbackData == "BBB") {
 	
-	$bot_icq->answerCallbackQuery($queryId, "Вот такой вот тут текст", true);
+	//$bot_icq->answerCallbackQuery($queryId, "Вот такой вот тут текст", true);
+	
+	$bot_icq->sendText($chatId, $queryId);
 	
 }
 
