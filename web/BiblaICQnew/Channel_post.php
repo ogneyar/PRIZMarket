2e3875b369d173129046e37465b0f1fea5b0f532
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-03 22:28:24 +0300

    Add files via upload

--- web/BiblaICQnew/Channel_post.php
+++ web/BiblaICQnew/Channel_post.php
@@ -54,6 +54,9 @@ if ($события != []) {
 		$type = $event['type'];
 	}	
 	
+	if ($callbackData) $bot_icq->sendText($chatId, "есть callbackData"); //: {$queryId}
+	else $bot_icq->sendText($chatId, "нет callbackData");
+	
 	if ($queryId) $bot_icq->sendText($chatId, "есть queryId"); //: {$queryId}
 	else $bot_icq->sendText($chatId, "нет queryId");
 	
