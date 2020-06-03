8def9a2e1ef3ef69fa2725653013ad6955f73ea6
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-03 13:22:14 +0300

    Add files via upload

--- web/myBotApi/ICQnew.php
+++ web/myBotApi/ICQnew.php
@@ -13,6 +13,10 @@
  *
  *  getEvents
  *
+ *-----------------------
+ * / messages /
+ *-----------------------
+ *
  *  sendText
  *
  *  sendFile
@@ -24,6 +28,18 @@
  *  ​deleteMessages
  *
  *  ​answerCallbackQuery
+ *
+ *----------------
+ * / chats /
+ *----------------
+ *
+ *  ​sendActions
+ *
+ *  getInfo
+ *
+ *  ​getAdmins
+ *
+ *
  *
  */
 
@@ -212,6 +228,10 @@ $response = false;
 				'forwardMsgId' => $forwardMsgId,			
 				'inlineKeyboardMarkup' => $inlineKeyboardMarkup
 			], $file);	
+
+if ($response['ok']) {
+				$response = true;
+			}else $response = false;
 			
 		}else {
 
@@ -273,6 +293,10 @@ $response = false;
 				'forwardMsgId' => $forwardMsgId,			
 				'inlineKeyboardMarkup' => $inlineKeyboardMarkup
 			], $file);	
+
+if ($response['ok']) {
+				$response = true;
+			}else $response = false;
 			
 		}else {
 
@@ -400,7 +424,86 @@ $url = null
 
 
 
+​
+/*
+	**  функция 
+	**
+	**  @param str $chatId
+**  @param arr[str] $actions
+	**  	
+	**
+	**  @return bool
+	*/
 
-}
+	public function ​sendActions(
+		$chatId, 
+$actions
+	) {
+	
+		$response = $this->call("/chats​/sendActions", [
+			'chatId' => $chatId,
+	'actions' => $actions
+		]);	
+			
+		if ($response['ok']) {
+			$response = true;
+		}else $response = false;
+			
+		return $response;
+	}
 
+
+
+
+/*
+	**  функция возвращает информацию о чате
+	**
+	**  @param str $chatId
+	**  	
+	**
+	**  @return array
+	*/
+
+	public function ​​getInfo(
+		$chatId
+	) {
+	
+		$response = $this->call("/chats​/getInfo", [
+			'chatId' => $chatId
+		]);	
+			
+if ($response['ok']) {
+		return $response;
+}else return false;
+	}
+
+
+
+/*
+	**  функция возвращает информацию об админах в чате
+	**
+	**  @param str $chatId
+	**  	
+	**
+	**  @return array[array] 
+	*/
+
+	public function ​​getAdmins(
+		$chatId
+	) {
+	
+		$response = $this->call("/chats​/getAdmins", [
+			'chatId' => $chatId
+		]);	
+			
+if ($response['ok']) {
+		return $response['admins'];
+}else return false;
+
+	}
+
+
+
+
+} 
 ?>
