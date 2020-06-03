a5c31c965244646b273eb0f014c90a1cc8aa1f76
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-03 14:16:02 +0300

    Add files via upload

--- web/BiblaICQnew/ICQnew.php
+++ web/BiblaICQnew/ICQnew.php
@@ -1,134 +1,158 @@
 <?php
 
-/**-------------+
- * Class ICQnew |
- * -------------+
+/* +----------------+
+ * |  Class ICQnew  |
+ * +----------------+
  *
- * call
+ *  call
  *
- * PrintArray
+ * 
+ * +--------------------+
+ * |  Список методов:   |
+ * +--------------------+
  *
- * ---------------
- * Список методов:
- * ---------------
+ *  getEvents
  *
- * getEvents
+ *---------------------
+ *  messages 
+ *---------------------
  *
- * sendText
+ *  sendText
  *
- * sendFile
+ *  sendFile
+ *
+ *  sendVoice
+ *
+ *  editText
+ *
+ *  ​deleteMessages
+ *
+ *  ​answerCallbackQuery
+ *
+ *-------------
+ *  chats 
+ *-------------
+ *
+ *  ​sendActions
+ *
+ *  getInfo
+ *
+ *  ​getAdmins
+ *
+ *
  *
  */
 
 class ICQnew
 {
-    // $token - созданный токен для нашего бота 
-    public $token = null;
-    // адрес для запросов к API
-    public $apiUrl = "https://api.icq.net/bot/v1";
+	// $token - созданный токен для нашего бота 
+	public $token = null;
 	
+	// адрес для запросов к API
+	public $apiUrl = "https://api.icq.net/bot/v1";
+		
 	/*
 	** @param str $token
 	*/
-    public function __construct($token)
-    {
-        $this->token = $token;
-    }    
-    
-    
-    /* 
+	public function __construct($token)
+	{
+		$this->token = $token;
+	}    
+		
+		
+	/* 
 	** Отправляем запрос в ICQ
 	**
-    ** @param str $method
-    ** @param array $data    
+	** @param str $method
+	** @param array $data    
 	**
-    ** @return mixed
-    */
-    public function call($method, $data, $photo=false)
-    {
-        $result = null;
-        if (is_array($data)) {
-            $ch = curl_init();
-            curl_setopt ($ch, CURLOPT_URL, $this->apiUrl . $method);
-            if ($photo) {				
-				curl_setopt ($ch, CURLOPT_POST, count($data));
-				curl_setopt ($ch, CURLOPT_POSTFIELDS, $data);
-				curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, false);	
-				curl_setopt ($ch, CURLOPT_HEADER, false);
-				//curl_setopt ( $ch, CURLOPT_SAFE_UPLOAD, true );
-			}else {
-				curl_setopt ($ch, CURLOPT_POST, count($data));
-				curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);	           
-				curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-			}
-            $result = curl_exec($ch);
-            curl_close($ch);
-        }
-        return $result;
-    }
-    
-	
-	/*
-	**  функция вывода на печать массива
-	**
-	**  @param array $mass
-	**  @param int $i
-	**  @param str $flag
-	**
-	**  @return string
+	** @return mixed
 	*/
-	public function PrintArray($mass, $i = 0) {		
-		global $flag;			
-		$flag .= "\t\t\t\t";						
-		foreach($mass as $key[$i] => $value[$i]) {				
-			if (is_array($value[$i])) {			
-					$response .= $flag . $key[$i] . " : \n";					
-					$response .= $this->PrintArray($value[$i], ++$i);					
-			}else $response .= $flag . $key[$i] . " : " . $value[$i] . "\n";			
-		}		
-		$str = $flag;		
-		$flag = substr($str, 0, -4);		
-		return $response;		
+
+	public function call(
+		$method, 
+		$data, 
+		$file = null
+	) {
+		$result = null;
+
+		$data['token'] = $this->token;
+
+		if ($file == null) {
+
+			$ch = curl_init();
+			curl_setopt ($ch, CURLOPT_URL, $this->apiUrl . $method);
+			curl_setopt ($ch, CURLOPT_POST, count($data));
+			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);	           
+			curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+		}else {
+
+			$url = $this->apiUrl . $method . "?" . http_build_query($data);
+			
+			$mimetype = mime_content_type($file);
+			$file_name = basename($file);
+			$curl_file = new CURLFile($file, $mimetype, $file_name);
+				
+			$dataFile = ['file' => $curl_file];
+				
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $dataFile);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+		} 
+
+		$response = json_decode($result, true);
+
+		return $response;
+		
 	}
-	
-	
-	
+
+
+
 	/*
 	**  функция получения событий
 	**
 	**  @param str $lastEventId
- 	**  @param str $pollTime
+	**  @param str $pollTime
 	**  
 	**  @return array
 	*/
-    public function getEvents(
+
+	public function getEvents(
 		$lastEventId, 
 		$pollTime
 	) {
-				
+		
 		$response = $this->call("/events/get", [
-			'token' => $this->token,
 			'lastEventId' => $lastEventId,
 			'pollTime' => $pollTime
 		]);	
-				
-		$response = json_decode($response, true);
-		
+			
 		if ($response['ok']) {
 			$response = $response['events'];
 		}else $response = false;
-		
+			
 		return $response;
+		
 	}
-	
-	
-    
-    /*
+		
+		
+		
+	/*
 	**  функция отправки сообщения 
 	**
 	**  @param str $chatId
- 	**  @param str $text
+	**  @param str $text
 	**  @param array $inlineKeyboardMarkup
 	**  @param array $replyMsgId	
 	**  @param str $forwardChatId
@@ -136,7 +160,8 @@ class ICQnew
 	**  
 	**  @return int (msgId)
 	*/
-    public function sendText(
+
+	public function sendText(
 		$chatId, 
 		$text,
 		$inlineKeyboardMarkup = null,
@@ -144,11 +169,10 @@ class ICQnew
 		$forwardChatId = null,
 		$forwardMsgId = null
 	) {
-		
+	
 		if ($inlineKeyboardMarkup) $inlineKeyboardMarkup = json_encode($inlineKeyboardMarkup);
-		
+			
 		$response = $this->call("/messages/sendText", [
-			'token' => $this->token,
 			'chatId' => $chatId,
 			'text' => $text,
 			'replyMsgId' => $replyMsgId,			
@@ -156,31 +180,31 @@ class ICQnew
 			'forwardMsgId' => $forwardMsgId,			
 			'inlineKeyboardMarkup' => $inlineKeyboardMarkup
 		]);	
-				
-		$response = json_decode($response, true);
-		
+			
 		if ($response['ok']) {
 			$response = $response['msgId'];
 		}else $response = false;
-		
+			
 		return $response;
 	}
-	
-	
+		
+
+		
 	/*
-	**  функция отправки сообщения 
+	**  функция отправки файла
 	**
 	**  @param str $chatId
-	**  @param str $file
- 	**  @param str $caption
+	**  @param str $file or $file_id
+	**  @param str $caption
 	**  @param array $inlineKeyboardMarkup
 	**  @param array $replyMsgId	
 	**  @param str $forwardChatId
 	**  @param array $forwardMsgId
 	**  
-	**  @return int (msgId)
+	**  @return int or array
 	*/
-    public function sendFile(
+
+	public function sendFile(
 		$chatId, 
 		$file,
 		$caption = null,
@@ -189,32 +213,287 @@ class ICQnew
 		$forwardChatId = null,
 		$forwardMsgId = null
 	) {
-		//$image_mime = image_type_to_mime_type(exif_imagetype($file));
-		
+
 		if ($inlineKeyboardMarkup) $inlineKeyboardMarkup = json_encode($inlineKeyboardMarkup);
 		
-		$curl_file = curl_file_create($file, mime_content_type($file), "file-108.jpg");
+		if (strpos($file, "://")!==false) {
+
+			$response = $this->call("/messages/sendFile", [
+				'chatId' => $chatId,
+				'caption' => $caption,
+				'replyMsgId' => $replyMsgId,			
+				'forwardChatId' => $forwardChatId,				
+				'forwardMsgId' => $forwardMsgId,			
+				'inlineKeyboardMarkup' => $inlineKeyboardMarkup
+			], $file);	
+
+if ($response['ok']) {
+				$response = true;
+			}else $response = false;
+			
+		}else {
+
+			$response = $this->call("/messages/sendFile", [
+				'chatId' => $chatId,			
+				'fileId' =>  $file, 
+				'caption' => $caption,
+				'replyMsgId' => $replyMsgId,			
+				'forwardChatId' => $forwardChatId,				
+				'forwardMsgId' => $forwardMsgId,			
+				'inlineKeyboardMarkup' => $inlineKeyboardMarkup
+			]);	
+			
+			if ($response['ok']) {
+				$response = $response['msgId'];
+			}else $response = false;
+			
+		} 
+
+
+return $response;
+
+	}
+
+
+	
+	/*
+	**  функция отправки голосового сообщения
+	**
+	**  @param str $chatId
+	**  @param str $file or $file_id
+	**  @param array $inlineKeyboardMarkup
+	**  @param array $replyMsgId	
+	**  @param str $forwardChatId
+	**  @param array $forwardMsgId
+	**  
+	**  @return int or array 
+	*/
+
+	public function sendVoice(
+		$chatId, 
+		$file,
+		$inlineKeyboardMarkup = null,
+		$replyMsgId = null,
+		$forwardChatId = null,
+		$forwardMsgId = null
+	) {
+
+		if ($inlineKeyboardMarkup) $inlineKeyboardMarkup = json_encode($inlineKeyboardMarkup);
 		
-		$response = $this->call("/messages/sendFile", [
-			'token' => $this->token,
-			'chatId' => $chatId,			
-			'caption' => $caption,
-			'replyMsgId' => $replyMsgId,			
-			'forwardChatId' => $forwardChatId,				
-			'forwardMsgId' => $forwardMsgId,			
-			'inlineKeyboardMarkup' => $inlineKeyboardMarkup,
-			'file' =>  $curl_file
-		], true);	
-		
-		$response = json_decode($response, true);
-		
+		if (strpos($file, "://")!==false) {
+
+			$response = $this->call("/messages​/sendVoice", [
+				'chatId' => $chatId,
+				'replyMsgId' => $replyMsgId,			
+				'forwardChatId' => $forwardChatId,				
+				'forwardMsgId' => $forwardMsgId,			
+				'inlineKeyboardMarkup' => $inlineKeyboardMarkup
+			], $file);	
+
+if ($response['ok']) {
+				$response = true;
+			}else $response = false;
+			
+		}else {
+
+			$response = $this->call("/messages/sendFile", [
+				'chatId' => $chatId,			
+				'fileId' =>  $file, 
+				'replyMsgId' => $replyMsgId,			
+				'forwardChatId' => $forwardChatId,				
+				'forwardMsgId' => $forwardMsgId,			
+				'inlineKeyboardMarkup' => $inlineKeyboardMarkup
+			]);	
+			
+			if ($response['ok']) {
+				$response = $response['msgId'];
+			}else $response = false;
+			
+		} 
+
+
+return $response;
+
+	}
+
+
+
+	/*
+	**  функция редактирования сообщения 
+	**
+	**  @param str $chatId
+**  @param int $msgId
+	**  @param str $text
+	**  @param array $inlineKeyboardMarkup
+	**  	
+	**
+	**  @return bool
+	*/
+
+	public function editText(
+		$chatId, 
+$msgId,
+		$text,
+		$inlineKeyboardMarkup = null
+	) {
+	
+		if ($inlineKeyboardMarkup) $inlineKeyboardMarkup = json_encode($inlineKeyboardMarkup);
+			
+		$response = $this->call("/messages/editText", [
+			'chatId' => $chatId,
+	'msgId' => $msgId,		
+			'text' => $text,	
+			'inlineKeyboardMarkup' => $inlineKeyboardMarkup
+		]);	
+			
 		if ($response['ok']) {
-			$response = $response['msgId'];
+			$response = true;
 		}else $response = false;
-		
+			
 		return $response;
 	}
-	
-}
+		
 
+
+/*
+	**  функция удаления сообщения 
+	**
+	**  @param str $chatId
+**  @param int $msgId
+	**  	
+	**
+	**  @return bool
+	*/
+
+	public function ​deleteMessages(
+		$chatId, 
+$msgId
+	) {
+	
+		$response = $this->call("/messages/deleteMessages", [
+			'chatId' => $chatId,
+	'msgId' => $msgId
+		]);	
+			
+		if ($response['ok']) {
+			$response = true;
+		}else $response = false;
+			
+		return $response;
+	}
+
+
+
+
+/*
+	**  функция ответа на событие callbackQuery 
+	**
+	**  @param str $queryId
+**  @param str $text
+**  @param bool $showAlert
+**  @param str $url
+	**  	
+	**
+	**  @return bool
+	*/
+
+	public function ​answerCallbackQuery(
+		$queryId, 
+$text = null, 
+$showAlert = false, 
+$url = null
+	) {
+	
+		$response = $this->call("/messages/answerCallbackQuery", [
+			'queryId' => $queryId,
+	'text' => $text, 
+	'showAlert' => $showAlert, 
+	'url' => $url
+		]);	
+			
+		if ($response['ok']) {
+			$response = true;
+		}else $response = false;
+			
+		return $response;
+	}
+
+
+/*
+	**  функция 
+	**
+	**  @param str $chatId
+**  @param arr[str] $actions
+	**  	
+	**
+	**  @return bool
+	*/
+
+	public function ​sendActions(
+		$chatId, 
+$actions
+	) {
+	
+		$response = $this->call("/chats/sandActions", [
+			'chatId' => $chatId,
+	'actions' => $actions
+		]);	
+			
+		if ($response['ok']) {
+			$response = true;
+		}else $response = false;
+			
+		return $response;
+	}
+
+
+/*
+	**  функция возвращает информацию о чате
+	**
+	**  @param str $chatId
+	**  	
+	**
+	**  @return array
+	*/
+
+	public function ​​getInfo(
+		$chatId
+	) {
+	
+		$response = $this->call("/chats​/getInfo", [
+			'chatId' => $chatId
+		]);	
+			
+if ($response['ok']) {
+		return $response;
+}else return false;
+	}
+
+
+/*
+	**  функция возвращает информацию об админах в чате
+	**
+	**  @param str $chatId
+	**  	
+	**
+	**  @return array[array] 
+	*/
+
+	public function ​​getAdmins(
+		$chatId
+	) {
+	
+		$response = $this->call("/chats/getAdmins", [
+			'chatId' => $chatId
+		]);	
+			
+if ($response['ok']) {
+		return $response['admins'];
+}else return false;
+
+	}
+
+
+
+} 
 ?>
