aff6ced2283ad29afa25b9149e4a02285145b4d2
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-04 19:36:56 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_Message.php
+++ web/BiblaICQnew/ICQ_Message.php
@@ -8,23 +8,32 @@ if ($text=='/start') {
 }elseif ($text=='/help') {
 		
 }elseif ($text=='/kurs' || $text=='курс' || $text=='Курс') {
-
-        $курс = _kurs_PZM();
-        $курс = str_replace("[CoinMarketCap](https://coinmarketcap.com/ru/currencies/prizm/)", "CoinMarketCap.com", $курс);
+	$курс = _kurs_PZM();
+	$курс = str_replace("[CoinMarketCap](https://coinmarketcap.com/ru/currencies/prizm/)", "CoinMarketCap.com", $курс);
 	$bot_icq->sendText($chatId, $курс);
 
+}elseif ($text=='матюк') {
+	$реплика = "Таких слов нельзя даже мыслить";
+	$bot_icq->sendText($chatId, $реплика);
+	
+	$bot_icq->​deleteMessages($chatId, $msgId);
+	
 }elseif ($text=='/privet' || $text=='привет' || $text=='Привет') {
 	$реплика = "Сам ты привет. И брат твой привет. И сестра твоя привет.";
 	$bot_icq->sendText($chatId, $реплика);
+	
 }elseif ($text=='/KakDela' || $text=='Как дела?' || $text=='как дела?' || $text=='Как дела' || $text=='как дела') {
 	$реплика = "Дааа норм чо, #сам_чо_как?";
 	$bot_icq->sendText($chatId, $реплика);
+	
 }elseif ($text=='/eee' || $text=='Еее' || $text=='еее' || $text=='Eee' || $text=='eee') {
 	$реплика = "Так держать хозяин!";
 	$bot_icq->sendText($chatId, $реплика);
+	
 }elseif ($text=='/uf' || $text=='Уфь' || $text=='уфь') {
 	$реплика = "Эт ты на каком языке? Уууфь, нет такой буквы в алфавите!";
 	$bot_icq->sendText($chatId, $реплика);
+	
 }elseif ($text) $bot_icq->sendText($chatId, "я не понимаю(");
 
 //------------------------------------------
