f9c29182d157cbba8d2754b4a57b94063efa9e8c
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-05 20:43:35 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_CallBackQuery.php
+++ web/BiblaICQnew/ICQ_CallBackQuery.php
@@ -1,65 +1,47 @@
 ﻿<?
-/*
-"message":{
-		"chat":{
-			"chatId":"752067062",
-			"type":"private"
-		},
-		"from":{
-			"firstName":"TesterPZMarket",
-			"nick":"TesterBotoffBot",
-			"userId":"752122979"
-		},
-		"msgId":"6834904851982320055",
-		"parts":[{
-			"payload":[[{
-				"callbackData":"najmimenya",
-				"text":"\u041d\u0430\u0436\u043c\u0438 \u043c\u0435\u043d\u044f"
-			}],[{
-				"callbackData":"ili",
-				"text":"\u0438\u043b\u0438"
-			}],[{
-				"callbackData":"udalimenya",
-				"text":"\u0423\u0434\u0430\u043b\u0438 \u043c\u0435\u043d\u044f"
-			}]],
-			"type":"inlineKeyboardMarkup"
-		}],
-		"text":"\u041d\u0430\u0436\u043c\u0438 \u043a\u043d\u043e\u043f\u043a\u0443!",
-		"timestamp":1591375296
-	}
-*/
+$chat = $message['chat'];
+	$chatId = $chat['chatId'];
+	$type = $chat['type'];
+$from = $message['from'];
+	$firstName = $from['firstName'];
+	$nick = $from['nick'];
+	$userId = $from['userId'];
+$msgId = $message['msgId'];
+$parts = $message['parts'];
+	$parts_payload = $parts[0]['payload'];
+	$parts_type = $parts[0]['type'];
+$text = $message['text'];
+$timestamp = $message['timestamp'];
+
 if ($callbackData == "najmimenya") {
 	
 	$результат = $bot_icq->answerCallbackQuery($queryId, "Ай молодец)))");	
-	if ($результат['ok'] == false) $bot_icq->sendText($message['chat']['chatId'], "Ошибка: {$результат['description']}");
-	
-	$msg = $message['msgId'];
-	$txt = $message['text'];
+	if ($результат['ok'] == false) $bot_icq->sendText($chatId, "Ошибка: {$результат['description']}");
 	/*
 	$событие = json_encode($event);
-	$bot_icq->sendText($message['chat']['chatId'], $событие);
+	$bot_icq->sendText($chatId, $событие);
 	*/
-	if ($txt == "Нажми кнопку!") $реплика = "Хорошо, а ещё?";
+	if ($text == "Нажми кнопку!") $реплика = "Хорошо, а ещё?";
 	else $реплика = "Нажми кнопку!";
 	
-	$bot_icq->editText($message['chat']['chatId'], $msg, $реплика, $кнопа);	
+	$bot_icq->editText($chatId, $msgId, $реплика, $кнопа);	
 	
 }elseif ($callbackData == "ili") {
 
 	$результат = $bot_icq->answerCallbackQuery($queryId, "Или ничего)", true);	
-	if ($результат['ok'] == false) $bot_icq->sendText($message['chat']['chatId'], "Ошибка: {$результат['description']}");
+	if ($результат['ok'] == false) $bot_icq->sendText($chatId, "Ошибка: {$результат['description']}");
 	
 }elseif ($callbackData == "udalimenya") {
 
 	$результат = $bot_icq->answerCallbackQuery($queryId, "Ой, ну и ладно(");	
-	if ($результат['ok'] == false) $bot_icq->sendText($message['chat']['chatId'], "Ошибка: {$результат['description']}");
+	if ($результат['ok'] == false) $bot_icq->sendText($chatId, "Ошибка: {$результат['description']}");
 	
-	​$bot_icq->deleteMessages($message['chat']['chatId'], $message['msgId']);	
+	​$bot_icq->deleteMessages($chatId, $msgId);	
 	
 }elseif ($callbackData == "BBB") {
 
 	$результат = $bot_icq->answerCallbackQuery($queryId, "Вот такой вот тут текст", true);	
-	if ($результат['ok'] == false) $bot_icq->sendText($message['chat']['chatId'], "Ошибка: {$результат['description']}");
+	if ($результат['ok'] == false) $bot_icq->sendText($chatId, "Ошибка: {$результат['description']}");
 	
 }
 
