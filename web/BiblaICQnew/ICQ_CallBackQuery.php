b36466f41d0908b7ff7b8d473d299c9343a57844
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-05 19:52:34 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_CallBackQuery.php
+++ web/BiblaICQnew/ICQ_CallBackQuery.php
@@ -3,27 +3,27 @@
 if ($callbackData == "najmimenya") {
 	
 	$результат = $bot_icq->answerCallbackQuery($queryId, "Ай молодец)))");	
-	if ($результат['ok'] == false) $bot_icq->sendText($userId, "Ошибка: {$результат['description']}");
+	if ($результат['ok'] == false) $bot_icq->sendText($message['chatId'], "Ошибка: {$результат['description']}");
+	
+	$msg = $message['msgId'];
+	$bot_icq->sendText($message['chatId'], $msg);
+	
+	$txt = $message['text'];
+	$bot_icq->sendText($message['chatId'], $txt);
 	/*
-	$msg = $message['payload']['msgId'];
-	$bot_icq->sendText($userId, $msg);
-	
-	$txt = $message['payload']['text'];
-	$bot_icq->sendText($userId, $txt);
-	*/
 	$событие = json_encode($event);
-	$bot_icq->sendText($userId, $событие);
-	
+	$bot_icq->sendText($message['chatId'], $событие);
+	*/
 	if ($txt == "Нажми кнопку!") $реплика = "Хорошо, а ещё?";
 	else $реплика = "Нажми кнопку!";
 	
-	$bot_icq->editText($userId, $msg, $реплика, $кнопа);	
+	$bot_icq->editText($message['chatId'], $msg, $реплика, $кнопа);	
 	
 }elseif ($callbackData == "BBB") {
 
 	$результат = $bot_icq->answerCallbackQuery($queryId, "Вот такой вот тут текст", true);
 	
-	if ($результат['ok'] == false) $bot_icq->sendText($userId, "Ошибка: {$результат['description']}");
+	if ($результат['ok'] == false) $bot_icq->sendText($message['chatId'], "Ошибка: {$результат['description']}");
 	
 }
 
