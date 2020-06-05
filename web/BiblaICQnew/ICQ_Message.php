5d336ba921a95f04dd847677323333b55dad4db4
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-05 19:09:08 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_Message.php
+++ web/BiblaICQnew/ICQ_Message.php
@@ -15,6 +15,18 @@ if ($text=='/start') {
 				"text" => "Нажми меня",
 				"callbackData" => "najmimenya"
 			]
+		],
+		[			
+			[
+				"text" => "или",
+				"callbackData" => "ili"
+			]
+		],
+		[			
+			[
+				"text" => "Удали меня",
+				"callbackData" => "udalimenya"
+			]
 		]
 	];
 	
