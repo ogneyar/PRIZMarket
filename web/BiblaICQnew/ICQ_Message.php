b7671c0f7ab82569bf3a0ccdbe2b67147a8157ad
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-05 16:24:50 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_Message.php
+++ web/BiblaICQnew/ICQ_Message.php
@@ -66,7 +66,7 @@ if ($text=='/start') {
 	$реплика = "Эт ты на каком языке? Уууфь, нет такой буквы в алфавите!";
 	$bot_icq->sendText($chatId, $реплика);
 	
-}elseif ($text) $bot_icq->sendText($chatId, "я не понимаю(");
+}elseif ($text && $chatType == 'private') $bot_icq->sendText($chatId, "я не понимаю(");
 
 //------------------------------------------
 }
