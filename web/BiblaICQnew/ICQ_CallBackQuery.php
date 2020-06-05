eba18ca1cd658d24966e51b30a23eecb9b3cd2ff
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-05 19:40:39 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_CallBackQuery.php
+++ web/BiblaICQnew/ICQ_CallBackQuery.php
@@ -4,12 +4,15 @@ if ($callbackData == "najmimenya") {
 	
 	$результат = $bot_icq->answerCallbackQuery($queryId, "Ай молодец)))");	
 	if ($результат['ok'] == false) $bot_icq->sendText($userId, "Ошибка: {$результат['description']}");
-	
+	/*
 	$msg = $message['payload']['msgId'];
 	$bot_icq->sendText($userId, $msg);
 	
 	$txt = $message['payload']['text'];
 	$bot_icq->sendText($userId, $txt);
+	*/
+	$событие = json_encode($event);
+	$bot_icq->sendText($userId, $событие);
 	
 	if ($txt == "Нажми кнопку!") $реплика = "Хорошо, а ещё?";
 	else $реплика = "Нажми кнопку!";
