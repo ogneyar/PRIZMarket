cdc66616a64bc0237a1a407ffb6f6c3bd93c24d5
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-05 21:00:29 +0300

    Add files via upload

--- web/BiblaICQnew/ICQ_CallBackQuery.php
+++ web/BiblaICQnew/ICQ_CallBackQuery.php
@@ -36,7 +36,7 @@ if ($callbackData == "najmimenya") {
 	$результат = $bot_icq->answerCallbackQuery($queryId, "Ой, ну и ладно(");	
 	if ($результат['ok'] == false) $bot_icq->sendText($chatId, "Ошибка: {$результат['description']}");
 	
-	​$bot_icq->deleteMessages($chatId, $msgId);	
+	​//$bot_icq->deleteMessages($chatId, $msgId);	
 	
 }elseif ($callbackData == "BBB") {
 
