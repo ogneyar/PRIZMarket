4cc40a259a564c33bc0f3147642a32d14178f34f
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-23 20:28:52 +0300

    Add files via upload

--- web/site_pzm/site_files/head.php
+++ web/site_pzm/site_files/head.php
@@ -17,7 +17,7 @@
 	
 	<meta http-equiv="Cache-Control" content="no-cache">
 	
-	<link rel="stylesheet" href="/site_pzm/css/style5.css">
+	<link rel="stylesheet" href="/site_pzm/css/style7.css">
 	
 	<link rel="stylesheet" href="/site_pzm/font-awesome/css/font-awesome.min.css">
 	
