44e9ea47838818193a07d2c4d50190b8073cd9bc
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-06-23 19:30:31 +0300

    Add files via upload

--- web/site_pzm/site_files/footer.php
+++ web/site_pzm/site_files/footer.php
@@ -1,12 +1,7 @@
 ﻿<div id="rights">
 	&copy; PRIZMarket <?=date('Y')?>
 </div>
-<div id="social">	
-	<article>
-		<a href="https://t.me/prizm_market" title="Телеграм канал" target="_blank">
-			<img src="/site_pzm/img/social/tg.png" alt="Телеграм канал" />
-		</a>
-	</article>
+<div id="social">
 	<article>
 		<a href="https://vk.com/prizmarket_vk" title="Группа Вконтакте" target="_blank">
 			<img src="/site_pzm/img/social/vkontakte.png" alt="Вконтакте PRIZMarket" />
@@ -20,6 +15,11 @@
 	<article>
 		<a href="https://www.instagram.com/prizm_market_inst" title="Инстаграм" target="_blank">
 			<img src="/site_pzm/img/social/instagram.png" alt="Инстаграм PRIZMarket" />
+		</a>
+	</article>
+	<article>
+		<a href="https://t.me/prizm_market" title="Телеграм канал" target="_blank">
+			<img src="/site_pzm/img/social/tg.png" alt="Телеграм канал" />
 		</a>
 	</article>
 </div>
