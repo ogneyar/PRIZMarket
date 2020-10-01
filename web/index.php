7a698b929f550ad091b95ad8ded1f44bc46725bd
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-10-01 22:41:31 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,54 +1,51 @@
 <?php
-$indicesServer = array('PHP_SELF',
-'argv',
-'argc',
-'GATEWAY_INTERFACE',
-'SERVER_ADDR',
-'SERVER_NAME',
-'SERVER_SOFTWARE',
-'SERVER_PROTOCOL',
-'REQUEST_METHOD',
-'REQUEST_TIME',
-'REQUEST_TIME_FLOAT',
-'QUERY_STRING',
-'DOCUMENT_ROOT',
-'HTTP_ACCEPT',
-'HTTP_ACCEPT_CHARSET',
-'HTTP_ACCEPT_ENCODING',
-'HTTP_ACCEPT_LANGUAGE',
-'HTTP_CONNECTION',
-'HTTP_HOST',
-'HTTP_REFERER',
-'HTTP_USER_AGENT',
-'HTTPS',
-'REMOTE_ADDR',
-'REMOTE_HOST',
-'REMOTE_PORT',
-'REMOTE_USER',
-'REDIRECT_REMOTE_USER',
-'SCRIPT_FILENAME',
-'SERVER_ADMIN',
-'SERVER_PORT',
-'SERVER_SIGNATURE',
-'PATH_TRANSLATED',
-'SCRIPT_NAME',
-'REQUEST_URI',
-'PHP_AUTH_DIGEST',
-'PHP_AUTH_USER',
-'PHP_AUTH_PW',
-'AUTH_TYPE',
-'PATH_INFO',
-'ORIG_PATH_INFO') ;
+include_once '../vendor/autoload.php';	
+include_once 'a_conect.php';
 
-echo '<table cellpadding="10">' ;
-foreach ($indicesServer as $arg) {
-    if (isset($_SERVER[$arg])) {
-        echo '<tr><td>'.$arg.'</td><td>' . $_SERVER[$arg] . '</td></tr>' ;
-    }
-    else {
-        echo '<tr><td>'.$arg.'</td><td>-</td></tr>' ;
-    }
-}
-echo '</table>' ;
+$количество_лотов = 10;
+
+include_once 'site_pzm/pzmarket.php';
 
 ?>
+<!DOCTYPE html>
+<html xmlns="https://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />
+	<title>PRIZMarket!</title>
+	<meta name="description" content="Сайт для рекламы товаров/услуг за криптовалюту PRIZM" />
+	<meta name="keywords" content="PRIZM, криптовалюта, товары, услуги, куплю, продам, безоплатно, бесплатно" />
+	<?include_once 'site_pzm/site_files/head.php';?>
+	<style type="text/css">
+		@media (min-width: 700px) {
+		nav a:first-child, nav#fixed a:first-child {
+			border-top: 5px solid rgba(255,235,59);
+		}} 
+	</style>
+</head>
+<body>
+	<header>
+		<?include_once 'site_pzm/site_files/header.php';?>
+	</header>
+	<div id="lk_menu">
+		<?include_once 'site_pzm/lk/index-lk_menu.php';?>		
+	</div>
+	<nav>
+		<?include_once 'site_pzm/site_files/nav.php';?>
+	</nav>
+	<div id="slideMenu">Моё)</div>
+	<div id="wrapper">
+		<div id="TopCol">		
+			<?include_once 'site_pzm/site_files/wrapper-topCol.php';?>
+		</div>
+		<div id="leftCol">		
+			<?include_once 'site_pzm/site_files/wrapper-leftCol.php';?>
+		</div>
+		<div id="rightCol">
+			<?include_once 'site_pzm/site_files/wrapper-rightCol.php';?>
+		</div>
+	</div>
+	<footer>
+		<?include_once 'site_pzm/site_files/footer.php';?>
+	</footer>
+</body>
+</html>
