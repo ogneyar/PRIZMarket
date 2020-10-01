1a431861e1cdd9623d0c4d8201a6440b2ede5df5
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-10-01 22:30:11 +0300

    Update nav.php

--- web/site_pzm/site_files/nav.php
+++ web/site_pzm/site_files/nav.php
@@ -1,4 +1,4 @@
-﻿<div class="line-menu">
+<div class="line-menu">
 <a href="/">Главная</a>
 <a href="/site_pzm/podrobnosti/index.php?podrobnosti=st">Подробно</a>
 <a href="/site_pzm/kategory/index.php">Категории</a>
@@ -14,4 +14,4 @@
 			<li><a href="/site_pzm/support/index.php">ТехПоддержка</a></li>
 			<li><a href="/site_pzm/o_prizmarket/index.php">О_нас</a></li>
 		</ul>
-</ul>
+
