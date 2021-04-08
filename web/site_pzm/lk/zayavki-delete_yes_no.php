f67cfd04fca82f0a23c765f9a1934728fb626009
ogneyar <ogneyar-ne@ya.ru>
2021-04-08 20:50:15 +0300

    eee

--- web/site_pzm/lk/zayavki-delete_yes_no.php
+++ web/site_pzm/lk/zayavki-delete_yes_no.php
@@ -1,7 +1,7 @@
 ﻿<article id="repeat_delete">
 	<form action='/site_pzm/lk/zayavki.php' method='post'>		
 		<input type='hidden' name='id_lota' id='id_lota' value='<?php echo $_POST['id_lota'];?>'>
-		<input type='hidden' name='file' id='file' value='<?php echo $_POST['file'];?>'>
+		<input type='text' name='file' id='file' value='<?php echo $_POST['file'];?>'>
 
 		<h4><br>
 		<label>Вы уверены что хотите удалить лот <?php echo $_POST['id_lota'];?>?</label><br><br>
@@ -24,7 +24,7 @@ $(document).ready (function (){
 			type: 'POST',
 			cache: false,
 			data: {
-				'file': $("#file")
+				'file': <?php echo $_POST['file'];?>
 			},
 			dataType: 'html',
 			success: function (data) {
