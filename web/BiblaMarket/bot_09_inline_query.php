4a0d389a4d4022eb53cda8c7fe2c3d9a7526c4e9
ogneyar <ogneyar-ne@ya.ru>
2021-08-31 16:04:50 +0300

    test

--- web/BiblaMarket/bot_09_inline_query.php
+++ web/BiblaMarket/bot_09_inline_query.php
@@ -52,7 +52,7 @@ if ($arr['inline_query']) {
 	// $res2=[["type"=>"article","id"=>$inline_query_from_id,"title"=>"kurs","description"=>"desc","input_message_content"=>["message_text"=>"text"]]];
 	$res2=[["type"=>"article","id"=>$inline_query_from_id,"title"=>$title,"description"=>"desc","input_message_content"=>["message_text"=>$kurs_PZM],$keyInLine10]];
 
-	$tg->sendMessage($master, "ghg".$textClick);
+	$tg->sendMessage($master, "ghg");
 	
 	include_once 'myBotApi/Bot.php';
 	$bot = new Bot($tokenMARKET);
