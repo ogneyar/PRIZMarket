76167e6c812478e52d83a8037490ad0e967f6be7
ogneyar <ogneyar-ne@ya.ru>
2021-08-31 12:43:00 +0300

    test

--- web/BiblaMarket/bot_09_inline_query.php
+++ web/BiblaMarket/bot_09_inline_query.php
@@ -43,15 +43,19 @@ if ($arr['inline_query']) {
 	$keyInLine10 = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup($inLine10_keyb);
 		
 	$inputText = new \TelegramBot\Api\Types\Inline\InputMessageContent\Text($reply);		
-		
+
+	
 	$queryArticle = new \TelegramBot\Api\Types\Inline\QueryResult\Article($inline_query_from_id,
-		$title, $textClick, null,null,null, $inputText, $keyInLine10);		
-	$res=[$queryArticle];				
+	$title, $textClick, null,null,null, $inputText, $keyInLine10);		
+	$res=[$queryArticle];
+	
+	$inputText2 = ["message_text"=>$reply];		
+	$resp=["type"=>"article","id"=>$inline_query_from_id,"title"=>$title,"input_message_content"=>$inputText2];
 
-	// $tg->sendMessage($master, json_encode($res));
-	$tg->sendMessage($master, $inline_query_from_id);
+	$tg->sendMessage($master, json_encode($resp));
+	// $tg->sendMessage($master, $inline_query_from_id);
 
-	$tg->answerInlineQuery($inline_query_id, $res, null, null, null, "в бот", "s");			
+	$tg->answerInlineQuery($inline_query_id, $resp, null, null, null, "в бот", "s");			
 	
 	exit('ok');
 }
