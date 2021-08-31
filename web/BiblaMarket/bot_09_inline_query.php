400bf581412672468a02881c3846150b03c39225
ogneyar <ogneyar-ne@ya.ru>
2021-08-31 16:25:43 +0300

    test

--- web/BiblaMarket/bot_09_inline_query.php
+++ web/BiblaMarket/bot_09_inline_query.php
@@ -9,7 +9,7 @@
 */
 if ($arr['inline_query']) {	
 	
-	//$reply = _kurs_PZM();
+	//$messageText = _kurs_PZM();
 	$kurs_PZM = _kurs_PZM();
 	//$курс_РКАЦ = _курс_РКАЦ();
 	
@@ -17,61 +17,55 @@ if ($arr['inline_query']) {
 	$kurs_PZM = str_replace("[CoinMarketCap](https://coinmarketcap.com/ru/currencies/prizm/)",
 		"CoinMarketCap.com", $kurs_PZM);
 		
-	//$reply = $курс_РКАЦ . $kurs_PZM;
-	$reply = $kurs_PZM;
+	//$messageText = $курс_РКАЦ . $kurs_PZM;
+	$messageText = $kurs_PZM;
 	
-	$title = "Курс PRIZM.";
+	// $title = "Курс PRIZM.";
 	
-	$chast_stroki = strstr($reply, 10, true);
-	$kol = strlen($chast_stroki) + 1;
-	$textClick = substr($reply, $kol);	
+	// $chast_stroki = strstr($messageText, 10, true);
+	// $kol = strlen($chast_stroki) + 1;
+	// $textClick = substr($messageText, $kol);	
 	
 	// КНОПКА Репост
-	$inLine10_but1=["text"=>"Репост","switch_inline_query"=>"курс"];
-
-	// class InlineButton{
-	// 	public $text = "Репост";
-	// 	public $switch_inline_query = "курс";
-	// }
-	// $inlineButton = new InlineButton();
-
-	// $tg->sendMessage($master, $inlineButton->text);
-
-	$inLine10_str1=[$inLine10_but1];
-	// $inLine10_str1=[$inlineButton];
-	$inLine10_keyb=[$inLine10_str1];
+	// $inLine10_but1=["text"=>"Репост","switch_inline_query"=>"курс"];
+	// $inLine10_str1=[$inLine10_but1];
+	$inLineKeyboard=[
+		[
+			[
+				"text"=>"Репост",
+				"switch_inline_query"=>"курс"
+			]
+		]
+	];
 	// $keyInLine10 = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup($inLine10_keyb);
-	$keyInLine10 = [$inLine10_keyb];
+	// $keyInLine10 = ["inline_keyboard"=>$inLine10_keyb];
 		
-	$inputText = new \TelegramBot\Api\Types\Inline\InputMessageContent\Text($reply);	
+	// $inputText = new \TelegramBot\Api\Types\Inline\InputMessageContent\Text($messageText);	
 
-	$queryArticle = new \TelegramBot\Api\Types\Inline\QueryResult\Article($inline_query_from_id,
-	$title, $textClick, null,null,null, $inputText, $keyInLine10);		
-	$res=[$queryArticle];
+	// $queryArticle = new \TelegramBot\Api\Types\Inline\QueryResult\Article($inline_query_from_id,
+	// $title, $textClick, null,null,null, $inputText, $keyInLine10);		
+	// $res=[$queryArticle];
 	
 	// $res2=[["type"=>"article","id"=>$inline_query_from_id,"title"=>"kurs","description"=>"desc","input_message_content"=>["message_text"=>"text"]]];
-	$res2=[
+	$result=[
 		[
 			"type"=>"article",
 			"id"=>$inline_query_from_id,
-			"title"=>$title,
-			"description"=>"desc",
-			"input_message_content"=>["message_text"=>$kurs_PZM],
-			// "reply_markup"=>$keyInLine10
+			"title"=>"Курс PRIZM.",
+			"description"=>"курс PZM на CoinMarketCap.com",
+			"input_message_content"=>["message_text"=>$messageText],
+			"reply_markup"=>["inline_keyboard"=>$inLineKeyboard]
 		]
 	];
 
-	$tg->sendMessage($master, "ghg");
 	
-	// include_once 'myBotApi/Bot.php';
-	// $bot = new Bot($tokenMARKET);
-	// $bot->answerInlineQuery($inline_query_id, $res2); 
+	include_once 'myBotApi/Bot.php';
+	$bot = new Bot($tokenMARKET);
+	$bot->answerInlineQuery($inline_query_id, $result); 
 	
-	$tg->sendMessage($master, $textClick);
 
 
-	// $tg->answerInlineQuery($inline_query_id, $res, null, null, null, "в бот", "s");		
-	// $tg->answerInlineQuery($inline_query_id, json_decode($res2), null, null, null, "в бот", "s");		
+	// $tg->answerInlineQuery($inline_query_id, $res, null, null, null, "в бот", "s");
 	
 	exit('ok');
 }
